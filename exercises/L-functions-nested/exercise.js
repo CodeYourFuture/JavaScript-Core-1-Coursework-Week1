d559168faf4e7c840feaa28cb50c8afcdff4775a
Cecilia <earwen1986@gmail.com>
2020-11-25 18:56:57 +0000

    Completed exercises G-L

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,5 +1,16 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+var students = { "type": "students", "number": 15};
+var mentors = { "type": "mentors", "number": 8};
+
+function calculatePercentage(a,b) {
+    return Math.round(a.number / (a.number + b.number) * 100);
+}
+
+function createMessage(cat1, cat2) {
+    return "Percentage " + cat1.type + ": " + calculatePercentage(cat1, cat2) + "%";
+}
+
+var percentageOfStudents = createMessage(students, mentors);
+var percentageOfMentors = createMessage(mentors, students);
+
+console.log(percentageOfStudents);
+console.log(percentageOfMentors);
