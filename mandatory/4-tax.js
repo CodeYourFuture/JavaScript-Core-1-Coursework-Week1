a861076575eec202b279a4604d2af74c29729f9c
Cecilia <earwen1986@gmail.com>
2020-11-26 09:07:53 +0000

    Completed mandatory exercises

--- mandatory/4-tax.js
+++ mandatory/4-tax.js
@@ -5,7 +5,9 @@
   Sales tax is 20% of the price of the product
 */
 
-function calculateSalesTax() {}
+function calculateSalesTax(price) {
+  return price += price * 0.2;
+}
 
 /*
   CURRENCY FORMATTING
@@ -17,7 +19,10 @@ function calculateSalesTax() {}
   Remember that the prices must include the sales tax (hint: you already wrote a function for this!)
 */
 
-function addTaxAndFormatCurrency() {}
+function addTaxAndFormatCurrency(price) {
+  price = calculateSalesTax(price);
+  return `£${price.toFixed(2)}`
+}
 
 /* ======= TESTS - DO NOT MODIFY =====
 There are some Tests in this file that will help you work out if your code is working.
