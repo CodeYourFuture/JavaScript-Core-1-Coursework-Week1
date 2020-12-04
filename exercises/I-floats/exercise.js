19ae4492aa8b71b8c73cf8742ff323a9a7b56129
Chizim Chinuru <garrohat@yahoo.co.uk>
2020-12-04 23:21:48 +0000

    completed exercises

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,9 +1,11 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
 
+var total = numberOfStudents + numberOfMentors;
+
 // get percentage of students and mentors
-var convertStudents = numberOfStudents + numberOfStudents * 0.15;
-var convertMentors = numberOfMentors + numberOfMentors * 0.08;
+var convertStudents =  numberOfStudents / total * 100;
+var convertMentors =  numberOfMentors / total * 100;
 
 // round the figues to a whole number
 var percentageOfStudents = Math.round(convertStudents);
--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,5 +1,39 @@
+function groupPercentage(num1, num2){
+    let totalInGroup = num1 + num2;    
+    return calculatePercentage = Math.round((num1 / totalInGroup) * 100);
+}
+
+function message(numberOfStudents, numberOfMentors){
+    let percentageOfStudents = groupPercentage(numberOfStudents, numberOfMentors);
+    let percentageOfMentors = groupPercentage (numberOfMentors, numberOfStudents);
+    return displayPercentage = `Percentage students: ${percentageOfStudents}%\nPercentage students: ${percentageOfMentors}%`;
+}
+ let percentage = message(15, 8);
+ console.log(percentage);
+
+
+// Exercise 2
+
 var mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function shoutyGreeting(mentor){
+    let blockNames = mentor.toUpperCase();
+    return greeting = `HELLO ${blockNames}`;
+
+}
+
+var firstGreeting = shoutyGreeting(mentor1);
+var seccondGreeting = shoutyGreeting(mentor2);
+var thirdGreeting = shoutyGreeting(mentor3);
+var fourthGreeting = shoutyGreeting(mentor4);
+var fifthGreeting = shoutyGreeting(mentor5);
+
+console.log(firstGreeting);
+console.log(seccondGreeting);
+console.log(thirdGreeting);
+console.log(fourthGreeting);
+console.log(fifthGreeting);
