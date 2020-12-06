0bba037fe86882cd5e06ec8d4fa67ac408beeb5e
Waleed Al-Rwaishan <waleedip@yahoo.com>
2020-12-06 14:19:40 +0000

    Update 3-magic-8-ball.js
    passed all the tests successfully

--- extra/3-magic-8-ball.js
+++ extra/3-magic-8-ball.js
@@ -42,11 +42,36 @@
     Outlook not so good.
     Very doubtful.
 */
+var answersArr = ["It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes - definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful."
+    ]
+    //var answersArr = ["Very positive", "Very negative"];
 
 // This should log "The ball has shaken!"
 // and return the answer.
 function shakeBall() {
-  //Write your code in here
+    //Write your code in here
+    console.log("The ball has shaken!");
+    let answer = answersArr[Math.floor(Math.random() * 19)];
+    return answer;
 }
 
 /* 
@@ -59,8 +84,43 @@ function shakeBall() {
   This function should expect to be called with any value which was returned by the shakeBall function.
 */
 function checkAnswer(answer) {
-  //Write your code in here
+    //Write your code in here
+    switch (answer) {
+        case "It is certain.":
+        case "It is decidedly so.":
+        case "Without a doubt.":
+        case "Yes - definitely.":
+        case "You may rely on it.":
+            return "very positive";
+            break;
+
+        case "As I see it, yes.":
+        case "Most likely.":
+        case "Outlook good.":
+        case "Yes.":
+        case "Signs point to yes.":
+            return "positive";
+            break;
+
+        case "Reply hazy, try again.":
+        case "Ask again later.":
+        case "Better not tell you now.":
+        case "Cannot predict now.":
+        case "Concentrate and ask again.":
+            return "negative";
+            break;
+
+        case "Don't count on it.":
+        case "My reply is no.":
+        case "My sources say no.":
+        case "Outlook not so good.":
+        case "Very doubtful.":
+            return "very negative";
+            break;
+    }
+    console.log(answer);
 }
+shakeBall();
 
 /* 
 ==================================
@@ -74,57 +134,57 @@ To run these tests type `node 3-magic-8-ball.js` into your terminal
 
 const log = console.log;
 let logged;
-console.log = function () {
-  log(...arguments);
-  logged = arguments[0];
+console.log = function() {
+    log(...arguments);
+    logged = arguments[0];
 };
 
 function test(test_name, expr) {
-  let status;
-  if (expr) {
-    status = "PASSED";
-  } else {
-    status = "FAILED";
-  }
+    let status;
+    if (expr) {
+        status = "PASSED";
+    } else {
+        status = "FAILED";
+    }
 
-  logged = undefined;
-  console.log(`${test_name}: ${status}`);
+    logged = undefined;
+    console.log(`${test_name}: ${status}`);
 }
 
 const validAnswers = [];
+
 function testAll() {
-  const answer = shakeBall();
-  test(
-    `shakeBall logs "The ball has shaken!"`,
-    logged === "The ball has shaken!"
-  );
-  test(`shakeBall returns an string answer`, typeof answer === "string");
+    const answer = shakeBall();
+    test(
+        `shakeBall logs "The ball has shaken!"`,
+        logged === "The ball has shaken!"
+    );
+    test(`shakeBall returns an string answer`, typeof answer === "string");
 
-  test(
-    `checkAnswer("It is decidedly so.") returns "very positive`,
-    checkAnswer("It is decidedly so.") === "very positive"
-  );
+    test(
+        `checkAnswer("It is decidedly so.") returns "very positive`,
+        checkAnswer("It is decidedly so.") === "very positive"
+    );
 
-  test(
-    `checkAnswer("My reply is no.") returns "very negative`,
-    checkAnswer("My reply is no.") === "very negative"
-  );
+    test(
+        `checkAnswer("My reply is no.") returns "very negative`,
+        checkAnswer("My reply is no.") === "very negative"
+    );
 
-  test(
-    `checkAnswer returns the level of positivity"`,
-    ["very positive", "positive", "negative", "very negative"].includes(
-      checkAnswer(answer)
-    )
-  );
-  const answers = new Set();
-  for (let i = 0; i < 10; ++i) {
-    answers.add(shakeBall());
-  }
-  test(`shakeBall returns different answers`, answers.size > 1);
-  test(
-    `checkAnswer returns different answers`,
-    new Set(Array.from(answers.values()).map(checkAnswer)).size > 1
-  );
+    test(
+        `checkAnswer returns the level of positivity"`, ["very positive", "positive", "negative", "very negative"].includes(
+            checkAnswer(answer)
+        )
+    );
+    const answers = new Set();
+    for (let i = 0; i < 10; ++i) {
+        answers.add(shakeBall());
+    }
+    test(`shakeBall returns different answers`, answers.size > 1);
+    test(
+        `checkAnswer returns different answers`,
+        new Set(Array.from(answers.values()).map(checkAnswer)).size > 1
+    );
 }
 
 testAll();
