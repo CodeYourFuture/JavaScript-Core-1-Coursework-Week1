64e085373497bc506fd48a5fb52b6b66b1f0f90d
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2020-12-06 00:25:03 +0000

    Extra Exercise 1 - Currency Conversion Completed

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -5,17 +5,21 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(priceInPound) {
+  return priceInPound * 1.4;
+}
 
 /*
   CURRENCY FORMATTING
   ===================
   The business is now breaking into the Brazilian market
-  Write a new function for converting to the Brazilian real (exchange rate is 5.7 BRL to £)
+  Write a new function for converting to the Brazilian real (exchange rate is 5.643 BRL to £)
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL() {}
+function convertToBRL(priceInPound) {
+  return priceInPound * 5.643;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
