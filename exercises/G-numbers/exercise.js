dd996e69c92ac012f48a14de56f895f2d688a798
Cecilia <earwen1986@gmail.com>
2020-12-06 09:35:10 +0000

    Fixed exercise G

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -2,6 +2,6 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
 
-console.log("Number of students: ",numberOfStudents);
-console.log("Number of mentors: ", numberOfMentors);
-console.log("Total number of students and mentors: ", numberOfMentors + numberOfStudents);
+console.log("Number of students: " + numberOfStudents);
+console.log("Number of mentors: " + numberOfMentors);
+console.log("Total number of students and mentors: " + (numberOfMentors + numberOfStudents));
