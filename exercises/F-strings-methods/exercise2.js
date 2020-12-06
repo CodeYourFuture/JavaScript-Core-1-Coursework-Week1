5d0b4dd7ebcd455fd5c3c049aa48fa924db965f2
Waleed Al-Rwaishan <waleedip@yahoo.com>
2020-12-06 04:44:30 +0000

    updated exercises\F-strings-methods\exercise.js and exercise2.js
    exercise.js: declared string variable and assigned it to my name.
    - declared numeric variable and assigned it to length of my name using the string method lenght
    - Build the message inside a string variable using the previous method and string concatenation operator +
    - Output the message using the console.log
    
    exercise2.js:
    delcared my name as const
    used another variable to get my name without the extra whitespace using the string trim method
    declared a variable to store the string length using length string method.
    - Build the message using previous variable and string concatenation operator +
    - output it the message using console.log message

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,6 @@
-const name = " Daniel  ";
+const name = " Waleed  ";
+var myName = name.trim();
+var myNameLength = myName.length;
+var message = "My name is " + myName + " and my name is " + myNameLength + " characters long";
 
 console.log(message);
