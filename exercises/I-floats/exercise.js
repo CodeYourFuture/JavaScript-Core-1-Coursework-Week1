8996b8cf1960c295a526bca40bca7dfc1802b9fb
Waleed Al-Rwaishan <waleedip@yahoo.com>
2020-12-06 05:05:02 +0000

    Updated exercises\I-floats\exercise.js
    -declared two varaibles with number of students and mentors
    -declared a variable to find the total
    _ declared a variable to store student percentage using round from the Math libary to round it to the nearest whole number.
    _ again declared a variable for mentor percentage and used round method from math library and mathmatical formula to find the percentage.
    - output it both percentages

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,8 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var total = numberOfStudents + numberOfMentors;
+var studentPercentage = Math.round(numberOfStudents / total * 100);
+var mentorPercentage = Math.round(numberOfMentors / total * 100);
+
+console.log("Percentage students: " + studentPercentage + "%");
+console.log("Percentage mentors: " + mentorPercentage + "%");
