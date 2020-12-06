a0bd7130281885f21df3f97d8264c5d078ea492c
Waleed Al-Rwaishan <waleedip@yahoo.com>
2020-12-06 04:53:31 +0000

    Updated exercises\G-numbers\exercise.js
    -declared two cons for number of students and number of mentors
    - declared a variable named sum to find the summation of the two variable.
    - used the console.log to output to the terminal number of students, number of mentors, and the total number of both

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,8 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+
+const numberOfStudents = 15;
+const numberOfMentors = 8;
+var sum = numberOfStudents + numberOfMentors;
+console.log("Number of students: " + numberOfStudents);
+console.log("Number of mentors: " + numberOfMentors);
+console.log("Total numnber of students and mentors: " + sum);
