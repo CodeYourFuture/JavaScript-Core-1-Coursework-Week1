496a35da2d6bb97b0f69b01638a4de2807dceba5
Miss <emesquitta73@gmail.com>
2020-12-06 17:04:14 +0000

    mandatory1-4

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,14 +1,17 @@
 // Add comments to explain what this function does. You're meant to use Google!
+//To return a number between 0-10
 function getNumber() {
   return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+//To concatenate the two strings together
 function s(w1, w2) {
   return w1.concat(w2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
+ return firstWord.concat (secondWord, thirdWord);
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
 }
