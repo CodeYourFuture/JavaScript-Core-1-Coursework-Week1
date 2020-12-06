ca18963ae1481dc5db98ec85f61da10eb59ef4a1
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2020-12-06 21:52:12 +0000

    Exercise F-strings-methods Completed

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,14 @@
+// Code that existed already
 const name = " Daniel  ";
 
-console.log(message);
+// Code written by me
+// Variable initialization
+var trimmedName = name.trim();
+var sentencePart1 = "My name is";
+var sentencePart2 = "and my name is";
+var nameLength = trimmedName.length;
+var sentencePart3 = "characters long.";
+
+// Print the message on console
+console.log(sentencePart1 + " " + trimmedName + " " + sentencePart2 + " " + nameLength + " " + sentencePart3);
+
