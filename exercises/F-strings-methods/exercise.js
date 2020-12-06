3c1683f48eb6f559f8bd859894757e0baac8f392
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2020-12-06 21:49:36 +0000

    Exercise F-string-methods Completed

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,7 @@
 // Start by creating a variable `message`
-
-console.log(message);
+var sentencePart1 = "My name is";
+var name = "Bob";
+var sentencePart2 = "and my name is";
+var nameLength = name.length;
+var sentencePart3 = "characters long.";
+console.log(sentencePart1 + " " + name + " " + sentencePart2 + " " + nameLength + " " + sentencePart3);
