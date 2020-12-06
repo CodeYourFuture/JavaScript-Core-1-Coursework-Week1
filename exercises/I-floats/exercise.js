f4eee773031d86c662110f3a14d5e4115a893170
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2020-12-06 22:06:08 +0000

    Exercise I-floats Completed

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,12 @@
+// Variable initialization
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var total = numberOfStudents + numberOfMentors;
+var percentOfStudents = numberOfStudents / (total) * 100;
+var percentOfMentors = numberOfMentors / (total) * 100;
+
+// Print the message
+console.log(`Percentage students: ${Math.round(percentOfStudents)}%`);
+console.log(`Percentage mentors: ${Math.round(percentOfMentors)}%`);
+
+
