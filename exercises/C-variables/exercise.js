45154eab687f8dfafb55dd95332a6ab5cdfe72d3
jwillson2020 <73646450+jwillson2020@users.noreply.github.com>
2020-12-07 17:47:12 +0000

    added code to B & C
    added code to B & C
    B new console log
    C repeat x3 \n

--- exercises/C-variables/exercise.js
+++ exercises/C-variables/exercise.js
@@ -1,3 +1,3 @@
 // Start by creating a variable `greeting`
-
-console.log(greeting);
+var greeting = "Hello world\n";  // added  backspace \n newline
+console.log(greeting.repeat(3)); // added repeat 3
