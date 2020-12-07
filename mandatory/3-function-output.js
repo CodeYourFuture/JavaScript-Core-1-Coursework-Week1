b130fe024807c4bb4ee980e5d6f410bc39c59407
samia-pk <samiaarshad92@gmail.com>
2020-12-07 03:48:13 +0000

    defines function and fix error

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,16 +1,18 @@
 // Add comments to explain what this function does. You're meant to use Google!
 function getNumber() {
-  return Math.random() * 10;
+  return Math.random() * 10; // returns random numbers between 0-10. 
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
 function s(w1, w2) {
-  return w1.concat(w2);
+  return w1.concat(w2); //combines w1 and w2. 
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+  return firstWord + " " + secondWord + " " + thirdWord;
+
 }
 
 /* 
