830c9837a2b3513e1bf42654fd2325389115b4cd
moniqueking57 <65675759+moniqueking57@users.noreply.github.com>
2020-12-08 10:16:29 +0000

    Done 1,2,3 working on 4

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,14 +1,17 @@
-// Add comments to explain what this function does. You're meant to use Google!
+// This function is getting a random number to be defined later to be times by 10. 
 function getNumber() {
   return Math.random() * 10;
 }
 
-// Add comments to explain what this function does. You're meant to use Google!
+
+
+// This function is concatenating word 1 and word 2. The words will be defined later.
 function s(w1, w2) {
   return w1.concat(w2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
+  return firstWord.concat(" " + secondWord) + " " + thirdWord;
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
 }
