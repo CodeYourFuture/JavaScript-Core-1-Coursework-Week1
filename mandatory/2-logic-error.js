5f5adb33d4e6c6cd60cb880d5a1b624022bd1b41
Amanuel <weditedi25@gmail.com>
2020-12-08 13:52:19 +0000

    Logic erros fixed

--- mandatory/2-logic-error.js
+++ mandatory/2-logic-error.js
@@ -1,11 +1,13 @@
 // The syntax for this function is valid but it has an error, find it and fix it.
 
 function trimWord(word) {
-  return wordtrim();
+  // return wordtrim()
+  return word.trim();
 }
 
+
 function getWordLength(word) {
-  return "word".length();
+  return word.length;
 }
 
 function multiply(a, b, c) {
