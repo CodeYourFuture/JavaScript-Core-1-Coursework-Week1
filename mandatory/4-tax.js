5b2a046b0a0b359d301ac37fef5517fa9d60a436
Amanuel <weditedi25@gmail.com>
2020-12-08 14:28:42 +0000

    Tax errors fixed

--- mandatory/4-tax.js
+++ mandatory/4-tax.js
@@ -5,7 +5,11 @@
   Sales tax is 20% of the price of the product
 */
 
-function calculateSalesTax() {}
+function calculateSalesTax(price) {
+  let saleTax = (0.2 * price) + price;
+  return saleTax;
+
+}
 
 /*
   CURRENCY FORMATTING
@@ -16,8 +20,12 @@ function calculateSalesTax() {}
 
   Remember that the prices must include the sales tax (hint: you already wrote a function for this!)
 */
-
-function addTaxAndFormatCurrency() {}
+ 
+function addTaxAndFormatCurrency(price) {
+  let saleTax = (0.2 * price) + price;
+  let retailPrize = "£" + saleTax.toFixed(2);
+  return retailPrize;
+}
 
 /* 
 ===================================================
