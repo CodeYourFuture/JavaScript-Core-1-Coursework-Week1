d9b31014af933b8019c7ee414261621b750a9339
jwillson2020 <73646450+jwillson2020@users.noreply.github.com>
2020-12-09 14:16:40 +0000

    J done
    2 x exercises

--- exercises/J-functions/exercise.js
+++ exercises/J-functions/exercise.js
@@ -1,7 +1,21 @@
+
+//Complete the function in exercise.js so that it halves the input
+
+//Try calling the function more than once with some different numbers
+
+// Remember to use the return keyword to get a value out of the function
+
+
+
+
+
 function halve(number) {
-  // complete the function here
+   return number/2;
 }
 
 var result = halve(12);
+var result2 = halve(50);
 
-console.log(result);
+
+console.log(result);   // half 12 = 6
+console.log(result2); //  half 50 = 25
--- exercises/J-functions/exercise2.js
+++ exercises/J-functions/exercise2.js
@@ -1,7 +1,12 @@
+
+//Complete the function in exercise2.js so that it triples the input
+
+
 function triple(number) {
   // complete function here
+  return number*3;
 }
 
-var result = triple(12);
+var result = triple(12);  //  12 x 3 = 36
 
 console.log(result);
