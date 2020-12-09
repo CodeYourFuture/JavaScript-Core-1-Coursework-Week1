bd973a3ef96ebfa699baba3b1c28c8cf98291923
jwillson2020 <73646450+jwillson2020@users.noreply.github.com>
2020-12-09 13:10:07 +0000

    finished G
    G finished

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,20 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+
+//Create two variables `numberOfStudents` and `numberOfMentors`
+//Log a message that displays the total number of students and mentors
+//Number of students: 15
+//Number of mentors: 8
+//Total number of students and mentors: 23
+
+
+var numberOfStudents = "Number of students: ";
+var numberOfMentors = "Number of mentors: ";
+var numberOfStudentsMentors = "Total number of students and mentors:";
+var students = 30;
+var mentors = 5;
+var totalClass = students + mentors; // 35
+
+console.log(numberOfStudents + students);
+console.log(numberOfMentors + mentors);
+console.log(numberOfStudentsMentors + totalClass);
+
