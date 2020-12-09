4eff8623419b5ef7a5e2b898dc3cee9d2e41e755
ltteh <67978926+ltteh@users.noreply.github.com>
2020-12-09 12:32:53 +0000

    completed exercises A-C

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,12 @@
 console.log("Hello world");
+console.log('\n' + "Hello world, I just started learning JavaScript!");
+
+var name = 'Liew';
+var num = 1234;
+var quote = ' is learning JavaScript!';
+var noQuoteMark = testing;
+
+console.log('\n' + name + ' ' + num + quote);
+console.log('\n' + 12345678);
+
+console.log(noQuoteMark); //uncaught Reference Error: testing is not defined
