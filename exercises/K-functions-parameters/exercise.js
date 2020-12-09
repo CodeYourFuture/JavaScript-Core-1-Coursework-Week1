4b3c7ab16111a31570714efcb93a7202fcc01136
jwillson2020 <73646450+jwillson2020@users.noreply.github.com>
2020-12-09 14:33:24 +0000

    K done ex 1,2
    done ex 1,2

--- exercises/K-functions-parameters/exercise.js
+++ exercises/K-functions-parameters/exercise.js
@@ -1,9 +1,20 @@
+
+//Write a function that multiplies two numbers together
+
+
+
+
 // Complete the function so that it takes input parameters
-function multiply() {
+function multiply(num1,num2) {
   // Calculate the result of the function and return it
+  return num1 * num2;
+
 }
 
 // Assign the result of calling the function the variable `result`
 var result = multiply(3, 4);
+var result2 = multiply(6, 9);
 
-console.log(result);
+console.log(result); // 12  3x4=12
+console.log(result2); // 54 6x9=54
+
--- exercises/K-functions-parameters/exercise2.js
+++ exercises/K-functions-parameters/exercise2.js
@@ -1,5 +1,13 @@
 // Declare your function first
+//From scratch, write a function that divides two numbers
+
+function divide (num1,num2) {
+    return num1/num2;
+}
+
 
 var result = divide(3, 4);
+var result2 = divide(100, 3);
 
-console.log(result);
+console.log(result);  // output 0.75
+console.log(Math.round(result2)); // rounded to 33 for 33.33333
