3f6475bbc8b68adaadd56a8010f837d79ff59085
ltteh <67978926+ltteh@users.noreply.github.com>
2020-12-09 17:35:42 +0000

    completed mandatory 1 - 3

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,9 +1,15 @@
 // Add comments to explain what this function does. You're meant to use Google!
+// below is a function which return a random number between >= 0 and < 10, due to the multiplication of 10. 
+// Math.random is a function returns a floating-point, pseudo-random number in the range 0 to less than 1 (inclusive of 0, but not 1)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getNumber() {
   return Math.random() * 10;
 }
 
+
 // Add comments to explain what this function does. You're meant to use Google!
+//concat() is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
 function s(w1, w2) {
   return w1.concat(w2);
 }
@@ -11,6 +17,7 @@ function s(w1, w2) {
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+  return firstWord + " " + secondWord + " " + thirdWord;
 }
 
 /* 
