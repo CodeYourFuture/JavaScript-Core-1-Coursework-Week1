2bfef81cf3bddab067121f99422db1eb49e1312c
jwillson2020 <73646450+jwillson2020@users.noreply.github.com>
2020-12-09 13:51:56 +0000

    I float done
    done I float

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,22 @@
+//Percentage students: 65%
+//Percentage mentors: 35%
+
+
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var studentsPer = "Percentage students: ";
+var mentorsPer = "Percentage mentors: ";
+var per = "%"
+var total = numberOfStudents + numberOfMentors; // 23
+//function for percentage
+function percentage (x,y) {
+  return  x/y*100;
+
+};
+var perRoundStud = Math.round(percentage(numberOfStudents,total)); // rounded up
+var perRoundMentor = Math.round(percentage(numberOfMentors,total)); // rounded up
+
+console.log(studentsPer + percentage(numberOfStudents,total) + per);
+console.log(mentorsPer + percentage(numberOfMentors,total) + per  );
+console.log(studentsPer + perRoundStud + per); // round down
+console.log(studentsPer + perRoundMentor + per); // round up
