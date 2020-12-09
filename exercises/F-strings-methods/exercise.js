d46da23e495270d462f61cb3b250c62d0d43a4d0
jwillson2020 <73646450+jwillson2020@users.noreply.github.com>
2020-12-09 12:10:17 +0000

    done E EX1
    done first part of E

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,8 @@
 // Start by creating a variable `message`
+var name = "Jonathan"; //  8 letters
+var nameLength = name.length;
+var greetingStart = "Hello, my name is Jonathan and my name is "  + nameLength + " letters long.";
+var message = greetingStart;
+
 
 console.log(message);
