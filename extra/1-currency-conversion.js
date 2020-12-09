0738b47775af7884fbab5942a35a2bca63531698
alsafi25 <62480618+alsafi25@users.noreply.github.com>
2020-12-09 13:02:47 +0000

    Currency conversion Completed

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -5,7 +5,10 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(amountInUs) {
+    let convertedToGB = Number((amountInUs * 1.4).toFixed(1));
+    return convertedToGB;
+}
 
 /*
   CURRENCY FORMATTING
@@ -15,7 +18,13 @@ function convertToUSD() {}
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL() {}
+function convertToBRL(amountInBRL) {
+
+    let convertedToGB = Number((amountInBRL * 5.70))
+    let remainAmountGB = convertedToGB - (convertedToGB * 0.01);
+    return remainAmountGB;
+
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
@@ -26,16 +35,16 @@ To run these tests type `node 1-currency-conversion` into your terminal
 const util = require("util");
 
 function test(test_name, actual, expected) {
-  let status;
-  if (actual === expected) {
-    status = "PASSED";
-  } else {
-    status = `FAILED: expected: ${util.inspect(
+    let status;
+    if (actual === expected) {
+        status = "PASSED";
+    } else {
+        status = `FAILED: expected: ${util.inspect(
       expected
     )} but your function returned: ${util.inspect(actual)}`;
-  }
+    }
 
-  console.log(`${test_name}: ${status}`);
+    console.log(`${test_name}: ${status}`);
 }
 
 test("convertToUSD function works", convertToUSD(32), 44.8);
