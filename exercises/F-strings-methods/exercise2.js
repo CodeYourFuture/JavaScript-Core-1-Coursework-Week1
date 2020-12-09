3557d17be908626a2f9622e86efa4710ce328cbe
jwillson2020 <73646450+jwillson2020@users.noreply.github.com>
2020-12-09 12:52:21 +0000

    F done
    prolem with .trim
    remember to trim the string first then the .length

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,8 @@
-const name = " Daniel  ";
+var name = " Daniel  ";  //  6 letters long
+var nameTrim = name.trim();  //  need to trim the string first!
+var nameTrimLength = nameTrim.length;
+var greetingStart = "Hello, my name is Daniel and my name is ";
+var greetingEnd = " letters long.";
 
-console.log(message);
+
+console.log(greetingStart + nameTrimLength + greetingEnd );
