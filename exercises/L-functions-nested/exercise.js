914778aa9d94bf1ee56bc3739bb869a767ce16ba
SenaitEkubai <73396195+SenaitEkubai@users.noreply.github.com>
2020-12-10 21:41:34 +0000

    created a function that returns a shouty greeting

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,13 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+var name;
+
+function shoutyGreeting(name) {
+  var nameAndGreeting = console.log(("Hello " + name).toUpperCase());
+}
+shoutyGreeting(mentor1);
+shoutyGreeting(mentor2);
+shoutyGreeting(mentor3);
+shoutyGreeting(mentor4);
+shoutyGreeting(mentor5);
