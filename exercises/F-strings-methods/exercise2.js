74c77b13be25e0296501304159ea41093cfd3edb
Sozchi <73253494+Sozchi@users.noreply.github.com>
2020-12-14 22:13:19 +0000

    i corrected my mistake

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -4,7 +4,7 @@ var nameTrim = name.trim();
 var messageType = "and my name is ";
 var nameLength = name.length;
 var many = " characters long.";
-var message = messageTypes + name + messageType + name.length + many;
+var message = messageTypes + " " +  nameTrim  + " " +  messageType  + nameLength + many;
 console.log(nameTrim);
 console.log(nameLength);
 console.log(message);
