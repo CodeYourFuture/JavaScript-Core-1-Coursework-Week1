8fd63f981f3ad28479028c152112e68ebd444c0e
Fatimoh Kuforiji <fatimohkuforiji@yahoo.com>
2020-12-17 20:31:42 +0000

    Update exercise
    Updated answer on L-functions  exercise

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,20 +1,47 @@
-
-
-
-
-
 var students = 15;
 var mentors = 8;
-var total = students + mentors;
-function functionPercentage (s , m) {
-    return s / m * 100;
+function add (students, mentors) {
+    let  result = students + mentors;
+    return result;
 }
-// var percentageOfStudent = students / total * 100
-// var percentageOfMentor = mentors/ total * 100
-console.log(Math.round (students / total * 100));
+console.log(add(students, mentors));
+
+function PercentageOfStudentAndMentor (students, mentors){
+    let result = add(students, mentors)
+    let studentPercentage = Math.round(students/ result *  100);
+    let mentorPercentage = Math.round (mentors / result * 100); 
+    return [studentPercentage, mentorPercentage];
+}
+    [percentage, stdPercent] = PercentageOfStudentAndMentor(students, mentors)
+
+    // console.log(PercentageOfStudentAndMentor(students, mentors));
+    console.log(percentage, stdPercent);
+
+ function message (students, mentors) {
+    [stdPercent, mentorPercentage] = PercentageOfStudentAndMentor(students, mentors)
+    let msg = "Percentage of students : " + stdPercent + "%";
+    let msg2 = " Percentage of mentors : " + mentorPercentage + "%";
+    return msg + msg2; 
+ }
+ console.log (message (students, mentors));
 
 
 
+
+
+// var result = 8 / total * 100; 
+// console.log (Math.round(result));
+
+// var result2 = 15/ total * 100;
+// console.log (Math.round(result2));
+
+
+
+// function mentorNames ( nameUpperCase);
+//  return "Hello" + 
+
+// var nameUpperCase = name.toUpperCase();
+// console.log (nameUpperCase);
 // var mentor1 = "Daniel";
 // var mentor2 = "Irina";
 // var mentor3 = "Mimi";
