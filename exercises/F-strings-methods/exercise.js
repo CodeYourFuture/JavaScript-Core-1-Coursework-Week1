41ecdb0aab1d87776b4e9cdea6f346eb3995095e
haroon iqbal <h.iqbal0121@gmail.com>
2020-12-25 16:04:45 +0000

    6 tasks completed

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,7 @@
 // Start by creating a variable `message`
+var myName = "Haroon";
+var messageLength = myName.length;
+var message = "My name is " + myName + " and my name is " + messageLength + " characters long";
 
 console.log(message);
+
--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,3 @@
-const name = " Daniel  ";
-
+const myName = " Daniel  ";
+var message = "My name is " + myName.trim() + " and my name is 6 characters long"
 console.log(message);
