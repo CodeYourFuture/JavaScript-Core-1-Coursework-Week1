b0c43b53d28d4ab0ad11d8c671109643a95fa561
haroon iqbal <h.iqbal0121@gmail.com>
2020-12-27 18:12:09 +0000

    tasks G + I complete

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,6 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 15;
+var numberOfMentors = 8;
+console.log("Number of Students: " + numberOfStudents);
+console.log("Number of Mentors: " + numberOfMentors);
+console.log("Total number of Students & Mentors: " + (numberOfStudents + numberOfMentors));
--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,6 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var studentsPerc = numberOfStudents/23 * 100;
+var mentorsPerc = numberOfMentors/23 * 100;
+console.log("Percentage Students: " + Math.round(studentsPerc) + "%");
+console.log("Percentage Mentors: " + Math.round(mentorsPerc) + "%");
