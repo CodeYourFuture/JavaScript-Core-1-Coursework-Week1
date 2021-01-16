6abe75e612471df4d70f4ee82a81c9093d3f4fca
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-01-16 19:53:54 +0200

    Add comments to changes

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,2 +1,12 @@
 console.log("Hello World");
 console.log(5);
+
+// When I console.log several things at once, I get a list of answers.
+
+// When I get rid of quotes,I get a syntax error.
+console.log(Hello World.I just started learning JavaScript!);
+
+console.log("Hello World. I just started learning JavaScript!");
+
+// A number without quotes.
+console.log(205);
