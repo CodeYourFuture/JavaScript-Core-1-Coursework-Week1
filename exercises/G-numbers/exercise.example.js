69f60afaa0081f7feb2dfe19bb4539e764415e01
Joshua Balfe <joshuabalfe@googlemail.com>
2021-01-16 15:04:58 +0000

    Example solution for G-numbers

---
+++ exercises/G-numbers/exercise.example.js
@@ -0,0 +1,7 @@
+var numberOfStudents = 15;
+var numberOfMentors = 8;
+var totalNumberOfStudentsAndMentors = numberOfStudents + numberOfMentors;
+
+console.log("Number of students: " + numberOfStudents);
+console.log("Number of mentors: " + numberOfMentors);
+console.log("Total number of students and mentors: " + totalNumberOfStudentsAndMentors);
