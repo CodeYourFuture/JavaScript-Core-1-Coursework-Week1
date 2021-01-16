657b9d20b358ea7313a7ffdc0fdd6e79c4d51f9e
Joshua Balfe <joshuabalfe@googlemail.com>
2021-01-16 15:05:17 +0000

    Example solution for J-functions

---
+++ exercises/J-functions/exercise.example.js
@@ -0,0 +1,7 @@
+function halve(number) {
+  return number / 2;
+}
+
+var result = halve(12);
+
+console.log(result);
---
+++ exercises/J-functions/exercise2.example.js
@@ -0,0 +1,7 @@
+function triple(number) {
+  return number * 3;
+}
+
+var result = triple(12);
+
+console.log(result);
