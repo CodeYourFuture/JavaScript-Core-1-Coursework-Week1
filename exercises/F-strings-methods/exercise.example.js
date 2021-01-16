622ece948a918e144101f287cc59077b7c7a4bc1
Joshua Balfe <joshuabalfe@googlemail.com>
2021-01-16 15:04:51 +0000

    Example solution for F-strings-methods

---
+++ exercises/F-strings-methods/exercise.example.js
@@ -0,0 +1,5 @@
+var name = "Joshua"
+var nameLength = name.length;
+var message = "My name is " + name + " and my name is " + nameLength + " characters long";
+
+console.log(message);
---
+++ exercises/F-strings-methods/exercise2.example.js
@@ -0,0 +1,6 @@
+var name = " Daniel  ";
+var trimmedName = name.trim();
+var trimmedNameLength = trimmedName.length;
+var message = "My name is " + trimmedName + " and my name is " + trimmedNameLength + " characters long";
+
+console.log(message);
