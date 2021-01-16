5bd84f1aa0a3281703f1604afd308d96ff231e94
Joshua Balfe <joshuabalfe@googlemail.com>
2021-01-16 15:06:09 +0000

    Example solution for 2-piping

---
+++ extra/2-piping.example.js
@@ -0,0 +1,67 @@
+/*
+  PIPING FUNCTIONS
+  ================
+  1. Write 3 functions:
+  - one that adds 2 numbers together
+  - one that multiplies 2 numbers together
+  - one that formats a number so it's returned as a string with a £ sign before it (e.g. 20 -> £20)
+
+  2. Using the variable startingValue as input, perform the following operations using your functions all
+  on one line (assign the result to the variable badCode):
+  - add 10 to startingValue
+  - multiply the result by 2
+  - format it
+
+  3. Write a more readable version of what you wrote in step 2 under the BETTER PRACTICE comment. Assign
+  the final result to the variable goodCode
+*/
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function format(num) {
+  return "£" + num;
+}
+
+var startingValue = 2
+
+// This is hard to read for humans looking at the code
+var badCode = format(multiply(add(startingValue, 10), 2));
+
+/* BETTER PRACTICE */
+
+var startPlus10 = add(startingValue, 10);
+var startPlus10By2 = multiply(startPlus10, 2);
+var price = format(startPlus10By2);
+var goodCode = price;
+
+/* ======= TESTS - DO NOT MODIFY =====
+There are some Tests in this file that will help you work out if your code is working.
+
+To run these tests type `node 2-piping.js` into your terminal
+*/
+
+const util = require('util');
+
+function test(test_name, actual, expected) {
+  let status;
+  if (actual === expected) {
+    status = "PASSED";
+  } else {
+    status = `FAILED: expected: ${util.inspect(expected)} but your code returned: ${util.inspect(actual)}`;
+  }
+
+  console.log(`${test_name}: ${status}`);
+}
+
+test('add function - case 1 works', add(1,3), 4)
+test('add function - case 2 works', add(2.4,5), 7.4)
+test('multiply function works', multiply(2,3), 6)
+test('format function works', format(16), "£16")
+test('badCode variable correctly assigned', badCode, "£24")
+test('goodCode variable correctly assigned', goodCode, "£24")
