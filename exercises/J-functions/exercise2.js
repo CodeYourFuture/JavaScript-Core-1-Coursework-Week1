87245d1f505bc2a43570449577be4d29b1ac2a4e
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-01-17 13:01:30 +0200

    J-functions-exercise2

--- exercises/J-functions/exercise2.js
+++ exercises/J-functions/exercise2.js
@@ -1,7 +1,16 @@
-function triple(number) {
-  return number*3
+// function triple(number) {
+//   return number*3
+// }
+
+// var result = triple(12);
+
+// console.log(result);
+
+// My copy.
+function threeTimes(number) {
+  return (number * 3)*3;
 }
 
-var result = triple(12);
+var answer = threeTimes(3);
 
-console.log(result);
+console.log(`The answer is ${answer}`)
