f5b936ccd6ad6e6e624f70b222fd1b5642c7d00d
ShawenHarker <ShawenHarker@users.noreply.github.com>
2021-01-17 10:35:06 +0200

    south-africa_shawen-aldred-harker
    Did B-hello-world and C-variables complete.

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,6 @@
 console.log("Hello world");
+console.log("Hello world, my name is Shawen, I just started learning Javascript!!!");
+console.log("Shawen Aldred Harker");
+console.log("1989");
+console.log(1989);
+console.log(Shawen); // When you run a string without quotes, it gives an error stating it's "not defined" and stops running the code.
--- exercises/C-variables/exercise.js
+++ exercises/C-variables/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `greeting`
 
+var greeting = "Hello, Shawen";
+console.log(greeting);
+console.log(greeting);
 console.log(greeting);
