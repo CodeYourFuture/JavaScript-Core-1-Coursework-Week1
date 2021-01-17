536905cefe6dc2542c464dd506a17f8b529b911c
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-01-17 09:22:07 +0200

    E-Strings-Concatenation

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,5 +1,13 @@
-const introduction = "Hello my name is";
-const name = "  Abdulqadir";
+// const introduction = "Hello my name is";
+// const name = "  Abdulqadir";
 
-console.log(introduction);
-console.log(name);
+// console.log(introduction);
+// console.log(name);
+
+// My copy
+let standardIntro = "Hello, my first name is";
+let yourName = " Douglas.";
+
+let greeting = standardIntro + yourName;
+
+console.log(greeting);
