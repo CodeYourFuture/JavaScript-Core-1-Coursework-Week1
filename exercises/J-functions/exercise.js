19b9c4035a8126c31a96fe30776f0b0a34987da8
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-01-17 12:50:18 +0200

    J-functions-exercise.js

--- exercises/J-functions/exercise.js
+++ exercises/J-functions/exercise.js
@@ -1,13 +1,25 @@
-function halve(number) {
-  return number/2;
+// function halve(number) {
+//   return number/2;
+// }
+
+// var result = halve(12);
+// var result1 = halve(20);
+// var result2 = halve(30);
+// var result3 = halve(100);
+
+// console.log(result);
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+
+function divide(number) {
+  return number / 2;
 }
 
-var result = halve(12);
-var result1 = halve(20);
-var result2 = halve(30);
-var result3 = halve(100);
+var answer1 = divide(5);
+var answer2 = divide(10);
+var answer3 = divide(15);
 
-console.log(result);
-console.log(result1);
-console.log(result2);
-console.log(result3);
+console.log(answer1);
+console.log(answer2);
+console.log(answer3);
