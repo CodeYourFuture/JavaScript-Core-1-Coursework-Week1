35679852c0a97ce875e53dd6e51f0f0bb45761c0
Kara Howard <kara.mh191@gmail.com>
2021-01-17 17:46:20 +0000

    Completed Ex e Concatenation
    Completed Exercise E

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
-
+//* Write a program that logs a message with a greeting and your name
+let greetingStart = "Hello, my name is ";
+let myName = "Kara";
+let message = greetingStart + myName;
 console.log(message);
