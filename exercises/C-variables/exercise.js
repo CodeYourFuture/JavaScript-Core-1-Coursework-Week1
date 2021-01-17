21bb8c8270735116ce75e558abc83a00be60a906
Kara Howard <kara.mh191@gmail.com>
2021-01-17 17:41:36 +0000

    Updated Ex c
    Changed message

--- exercises/C-variables/exercise.js
+++ exercises/C-variables/exercise.js
@@ -1,5 +1,5 @@
 // Start by creating a variable `greeting`
-var greeting = "Welcome to CYF!";
+var greeting = "Hello world";
 //* Print your `greeting` to the console 3 times
 for (i = 0; i < 3; i++){
     console.log(greeting);
