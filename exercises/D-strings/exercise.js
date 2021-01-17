70903a56915a51a020a9e274e9683a4dcee49077
Kara Howard <kara.mh191@gmail.com>
2021-01-17 17:41:58 +0000

    Completed Ex d Strings
    Completed Exercise D

--- exercises/D-strings/exercise.js
+++ exercises/D-strings/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
-
+//* Write a program that logs a message and its type
+let message = "This is a string";
 console.log(message);
+console.log(typeof message);
