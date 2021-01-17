d32cd9e9224be71629929c72a40e93e86ca5e295
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-01-17 09:03:22 +0200

    Exercise-D

--- exercises/D-strings/exercise.js
+++ exercises/D-strings/exercise.js
@@ -4,3 +4,10 @@ const messageType = typeof message;
 
 console.log(message);
 console.log(messageType);
+
+// My copy
+const myText = "This is a String of Douglas";
+const usingTypeof = typeof myText;
+
+console.log(myText);
+console.log(usingTypeof);
