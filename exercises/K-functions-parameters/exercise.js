5d4201467bffbdb1858fc41f327a90b27c2d6c8c
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-01-17 13:20:37 +0200

    K-functions-parameters-exercise1

--- exercises/K-functions-parameters/exercise.js
+++ exercises/K-functions-parameters/exercise.js
@@ -1,9 +1,19 @@
 // Complete the function so that it takes input parameters
+// function multiply(num1, num2) {
+//  return num1*num2; // Calculate the result of the function and return it
+// }
+
+// // Assign the result of calling the function the variable `result`
+// var result = multiply(3, 4);
+
+// console.log(result);
+
+// My copy.
 function multiply(num1, num2) {
- return num1*num2; // Calculate the result of the function and return it
+    return num1 * num2;
 }
 
-// Assign the result of calling the function the variable `result`
-var result = multiply(3, 4);
+let answer = multiply(5, 10);
 
-console.log(result);
+console.log(answer);
+
