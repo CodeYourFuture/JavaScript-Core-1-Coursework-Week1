f17533b3a947e2a709b8cd9497869d0925270900
Kara Howard <kara.mh191@gmail.com>
2021-01-17 18:23:27 +0000

    Completed Ex j Functions
    Completed Exercise J Functions

--- exercises/J-functions/exercise.js
+++ exercises/J-functions/exercise.js
@@ -1,7 +1,13 @@
+//* Complete the function in exercise.js so that it halves the input
+//* Try calling the function more than once with some different numbers
+
 function halve(number) {
-  // complete the function here
+  return number / 2;
 }
 
 var result = halve(12);
-
+console.log(result);
+result = halve(14);
+console.log(result);
+result = halve(16);
 console.log(result);
--- exercises/J-functions/exercise2.js
+++ exercises/J-functions/exercise2.js
@@ -1,5 +1,7 @@
+//* Complete the function in exercise2.js so that it triples the input
+
 function triple(number) {
-  // complete function here
+  return number * 3;
 }
 
 var result = triple(12);
