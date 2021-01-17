66017026f04b40c1e530ad08a794ed65cf2b5ead
yunusfirat <38798014+yunusfirat@users.noreply.github.com>
2021-01-17 23:45:52 +0000

    studied how to add number each other

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,8 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+let = numberOfStudents = 15;
+let = numberOfMentors = 8;
+let = totalNumnberOfStudentsAndMentors = numberOfStudents + numberOfMentors;
+
+console.log(`Number of students: ${numberOfStudents}
+Number of mentors: ${numberOfMentors}
+Total numnber of students and mentors: ${totalNumnberOfStudentsAndMentors}`);
