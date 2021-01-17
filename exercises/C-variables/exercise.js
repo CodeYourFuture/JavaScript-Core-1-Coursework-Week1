d524e42975b30333b138681db4318f60d81c951a
Kara Howard <kara.mh191@gmail.com>
2021-01-17 17:06:37 +0000

    Completed Ex c Variables
    Completed exercise c Variables

--- exercises/C-variables/exercise.js
+++ exercises/C-variables/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `greeting`
-
-console.log(greeting);
+var greeting = "Welcome to CYF!";
+//* Print your `greeting` to the console 3 times
+for (i = 0; i < 3; i++){
+    console.log(greeting);
+}
