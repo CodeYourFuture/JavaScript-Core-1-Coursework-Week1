3a15ad7c553e7e1d280a7433a65258ec343eefb8
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-01-17 11:57:57 +0200

    I-floats-exercise

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,15 +1,23 @@
-var numberOfStudents = 15;
-var numberOfMentors = 8;
+// var numberOfStudents = 15;
+// var numberOfMentors = 8;
+// var total = numberOfStudents + numberOfMentors;
+
+// var convertStudent = numberOfStudents/total*100;
+// var convertMentors = numberOfMentors/total*100;
+
+// var percentageOfStudents = Math.round(convertStudent);
+// var percentageOfMentors = Math.round(convertMentors);
+
+// console.log(`Student percentage: ${percentageOfStudents}%`);
+// console.log(`Student percentage: ${percentageOfMentors}%`);
+
+var numberOfStudents = 30;
+var numberOfMentors = 10;
 var total = numberOfStudents + numberOfMentors;
 
-var convertStudent = numberOfStudents/total*100;
-var convertMentors = numberOfMentors/total*100;
+var convertStudents = numberOfStudents / total * 100;
+var convertMentors = numberOfMentors / total * 100;
 
-var percentageOfStudents = Math.round(convertStudent);
-var percentageOfMentors = Math.round(convertMentors);
-
-console.log(`Student percentage: ${percentageOfStudents}%`);
-console.log(`Student percentage: ${percentageOfMentors}%`);
-
-
+console.log(`Student percentage: ${convertStudents}%`);
+console.log(`Mentor percentage: ${convertMentors}%`);
 
