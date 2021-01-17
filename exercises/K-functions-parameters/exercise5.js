85c3407b7b36b13431bd80366450e09fa87efb3c
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-01-17 13:59:31 +0200

    K-functions-parameters-exercise5

--- exercises/K-functions-parameters/exercise5.js
+++ exercises/K-functions-parameters/exercise5.js
@@ -1,7 +1,16 @@
-function createLongGreeting(name,age){
-    return`Hello, my name is ${name} and I'm ${age} years old`
-}// Declare your function here
+// function createLongGreeting(name,age){
+//     return`Hello, my name is ${name} and I'm ${age} years old`
+// }// Declare your function here
 
-const greeting = createLongGreeting("Daniel", 30);
+// const greeting = createLongGreeting("Daniel", 30);
+
+// console.log(greeting);
+
+// My copy.
+function nameAgeGreeting(yourName, yourAge) {
+    return `My name is ${yourName} and I'm ${yourAge} years old.`;
+}
+
+let greeting = nameAgeGreeting("Douglas", 47);
 
 console.log(greeting);
