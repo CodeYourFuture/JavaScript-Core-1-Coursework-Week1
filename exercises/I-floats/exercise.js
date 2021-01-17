0bb67e1ddb9b4f1612d0465c1aa83ff6726be895
Kara Howard <kara.mh191@gmail.com>
2021-01-17 18:11:53 +0000

    Completed Ex i Floats
    Completed Exercise i Floats

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,9 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+//* Using the variables provided in the exercise calculate the percentage of mentors and students in the group
+let total = numberOfStudents + numberOfMentors;
+let percentageStudents = Math.round((numberOfStudents / total) * 100);
+let percentageMentors = Math.round((numberOfMentors / total) * 100);
+console.log(`Percentage students: ${percentageStudents}%`);
+console.log(`Percentage mentors: ${percentageMentors}%`);
