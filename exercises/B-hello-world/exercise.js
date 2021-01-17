9aac1968e8c5069c25f4e11a42f233c63f610a44
Kara Howard <kara.mh191@gmail.com>
2021-01-17 17:02:43 +0000

    Completed Ex b hello world
    Completed the exercises in b

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,15 @@
 console.log("Hello world");
+
+//* Try to `console.log()` something different. For example, 'Hello World. I just started learning JavaScript!'.
+console.log('Hello World. I just started learning JavaScript!');
+
+// * Try to console.log() several things at once.
+let greeting = "Hello";
+let firstName = "Kara";
+console.log(`${greeting} ${firstName}`);
+
+//* What happens when you get rid of the quote marks?
+//console.log(Hello, World!);    SyntaxError: missing ) after argument list
+
+//* What happens when you console.log() just a number without quotes?
+console.log(12345);
