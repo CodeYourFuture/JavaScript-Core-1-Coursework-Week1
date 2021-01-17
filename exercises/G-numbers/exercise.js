8ed0f2fe2211bc47ec27079217b6ed60a58e17f8
Kara Howard <kara.mh191@gmail.com>
2021-01-17 18:04:15 +0000

    Completed Ex g Numbers
    Completed Exercise G Numbers

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,7 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+//* Create two variables `numberOfStudents` and `numberOfMentors`
+//* Log a message that displays the total number of students and mentors
+let numberOfStudents = 15;
+let numberOfMentors = 8;
+let total = numberOfStudents + numberOfMentors
+console.log(`Number of Students: ${numberOfStudents}\nNumber of Mentors: ${numberOfMentors}\nTotal number of students and mentors: ${total}`);
