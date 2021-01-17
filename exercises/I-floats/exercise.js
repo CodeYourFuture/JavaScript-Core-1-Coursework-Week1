dab5dba296db681c6244e991cadc15aef906ee89
yunusfirat <38798014+yunusfirat@users.noreply.github.com>
2021-01-17 23:53:43 +0000

    Math round is studied

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,9 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var totalStudentsAndMentors = numberOfMentors + numberOfStudents;
+var percentageStudents = Math.round(numberOfStudents * 100 / totalStudentsAndMentors);
+var percentageMentors = Math.round(numberOfMentors * 100 / totalStudentsAndMentors);
+
+console.log(`Percentage students: ${percentageStudents}%
+Percentage mentors: ${percentageMentors}%`);
+
