7ccc74c758c5708186d5547dad4661950fefb7ca
yunusfirat <38798014+yunusfirat@users.noreply.github.com>
2021-01-17 23:36:15 +0000

    trim() method and toLowerCase method is done

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
-
+let myName = "yunus"
+let nameLength = myName.length;
+let message = `My name is ${myName} and my name is ${nameLength} characters long`
 console.log(message);
--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,4 @@
 const name = " Daniel  ";
-
+let trimName = name.trim();
+let message = `My name is ${trimName} and my name is ${trimName.length} characters long`;
 console.log(message);
