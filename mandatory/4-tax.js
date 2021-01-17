31f3c9a5d8c19530a2402eb9b55d920772ab6a1b
Kara Howard <kara.mh191@gmail.com>
2021-01-17 20:02:52 +0000

    Completed Mandatory Ex 4 Tax
    Completed Mandatory Ex 4 Tax

--- mandatory/4-tax.js
+++ mandatory/4-tax.js
@@ -5,7 +5,10 @@
   Sales tax is 20% of the price of the product
 */
 
-function calculateSalesTax() {}
+function calculateSalesTax(productPrice) {
+  return productPrice * 1.2;
+}
+console.log(calculateSalesTax(15));
 
 /*
   CURRENCY FORMATTING
@@ -17,8 +20,11 @@ function calculateSalesTax() {}
   Remember that the prices must include the sales tax (hint: you already wrote a function for this!)
 */
 
-function addTaxAndFormatCurrency() {}
-
+function addTaxAndFormatCurrency(productPrice) {
+  let totalCost = productPrice * 1.2;
+  return `£${totalCost.toFixed(2)}`; // fixes to 2 decimal places
+}
+console.log(addTaxAndFormatCurrency(15));
 /* 
 ===================================================
 ======= TESTS - DO NOT MODIFY BELOW THIS LINE =====
