b9bfb6e5244e6aa14f959799229fa84e97d66d61
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-01-17 13:34:54 +0200

    K-functions-parameters-exercise2

--- exercises/K-functions-parameters/exercise2.js
+++ exercises/K-functions-parameters/exercise2.js
@@ -1,7 +1,16 @@
-function divide(num1, num2){
-    return num1/num2;
-}// Declare your function first
+// function divide(num1, num2){
+//     return num1/num2;
+// }// Declare your function first
 
-var result = divide(3, 4);
+// var result = divide(3, 4);
 
-console.log(result);
+// console.log(result);
+
+// My copy.
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+let answer = divide(5, 10);
+
+console.log(Math.round(answer));
