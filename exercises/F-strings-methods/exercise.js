ea6236125ffdb4c7777fb20b835d0bae04a7c925
ShawenHarker <ShawenHarker@users.noreply.github.com>
2021-01-17 11:18:27 +0200

    shawen-aldred-harker
    Did string methods

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,10 @@
 // Start by creating a variable `message`
 
+var greeting = "Hello, my name is ";
+var myName = "Shawen Aldred Harker ";
+var middleText = "and it is "
+var myNameLength = myName.length;
+var endMessage = " characters long."
+var message = greeting + myName + middleText + myNameLength + endMessage;
+
 console.log(message);
--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,3 @@
-const name = " Daniel  ";
-
+const name = " Shawen    ";
+var message = "My name is " + name.trim() + " and it is " + name.length + " characters long."
 console.log(message);
