b895d6e179fb58d513340686d07e52cd21a3fc90
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-01-17 16:12:08 +0200

    L-functions-nested-exercise1

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,49 +1,68 @@
-function percentage(num,total) {
-    return (num/total*100);
+// function percentage(num,total) {
+//     return (num/total*100);
+// }
+
+// var numberOfStudents = 15;
+// var numberOfMentors = 8;
+// var total = numberOfStudents + numberOfMentors;
+// var totalPercentage = percentage(numberOfStudents,numberOfMentors,total);
+
+// function initialMessage(numberOfStudents, numberOfMentors){
+//     message = `totalPercentage: ${numberOfStudents} ${numberOfMentors} ${Math.round(percentage)}%`;
+//     return message;
+// }
+
+// console.log(totalPercentage);
+
+function percentage(num, total) {
+    return Math.round((num / total) * 100);
 }
 
-var numberOfStudents = 15;
-var numberOfMentors = 8;
-var total = numberOfStudents + numberOfMentors;
-var totalPercentage = percentage(numberOfStudents,numberOfMentors,total);
+var numberOfStudents = 33;
+var numberOfMentors = 13;
 
-function initialMessage(numberOfStudents, numberOfMentors){
-    message = `totalPercentage: ${numberOfStudents} ${numberOfMentors} ${Math.round(percentage)}%`;
-    return message;
-}
+let total = numberOfStudents + numberOfMentors;
+let totalPercentageStudents = percentage(numberOfStudents, total);
+let totalPercentageMentors = percentage(numberOfMentors, total);
 
-console.log(totalPercentage);
+// function percentageType(catagory, percent) {
+//     // let message1 = `Percentage Students: ${totalPercentageStudents}`;
+//     // let message2 = `Percentage Mentors: ${totalPercentageMentors}`;
+//     return notemessage1, message2;
+// }
 
-
-
-
-
-
-
+console.log(`Percentage Students: ${totalPercentageStudents}`);
+console.log(`Pencentage Mentors: ${totalPercentageMentors}`);
+    
 
 
 
 
 
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
 
-function shoutyGreeting(name){
-    var greeting = name.toUpperCase();
-    return greeting = `HELLO ${greeting}`;
-}
 
-var greeting1 = shoutyGreeting(mentor1);
-var greeting2 = shoutyGreeting(mentor2);
-var greeting3 = shoutyGreeting(mentor3);
-var greeting4 = shoutyGreeting(mentor4);
-var greeting5 = shoutyGreeting(mentor5);
 
-console.log(greeting1);
-console.log(greeting2);
-console.log(greeting3);
-console.log(greeting4);
-console.log(greeting5);
+
+
+// var mentor1 = "Daniel";
+// var mentor2 = "Irina";
+// var mentor3 = "Mimi";
+// var mentor4 = "Rob";
+// var mentor5 = "Yohannes";
+
+// function shoutyGreeting(name){
+//     var greeting = name.toUpperCase();
+//     return greeting = `HELLO ${greeting}`;
+// }
+
+// var greeting1 = shoutyGreeting(mentor1);
+// var greeting2 = shoutyGreeting(mentor2);
+// var greeting3 = shoutyGreeting(mentor3);
+// var greeting4 = shoutyGreeting(mentor4);
+// var greeting5 = shoutyGreeting(mentor5);
+
+// console.log(greeting1);
+// console.log(greeting2);
+// console.log(greeting3);
+// console.log(greeting4);
+// console.log(greeting5);
