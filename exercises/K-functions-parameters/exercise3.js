75d4e88e65e874af15a210decf9b400d71b69f03
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-01-17 13:44:08 +0200

    K-functions-parameters-exercise3

--- exercises/K-functions-parameters/exercise3.js
+++ exercises/K-functions-parameters/exercise3.js
@@ -1,7 +1,15 @@
-function createGreeting(name){
-    return `Hello, my name is ${name}`;
-}// Write your function here
+// function createGreeting(name){
+//     return `Hello, my name is ${name}`;
+// }// Write your function here
 
-var greeting = createGreeting("Daniel");
+// var greeting = createGreeting("Daniel");
+
+// console.log(greeting);
+
+function standardGreeting(firstName) {
+    return `Hello my firstname is ${firstName}`
+}
+
+let greeting = standardGreeting("Douglas");
 
 console.log(greeting);
