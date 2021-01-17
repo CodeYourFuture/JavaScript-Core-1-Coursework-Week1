ac5bbbb4e475cd53830c066543a7e1c4d8504b1e
askinekinci <askinekincihf@gmail.com>
2021-01-17 23:45:16 +0000

    modified exercises F and I

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,3 @@
-const name = (" Askin  ").trim();
+const name = (" Askin  ");
 const nameLength = name.length;
-console.log(`My name is ${name} and my name is ${nameLength} characters long.`);
+console.log(`My name is ${name.trim()} and my name is ${nameLength} characters long.`);
--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,4 +1,8 @@
-var numberOfStudents = 15;
-var numberOfMentors = 8;
-let total = numberOfStudents + numberOfMentors
-console.log(`Percentage students: ${Math.round((numberOfStudents / total) * 100)}%\nPercentage mentors: ${Math.round((numberOfMentors / total) * 100)}%`);
+let numberOfStudents = 15;
+let numberOfMentors = 8;
+let total = numberOfStudents + numberOfMentors;
+let percentageOfStudents = Math.round((numberOfStudents / total) * 100);
+let percentageOfMentors = Math.round((numberOfMentors / total) * 100);
+
+
+console.log(`Percentage students: ${percentageOfStudents}%\nPercentage mentors: ${percentageOfMentors}%`);
