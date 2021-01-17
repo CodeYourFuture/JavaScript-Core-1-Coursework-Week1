88f948444e3a59efc89cee9ac80fb4912d521f6a
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-01-17 10:34:32 +0200

    F-strings-methods
    concatenation with interpolation.

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,4 +1,11 @@
-let name = "Abdulqadir";
-let nameLength = name.length;
+// let name = "Abdulqadir";
+// let nameLength = name.length;
 
-console.log(`My name is ${name} and my name is ${name.length} characters long`);
+// console.log(`My name is ${name} and my name is ${name.length} characters long`);
+
+
+let myName = "Douglas Trainee";
+let myNameLength = myName.length;
+
+// With interpolation, I don't need to create another variable for the greeting text.
+console.log(`My name is ${myName} and my name is ${myNameLength} characters long`);
