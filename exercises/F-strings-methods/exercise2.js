a07a5f425392915aaecaba1cb85b7f1494ee323b
Kara Howard <kara.mh191@gmail.com>
2021-01-17 17:59:29 +0000

    Completed Ex f String Methods
    Completed Exercise F String Methods

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,6 @@
-const name = " Daniel  ";
+//* Log the same message using the variable, `name` provided
+//* Use the `.trim` method to remove the extra whitespace
 
+const name = " Daniel  ";
+let message = `My name is ${name.trim()} and my name is ${name.length} characters long`;
 console.log(message);
