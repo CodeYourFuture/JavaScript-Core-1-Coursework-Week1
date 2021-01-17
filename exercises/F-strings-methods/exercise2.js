566951e6adef8f8b0f8ba81468c5df72db26886b
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-01-17 11:11:49 +0200

    F-strings-methods-exercise2

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -3,3 +3,15 @@ let addTrim = name.trim();
 let newNameLength = addTrim.length;
 
 console.log(`my name is ${addTrim} and my name is ${newNameLength} characters long`);
+
+let myName = "  Douglas Trainee  ";
+let myNameLength = myName.length;
+
+let myNameTrim = myName.trim();
+let myNewNameLength = myNameTrim.length;
+
+// 19 characters for myName
+console.log(`My name is ${myName} and my name is ${myNameLength} characters long`);
+
+// The whitespaces are removed: 15 characters for myName
+console.log(`My name is ${myNameTrim} and my name is ${myNewNameLength} characters long`);
