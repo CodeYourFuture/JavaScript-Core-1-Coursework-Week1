e6de8c0b4708f6502b90e0f60d9244f8a7c83b53
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-01-17 13:49:44 +0200

    K-functions-parameters-exercise4

--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,7 +1,17 @@
-function add(num1, num2) {
-    return num1+num2;
-}// Declare your function first
+// function add(num1, num2) {
+//     return num1+num2;
+// }// Declare your function first
 
-var sum = add(13,124);// Call the function and assign to a variable `sum`
+// var sum = add(13,124);// Call the function and assign to a variable `sum`
+
+// console.log(sum);
+
+// My Copy.
+
+function adding(num1, num2) {
+    return num1 + num2;
+}
+
+let sum = adding(5, 10);
 
 console.log(sum);
