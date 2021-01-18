68517c144d9b66c53d8c7b0671c7506ad6733039
Ionut Tripa <52714188+iTrip23@users.noreply.github.com>
2021-01-18 15:53:01 +0000

    removed extra parenthesis

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -15,7 +15,7 @@ const convertToUSD = pounds => pounds * 1.4;
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-const convertToBRL = pounds => (99*(pounds*5.7)/100);
+const convertToBRL = pounds => 99*(pounds*5.7)/100;
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
