a9c105b9384153cfa0cc97b4822a6916674a342f
JarrodBen <46967203+JarrodBen@users.noreply.github.com>
2021-01-18 13:36:12 +0200

    Did the rest of the exercise folder
    Did the last of the exercises that I had not finished in the 1st commit

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,5 +1,14 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+
+function divide(num1, num2) {
+    return num1/num2 *100
+}
+
+var resultMentors = divide(6, 17);
+var resultStudents = divide(11, 17);
+
+var percentMentors = "Percentage students: " + Math.round(resultMentors) + "%";
+var percentStudents = "Percentage students: " + Math.round(resultStudents) + "%";
+
+console.log(percentMentors);
+console.log(percentStudents);
+
