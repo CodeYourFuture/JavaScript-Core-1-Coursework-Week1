78a212bb8bc52d1f2bbc392ee2fea18b4d5cd678
ShawenHarker <ShawenHarker@users.noreply.github.com>
2021-01-18 20:09:12 +0200

    south-africa_shawen-aldred-harker
    Did the first two exercises of the extra folder.

--- extra/2-piping.js
+++ extra/2-piping.js
@@ -16,26 +16,35 @@
   the final result to the variable goodCode
 */
 
-function add() {
-
+function add(num1, num2) {
+    var sum = num1 + num2;
+    return sum;
 }
 
-function multiply() {
+console.log(add(5, 6));
 
+function multiply(num1, num2) {
+    var sum = num1 * num2;
+    return sum;
 }
 
-function format() {
+console.log(multiply(5, 6));
 
+function format(num1, num2) {
+    var sum = num1 + num2;
+    return "£" + sum;
 }
 
-const startingValue = 2
+console.log(format(11, 5));
+
+const startingValue = 2 + 10;
 
 // Why can this code be seen as bad practice? Comment your answer.
-let badCode = 
+let badCode = "£" + startingValue * 2;
 
 /* BETTER PRACTICE */
 
-let goodCode = 
+let goodCode = "£" + 12 * 2;
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
