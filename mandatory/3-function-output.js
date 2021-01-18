84755e7341a513cf93984390ef2a532effc32036
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-01-18 14:17:19 +0200

    mandatory-3-function-output

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -9,7 +9,7 @@ function getNumber() {
 
 // Add comments to explain what this function does. You're meant to use Google!
 // The concat() method is used to join two or more arrays.
-This method does not change the existing arrays, but returns a new array, containing the values of the joined arrays.
+// This method does not change the existing arrays, but returns a new array, containing the values of the joined arrays.
 function s(w1, w2) {
   return w1.concat(w2);
 }
@@ -21,6 +21,11 @@ function concatenate(firstWord, secondWord, thirdWord) {
   return `${firstWord} ${secondWord} ${thirdWord}`;
 }
 
+//Here, I called the last function.
+let threeWords = concatenate("I", "don't know", "what to do here!");
+
+// Here, I'm checking my output.
+console.log(threeWords);
 
 /* 
 ===================================================
