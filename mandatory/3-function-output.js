13abf9f6b696387a110a739e10bc1f7ac8d80cea
askinekinci <askinekincihf@gmail.com>
2021-01-18 20:02:58 +0000

    completed function outputs.

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,9 +1,11 @@
 // Add comments to explain what this function does. You're meant to use Google!
+// This function creates a random number between 1 and 10.
 function getNumber() {
   return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+// This function combines string into a new string.
 function s(w1, w2) {
   return w1.concat(w2);
 }
@@ -11,6 +13,7 @@ function s(w1, w2) {
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+  return firstWord.concat(" ", secondWord, " ", thirdWord);
 }
 
 /* 
