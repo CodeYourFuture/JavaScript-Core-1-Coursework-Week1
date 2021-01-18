5b89138a4158f6b3bd43c3fe05ecbc8a28452469
yunusfirat <38798014+yunusfirat@users.noreply.github.com>
2021-01-18 00:22:33 +0000

    All test is pased

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -2,15 +2,17 @@
 function getNumber() {
   return Math.random() * 10;
 }
-
+// it is basically picking the number between 1 and 10.
 // Add comments to explain what this function does. You're meant to use Google!
 function s(w1, w2) {
   return w1.concat(w2);
 }
+// the function is concatination w1 and w2  it's like w1 + w2
 
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+   return `${firstWord} ${secondWord} ${thirdWord}`
 }
 
 /* 
