d5de92335f440869c2fb022f7c114557ee7393be
theTrainMan <stormbreaker70@gmail.com>
2021-01-19 20:38:53 +0200

    F-strings-exercise2

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,4 @@
-const name = " Daniel  ";
-name.trim();
+let myName = " Daniel  ";
+let nameLength = myName.length;
+let message = 'My name is ' + myName.trim() + ' and my name is ' + nameLength + ' characters long.'
 console.log(message);
