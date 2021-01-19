64e6d600c007cdea1685f8854a4e199d5176c13f
JarrodBen <46967203+JarrodBen@users.noreply.github.com>
2021-01-19 13:52:12 +0200

    fixed exercise in `L` section of the `exercises` folder
    Just fixed my answer to this exercise.

--- exercises/L-functions-nested/exercise2.js
+++ exercises/L-functions-nested/exercise2.js
@@ -6,8 +6,16 @@ var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
 
-console.log(mentor1)
-console.log(mentor2)
-console.log(mentor3)
-console.log(mentor4)
-console.log(mentor5)
+function upperCaseName(name) {
+    return (name = name.toUpperCase());
+}
+
+function loudGreeting(name) {
+    return "hello ".toUpperCase() + upperCaseName(name);
+}
+
+console.log(loudGreeting(mentor1));
+console.log(loudGreeting(mentor2));
+console.log(loudGreeting(mentor3));
+console.log(loudGreeting(mentor4));
+console.log(loudGreeting(mentor5));
