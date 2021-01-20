e7c5e827b70f43abe7c4916114760fdc9c754386
Lawrence <74542269+LawrenceBaatjies@users.noreply.github.com>
2021-01-20 20:38:39 +0200

    B-Hello world complete
    added 5 console logs with defferent outputs.

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,5 @@
 console.log("Hello world");
+console.log("im learning javaScript")
+console.log("javaScript Is Fun and Hard to Learn");
+console.log("I like to code using JavaScript")
+console.log("Coding is so much fun WHEN YOU UNDERSTAND IS👌")
