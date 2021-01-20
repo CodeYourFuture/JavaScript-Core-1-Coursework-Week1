e0bfd64eff38df68156f9108a39438ef1fb0d86b
Lawrence <74542269+LawrenceBaatjies@users.noreply.github.com>
2021-01-20 20:53:47 +0200

    D -string completed
    added a string

--- exercises/D-strings/exercise.js
+++ exercises/D-strings/exercise.js
@@ -1,3 +1,4 @@
 // Start by creating a variable `message`
-
-console.log(message);
+var message ="This message is to notify you that i am learning Javascript"
+var messageType = typeof message;
+console.log(messageType);
