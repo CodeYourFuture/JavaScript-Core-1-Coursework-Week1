b50fb66710af2f63e1fa972f640d3eaa173a0a19
theTrainMan <stormbreaker70@gmail.com>
2021-01-20 19:08:05 +0200

    Floats

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,11 @@
-var numberOfStudents = 15;
-var numberOfMentors = 8;
+var numberOfStudents = 14.488;
+var roundStudent = Math.round(numberOfStudents);
+var numberOfMentors = 8.655;
+var roundMentor = Math.round(numberOfMentors)
+
+let total = numberOfStudents + numberOfMentors;
+
+
+
+console.log('Percentage students: ' + numberOfStudents / (roundStudent + roundMentor) + '%');
+console.log('Percentage students: ' + numberOfMentors / total + '%');
