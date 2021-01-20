15f6bb236e7a0ee1f87c07a72dda39f82d36a4d8
Andile-coder <61992639+Andile-coder@users.noreply.github.com>
2021-01-20 02:00:42 +0200

    update magic 3 ball

--- extra/3-magic-8-ball.js
+++ extra/3-magic-8-ball.js
@@ -47,8 +47,38 @@
 // and return the answer.
 function shakeBall() {
   console.log("The ball has shaken!");
-  let array = {};
-  return "Dont't count on it";
+  let array1 = [
+    [
+      "It is certain.",
+      "It is decidedly so.",
+      "Without a doubt.",
+      "Yes - definitely.",
+      "You may rely on it.",
+    ],
+    [
+      "As I see it, yes.",
+      "Most likely.",
+      "Outlook good.",
+      "Yes.",
+      "Signs point to yes.",
+    ],
+
+    [
+      " Reply hazy, try again.",
+      "Ask again later.",
+      "Better not tell you now.",
+      "Cannot predict now.",
+      "Concentrate and ask again.",
+    ],
+    [
+      "Don't count on it.",
+      "My reply is no.",
+      " My sources say no.",
+      "Outlook not so good.",
+      "Very doubtful.",
+    ],
+  ];
+  return array1[Math.floor(Math.random() * 3)][Math.floor(Math.random() * 3)];
   //Write your code in here
 }
 
@@ -67,6 +97,10 @@ function checkAnswer(answer) {
     positivity = "very positive";
   } else if (answer === "My reply is no.") {
     positivity = "very negative";
+  } else if (answer === "My reply is no.") {
+    positivity = "positive";
+  } else if (answer === "My reply is no.") {
+    positivity = "negative";
   }
   return positivity;
 }
