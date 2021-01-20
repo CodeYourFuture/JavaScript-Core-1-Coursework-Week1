5c3258202907b470da1020638807574721c628e8
alinosratipour <74776647+alinosratipour@users.noreply.github.com>
2021-01-20 13:38:08 +0000

    finished the tasks and add comment what was wrong

--- mandatory/1-syntax-errors.js
+++ mandatory/1-syntax-errors.js
@@ -1,17 +1,19 @@
 // There are syntax errors in this code - can you fix it to pass the tests?
 
+// you can not return param on its own if you expecting some results.
 function addNumbers(a, b, c) {
   let sum = a + b + c;
     return sum;
 }
 
-//  function introduceMe(Name, age){
-//       let message = "Hello, my name is " + Name + " and I am " + age + " years old ";
-//       return message;
-//  }
+//there were no curly braces and missing concatenation.
+  function introduceMe(Name, age){
+       let message = "Hello, my name is " + Name + " and I am " + age + " years old";
+       return message;
+  }
 
 
-
+// ++ has no meaning also you can not just retune string we are expecting result.
  function getTotal(a, b) {
     let total = a + b;
     let result = total;
@@ -44,5 +46,5 @@ function test(test_name, actual, expected) {
 }
 
 test("fixed addNumbers function - case 1", addNumbers(3, 4, 6), 13);
-//test("fixed introduceMe function", introduceMe("Sonjide", 27), "Hello, my name is Sonjide and I am 27 years old");
+test("fixed introduceMe function", introduceMe("Sonjide", 27), "Hello, my name is Sonjide and I am 27 years old");
 test("fixed getTotal function", getTotal(23, 5), "The total is 28");
