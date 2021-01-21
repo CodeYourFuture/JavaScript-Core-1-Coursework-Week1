9487c5f7db47f9cd28e40c32c847c651b890fa02
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-01-21 21:41:29 +0200

    creat initial function

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,3 +1,18 @@
+// exercise 1
+var students = 8;
+var mentors = 15;
+
+function sum(num1, num2){
+    return num1 + num2;
+}
+
+var percentageOfStudents = Math.round(students/sum(8, 15) * 100);
+var percentageOfMentors = Math.round(mentors/sum(8, 15) * 100);
+console.log(percentageOfStudents + "%");
+console.log(percentageOfMentors + "%");
+
+// exercise2
+
 var mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
