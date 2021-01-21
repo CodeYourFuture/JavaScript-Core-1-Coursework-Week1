fc19d3ca71a9332e02f56f462060a2a31a8eb79f
alinosratipour <74776647+alinosratipour@users.noreply.github.com>
2021-01-21 01:06:44 +0000

    completed hello world task

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,10 @@
 console.log("Hello world");
+
+// added new consol.log
+console.log("This week we started core java scrip course");
+
+// it just output 2; 
+console.log(2);
+
+// It gives error if there is no quote marks
+//console.log(This week we started core java scrip course);
