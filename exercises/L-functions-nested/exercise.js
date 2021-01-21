4a419ca80535be2c79347a1fe711cfd0fa2f6d9f
Jontas77 <68595133+Jontas77@users.noreply.github.com>
2021-01-21 23:07:08 +0200

    Made changes to L-functions exercise

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,21 +1,15 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
 
-function myGreeting (greet) {
-    return greet;
+let total = 15 + 8;
+
+function students (s) {
+    return s / total * 100;
 }
+ function mentors (m) {
+     return m / total * 100;
+ }
 
-var result = myGreeting ('HELLO') + ' ' + mentor1.toUpperCase();
-var result2 = myGreeting ('HELLO') + ' ' + mentor2.toUpperCase();
-var result3 = myGreeting ('HELLO') + ' ' + mentor3.toUpperCase();
-var result4 = myGreeting ('HELLO') + ' ' + mentor4.toUpperCase();
-var result5 = myGreeting ('HELLO') + ' ' + mentor5.toUpperCase();
-console.log(result);
-console.log(result2);
-console.log(result3);
-console.log(result4);
-console.log(result5);
+ let studentPercent = Math.round(students(15)) + '%';
+ let mentorPercent = Math.round(mentors(8)) + '%';
 
+ console.log(`Percentage of students: ${studentPercent}`);
+ console.log(`Percentage of mentors: ${mentorPercent}`);
