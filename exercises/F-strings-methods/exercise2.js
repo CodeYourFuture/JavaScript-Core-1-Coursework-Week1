c23c3a9079e5e5e01cd5613691d8c42978030809
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-01-21 15:25:05 +0200

    completed exercise F

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,5 @@
 const name = " Daniel  ";
+greeting = "Hi, my name is " + name.trim() + " and it is " + name.length + " characters long.";
+// message = greeting.trim();
 
-console.log(message);
+console.log(greeting);
