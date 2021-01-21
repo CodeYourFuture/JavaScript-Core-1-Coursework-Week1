044096f6d04aed4626d59a7bcfdd34c072d0e263
rfloresram <74856999+rfloresram@users.noreply.github.com>
2021-01-21 23:19:26 +0100

    Excercises A-I

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,4 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+let totalOf = numberOfStudents + numberOfMentors
+console.log(`Percentage students: ${Math.round((numberOfStudents / totalOf) * 100)}%\nPercentage mentors: ${Math.round((numberOfMentors / totalOf) * 100)}%`);
