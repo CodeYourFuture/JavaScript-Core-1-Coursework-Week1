caabe3f2d6241ea3a9643db22cb8deeabca527e6
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-01-21 08:48:21 +0200

    Complete first two exercises

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,4 @@
-console.log("Hello world");
+console.log("Hello world" + " I am enjoying the journey into JavaScript and taking the challenge on head-first!");
+console.log("This is a interesting exercise.")
+// console.log(What happens now?)
+console.log(21)
