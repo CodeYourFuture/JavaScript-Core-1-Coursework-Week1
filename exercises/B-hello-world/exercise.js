6035453216485e15ce0db4d9aff9a6cb110b2667
RodrigueNdzana <54138415+RodrigueNdzana@users.noreply.github.com>
2021-01-21 18:27:03 +0200

    B-hello-world done

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,7 @@
 console.log("Hello world");
+console.log("Rodrigue Ndzana")
+console.log("123")
+// if I get rid of the quote marks, i am having a reference error of Rodrigue not found.
+// Because the computer are reading as a variable that has not been declared but used.
+console.log(Rodrigue)
+console.log(123); //the number is display in the console, because the computer read it a a number
