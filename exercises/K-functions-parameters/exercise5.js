e2e479d4d534645a3eed2c0b81586e430dd019ec
omarsharifgr <75176327+omarsharif-gr@users.noreply.github>
2021-01-22 09:22:26 +0000

    Used template literals
    Made use of template literals to improve my code and make it easily readable.

--- exercises/K-functions-parameters/exercise5.js
+++ exercises/K-functions-parameters/exercise5.js
@@ -1,6 +1,6 @@
 // Declare your function here
 function createLongGreeting(name1, age) {
-    return "Hello, my name is " + name1 + " and I'm " + age + " years old.";  
+    return `Hello, my name is ${name1} and I'm ${age} years old.`;  
 }
 const greeting = createLongGreeting("Daniel", 30);
 
