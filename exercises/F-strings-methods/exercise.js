6e2fe00423f1c8e83f6d05a2d8933b16e193e2b9
Alexander Msumba <msumba83@gmail.com>
2021-01-22 17:13:23 +0200

    done f-string 1 and 2

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,7 @@
 // Start by creating a variable `message`
+var name = "Alexander";
+var nameLength = name.length;
+
+var message = "My name is " + name + " and my name is " + nameLength + " characters long"
 
 console.log(message);
