6ac74a44306425d591a14f6e723491e651d33d9c
alinosratipour <74776647+alinosratipour@users.noreply.github.com>
2021-01-22 01:48:48 +0000

    Task K exercise 3 finished

--- exercises/K-functions-parameters/exercise2.js
+++ exercises/K-functions-parameters/exercise2.js
@@ -1,8 +1,8 @@
 // Declare your function first
 function divide(a, b){
     //assign variable 
-    var sum = b/ a;
-    return Math.round(sum);
+    var sum = a/ b;
+    return sum;
 }
 
 var result = divide(3, 4);
--- exercises/K-functions-parameters/exercise3.js
+++ exercises/K-functions-parameters/exercise3.js
@@ -1,5 +1,8 @@
 // Write your function here
+function createGreeting(str){
+    var msg = "Hello my name is " + str;
+    return msg;
+}
 
 var greeting = createGreeting("Daniel");
-
 console.log(greeting);
