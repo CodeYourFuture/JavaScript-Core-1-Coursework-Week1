756c0ea2b2ddb932a1947398e13809365bbc52e6
Alexander Msumba <msumba83@gmail.com>
2021-01-22 10:25:18 +0200

    done string concatanation

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
-
-console.log(message);
+var greetingStart = "hello my name is: ";
+var name = "ALEXANDER MSUMBA";
+var greetings = greetingStart + name;
+console.log(greetings);
