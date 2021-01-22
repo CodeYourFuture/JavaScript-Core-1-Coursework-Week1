fd2a00e2a60caa854ae6993f108cdf2d5e011d43
alinosratipour <74776647+alinosratipour@users.noreply.github.com>
2021-01-22 00:41:24 +0000

    Task I completed successfully

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,9 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+var sum = numberOfStudents + numberOfMentors;
+
+var percentStudent = numberOfStudents / sum * 100;
+var percentMentors = numberOfMentors / sum * 100;
+console.log("Percentage Students: " + Math.round(percentStudent) + "%" + "\nPercentage Mentors: " + Math.round(percentMentors) + "%");
+
