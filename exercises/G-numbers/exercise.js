324bf572274678e6b2218d5568e4c92a87597e29
Lawrence <74542269+LawrenceBaatjies@users.noreply.github.com>
2021-01-22 22:44:49 +0200

    G-numbers complete

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,7 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 45;
+var numberOfMentors = 10;
+var sum = 45 + 10;
+console.log(numberOfStudents);
+console.log(numberOfMentors);
+console.log(sum);
