8df6e06e06b7b805443544db1db3aaf058aa03b8
Lawrence <74542269+LawrenceBaatjies@users.noreply.github.com>
2021-01-22 23:40:32 +0200

    K-function-paremters
    Exercise 1 complete

--- exercises/K-functions-parameters/exercise.js
+++ exercises/K-functions-parameters/exercise.js
@@ -1,9 +1,10 @@
 // Complete the function so that it takes input parameters
 function multiply() {
-  // Calculate the result of the function and return it
+  return 6 * 2;
+  
 }
 
 // Assign the result of calling the function the variable `result`
-var result = multiply(3, 4);
+var result = multiply(6, 2);
 
 console.log(result);
