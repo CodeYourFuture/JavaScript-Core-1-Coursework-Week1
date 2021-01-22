b30dd20d7ef35eef1392c42f48f2c8a4ee06ad2e
RodrigueNdzana <54138415+RodrigueNdzana@users.noreply.github.com>
2021-01-22 17:32:52 +0200

    I-floats
    done

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,6 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+numberOfStudents*=100/23; // for student
+numberOfMentors*=100/23; // for mentor
+console.log(Math.round(numberOfStudents));
+console.log(Math.round(numberOfMentors));
