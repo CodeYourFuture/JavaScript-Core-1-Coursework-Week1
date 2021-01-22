09b99b20704468b21fa04ac511520092ba9e1bef
Domenico <domenico.cyf@gmail.com>
2021-01-22 14:25:26 +0000

    Mandatory 1 and 2

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,5 +1,13 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+var numberOfStudents = 15;
+var numberOfMentors = 8;
+
+function percentage (numberOfStudents,numberOfMentors){
+    var totNum = numberOfMentors + numberOfStudents;
+    var numStudentPerc = numberOfStudents / totNum * 100;
+    var studentPercRound = Math.round(numStudentPerc);
+    var numMentorPerc = numberOfMentors / totNum * 100;
+    var mentorPercRound = Math.round(numMentorPerc);
+}
+function Mentors
+console.log("The Percentage of Mentors is: ", mentorPercRound);
+console.log("The percentage of Students is: ",studentPercRound);
