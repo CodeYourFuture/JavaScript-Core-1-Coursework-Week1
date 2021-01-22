430dfc169d04b94863191333fde26c2974c05e1a
alinosratipour <74776647+alinosratipour@users.noreply.github.com>
2021-01-22 03:25:11 +0000

    last Task completed first function

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,29 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+
+
+
+function calc(numberOfStudents,numberOfMentors){
+    var sum = numberOfStudents + numberOfMentors;
+    var percentStudent = "" + Math.round(numberOfStudents / sum * 100)+ "%"+ "\n" +  Math.round(numberOfMentors / sum * 100  )+"%" ; 
+    return percentStudent;
+
+}
+
+
+function message(m,numberOfStudents,numberOfMentors){
+     const per = calc(numberOfStudents,numberOfMentors);
+      const ms = `my name is ${personName} and i born ${ageInDays}`;
+}
+
+var g = calc(15,8);
+console.log(g);
+
+
+
+
+
+//console.log("Percentage Students: " + Math.round(percentStudent) + "%" + "\nPercentage Mentors: " + Math.round(percentMentors) + "%");
+
