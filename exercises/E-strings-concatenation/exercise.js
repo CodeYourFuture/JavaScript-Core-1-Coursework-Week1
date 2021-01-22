2ecd7fe199bbfd3fb2435303ad14439355820e24
Oksana Kostenko <kseniavlad31@gmail.com>
2021-01-22 15:55:20 +0000

    finishes extra/1, adds semicolon in exercises/E

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,5 +1,5 @@
 // Start by creating a variable `message`
 const myName = "Oksana Kostenko";
-const greeting = "Hello! My name is "
-const  message = greeting + myName;
+const greeting = "Hello! My name is ";
+const message = greeting + myName;
 console.log(message);
