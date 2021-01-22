5f010cd6b440ec60d34a04feffcad28cac906bd1
Domenico <domenico.cyf@gmail.com>
2021-01-22 13:54:39 +0000

    Finished all k exercises

--- exercises/K-functions-parameters/exercise5.js
+++ exercises/K-functions-parameters/exercise5.js
@@ -1,5 +1,7 @@
 // Declare your function here
-
+function createLongGreeting (fName, age){
+    return "Hello, my name is " + fName + " and I'm " + age + " years old";
+}
 const greeting = createLongGreeting("Daniel", 30);
 
 console.log(greeting);
