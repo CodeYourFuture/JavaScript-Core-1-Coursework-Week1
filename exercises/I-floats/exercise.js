876c027389f22a1f740e67c497bb341bbf05eef1
Lawrence <74542269+LawrenceBaatjies@users.noreply.github.com>
2021-01-22 23:02:09 +0200

    I-Floats completed

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,7 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var Percentages = Math.round(numberOfStudents);
+var Percentages = Math.round(numberOfMentors);
+
+console.log(numberOfStudents);
+console.log(numberOfMentors);
