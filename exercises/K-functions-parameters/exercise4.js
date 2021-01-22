04db47544c15cc122dab42b6d8011909b768b9c5
alinosratipour <74776647+alinosratipour@users.noreply.github.com>
2021-01-22 01:54:01 +0000

    Task K exercise 4 completed

--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,5 +1,8 @@
 // Declare your function first
-
-// Call the function and assign to a variable `sum`
-
-console.log(sum);
+function addUp(num1,num2){
+    var add = num1 + num2;
+    return add;
+}
+    // Call the function and assign to a variable `sum`
+    var sum = addUp(13,124);
+    console.log(sum);
