d14e76cd9991de04d1ad1f938eaec7af88a9ec25
AitkenFortuin <Aitkenf115@gmail.com>
2021-01-23 00:14:11 +0200

    Updated Function Output

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,12 +1,16 @@
 // Add comments to explain what this function does. You're meant to use Google!
-
-
+/*The Math.random() function returns a floating-point, pseudo-random number 
+in the range 0 to less than 1 (inclusive of 0, but not 1) with approximately uniform
+ distribution over that range — which you can then scale to your desired range. 
+*/
 function getNumber() {
   return Math.random() * 10;
 } 
 
 
 // Add comments to explain what this function does. You're meant to use Google!
+
+//The concat() method concatenates the string arguments to the calling string and returns a new string.
 function s(w1, w2) {
   return w1.concat(w2);
 }
@@ -14,6 +18,7 @@ function s(w1, w2) {
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+  return firstWord + ' ' + secondWord + ' ' + thirdWord
 }
 
 /* 
