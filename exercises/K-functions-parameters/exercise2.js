bd90917b91bb576210c6a012a6456d2ac6704469
alinosratipour <74776647+alinosratipour@users.noreply.github.com>
2021-01-22 01:31:19 +0000

    task K exersize 2 finished

--- exercises/K-functions-parameters/exercise2.js
+++ exercises/K-functions-parameters/exercise2.js
@@ -1,5 +1,9 @@
 // Declare your function first
+function divide(a, b){
+    //assign variable 
+    var sum = b/ a;
+    return Math.round(sum);
+}
 
 var result = divide(3, 4);
-
-console.log(result);
+console.log("Result is:" + result);
