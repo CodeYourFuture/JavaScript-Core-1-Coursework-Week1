58803b31cba8a64513e183eebf5d2f2becd872b3
RodrigueNdzana <54138415+RodrigueNdzana@users.noreply.github.com>
2021-01-22 18:33:37 +0200

    L-functions-nestedand some updates on float
    done the whole exercise

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -2,5 +2,5 @@ var numberOfStudents = 15;
 var numberOfMentors = 8;
 numberOfStudents*=100/23; // for student
 numberOfMentors*=100/23; // for mentor
-console.log(Math.round(numberOfStudents));
-console.log(Math.round(numberOfMentors));
+console.log(Math.round(numberOfStudents)+"%");
+console.log(Math.round(numberOfMentors)+"%");
--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,15 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function namesInUpperCase(names){
+    return names.toUpperCase();
+}
+function shoutyGreeting(names){
+    return "hello".toUpperCase() +" "+ namesInUpperCase(names);
+}
+console.log(shoutyGreeting(mentor1));
+console.log(shoutyGreeting(mentor2));
+console.log(shoutyGreeting(mentor3));
+console.log(shoutyGreeting(mentor4));
+console.log(shoutyGreeting(mentor5));
