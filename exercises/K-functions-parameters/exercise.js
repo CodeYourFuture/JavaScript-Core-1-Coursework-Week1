6fbd7a7509a2c4e5313aa0f00bbc97a4b14fece0
theTrainMan <stormbreaker70@gmail.com>
2021-01-22 16:32:23 +0200

    functions - K parameters

--- exercises/K-functions-parameters/exercise.js
+++ exercises/K-functions-parameters/exercise.js
@@ -1,9 +1,38 @@
 // Complete the function so that it takes input parameters
-function multiply() {
-  // Calculate the result of the function and return it
+function multiply(num1, num2) {
+    // Calculate the result of   the function and return it
+    return num1 * num2
 }
-
 // Assign the result of calling the function the variable `result`
 var result = multiply(3, 4);
 
 console.log(result);
+
+// Exercise 2
+function divide(num3, num4) {
+    return num3 / num4;
+}
+var results = divide(3, 4)
+console.log(results);
+
+//Exercise 3
+function string(str) {
+    return str;
+}
+var strResult = string('Hello my name is Daniel')
+console.log(strResult);
+
+//Exercise 4
+function add(num5, num6) {
+    return num5 + num6;
+}
+var sum = add(13, 124);
+console.log(sum);
+
+//Exercise 5
+function sentence(num) {
+    return num;
+}
+
+var outcome = sentence('Hello my name is Daniel and I am', 30)
+console.log(outcome);
