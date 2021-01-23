e050ecc6b5077d43b645c708bb0f83b8183b4e0c
soraiya <soraiyamm@gmail.com>
2021-01-23 20:38:37 +0000

    all exercises complete

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,7 @@
-const name = " Daniel  ";
-
-console.log(message);
+const name1 = " Daniel  ";   //'name' changed to 'name1' as it becomes void as a variable when I try to add .length to it
+var nameLength = name1.trim();
+// console.log(nameLength.length);
+var message = "My name is "
+var messageMid = " and my name is "
+var messageEnd = " characters long"
+console.log(message + name1.trim() + messageMid + nameLength.length + messageEnd);
--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,7 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+var percentageOfStudents = Math.round(numberOfStudents / 23 * 100) + "%";
+var percentageOfMentors = Math.round(numberOfMentors / 23 * 100) + "%";
+console.log("Percentage of students: " + percentageOfStudents);
+console.log("Percentage of mentors: " + percentageOfMentors);
