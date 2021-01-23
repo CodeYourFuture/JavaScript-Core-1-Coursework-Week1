0556884deb834f3ab9c1442ef2b87667a7b726ed
Adeline <bokaadeline@gmail.com>
2021-01-23 10:47:58 +0200

    added floats,sting methods and function parameters

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,4 @@
 // Start by creating a variable `message`
-
+const name= "Adeline";
+let message=`My name is Adeline and my name is ${name.length} character log`;
 console.log(message);
--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,5 +1,8 @@
 // Declare your function first
-
+function addNumber(num1,num2){
+    return num1 + num2;
+}
 // Call the function and assign to a variable `sum`
+let sum=addNumber('13',1+2+4);
 
 console.log(sum);
