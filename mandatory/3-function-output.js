ac5b3c4f8088f73869f1caaca7125bffc67b1875
soraiya <soraiyamm@gmail.com>
2021-01-23 23:23:11 +0000

    mandatory complete

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,16 +1,17 @@
 // Add comments to explain what this function does. You're meant to use Google!
 function getNumber() {
   return Math.random() * 10;
-}
+} // this function turns a random decimal/float number into an integer/whole number
 
 // Add comments to explain what this function does. You're meant to use Google!
 function s(w1, w2) {
   return w1.concat(w2);
-}
+} //concatenates two arguments
 
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+  return firstWord.concat(" " + secondWord, " " + thirdWord);
 }
 
 /* 
