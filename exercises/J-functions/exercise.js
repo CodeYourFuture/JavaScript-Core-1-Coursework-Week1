3899a17f4fec4d066afcfff1ada5b8677dd5d576
Samuelkau <72687211+Samuelkau@users.noreply.github.com>
2021-01-25 19:40:27 +0200

    J-function exer 1 + 2

--- exercises/J-functions/exercise.js
+++ exercises/J-functions/exercise.js
@@ -1,7 +1,9 @@
 function halve(number) {
   // complete the function here
+  return number/2;
 }
 
 var result = halve(12);
-
+var result = halve(24);
+var result = halve(101);
 console.log(result);
