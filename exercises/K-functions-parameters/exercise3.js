ca54be2914d46deeda1dff929ffb737de37403eb
Samuelkau <72687211+Samuelkau@users.noreply.github.com>
2021-01-25 21:10:14 +0200

    K-function exer 1-4

--- exercises/K-functions-parameters/exercise3.js
+++ exercises/K-functions-parameters/exercise3.js
@@ -1,4 +1,10 @@
 // Write your function here
+var createGreeting = "Hello, my name is ";
+function greeting (){
+    //var name = "Samuel";
+    //var createGreeting = "Hello, my name is ";
+    return createGreeting;
+}
 
 var greeting = createGreeting("Daniel");
 
--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,5 +1,8 @@
 // Declare your function first
+function sum(numb1, numb2){
+    return numb1 + numb2;
 
+}
 // Call the function and assign to a variable `sum`
-
-console.log(sum);
+sum(13, 124)
+//console.log(sum);
