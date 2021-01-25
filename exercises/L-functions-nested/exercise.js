aaa60c2836443407b4e7bc85ee65558bd24b1429
Lawrence <74542269+LawrenceBaatjies@users.noreply.github.com>
2021-01-25 21:08:08 +0200

    L-function completed

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -2,4 +2,13 @@ var mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+// var mentor5 = "Yohannes";
+
+function percentageCalculator(people,num, total){ 
+    let percent=(num/total)*100;
+function message(){
+   return "percentage " +people+ ": "+percent+ "%";
+}
+return message();
+}
+console.log(percentageCalculator("mentors",12,16));
