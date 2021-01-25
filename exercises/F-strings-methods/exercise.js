d9ae2a945ac8db712e6232d13b706a519af8a75f
Samuelkau <72687211+Samuelkau@users.noreply.github.com>
2021-01-25 18:59:07 +0200

    f-strings exer 1

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
+let name = "Samuel";
+let nameLength= name.length;
+let message = "My name is Daniel and my name is " + nameLength + " characters long";
 
 console.log(message);
