55c6b30d955a5a0f21566487938c848b5e01ad91
Samuelkau <72687211+Samuelkau@users.noreply.github.com>
2021-01-25 19:31:14 +0200

    exer G + I

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,8 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 30;
+var numberOfMentors = 15;
+var totalNumber = numberOfStudents + numberOfMentors;
+var totalNumber2 = 30 + 15;
+console.log(totalNumber);
+console.log(totalNumber2);
+
--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,8 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+var numberOfStudentsPerc = 15/23*100;
+var numberOfMentorsPerc = 8/23*100;
+
+console.log(numberOfStudentsPerc);
+console.log(numberOfMentorsPerc);
