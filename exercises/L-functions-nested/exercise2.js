f19cde2dd9f4027422137ff85a09dd7aa93e4dc7
sinopungula <74741624+sinopungula@users.noreply.github.com>
2021-01-26 14:24:15 +0200

    updated exercise 2.js in L-functions
    Tried to improve my functions

--- exercises/L-functions-nested/exercise2.js
+++ exercises/L-functions-nested/exercise2.js
@@ -1,30 +1,16 @@
- 
-// function percentageNumber(numOfPeople, totalNumOfPeople) {
-//     return numOfPeople * 100 / totalNumOfPeople;
-// }
-// var numOfStudents = percentageNumber(15, 23);
-// var numOfMentors = percentageNumber(8, 23);
 
-// function message(a, b) {
-//     return `Percentage of ${a} = ${b}`;
-// }
-
-// message('Students ', Math.round(numOfStudents));
-// console.log(message);
-
-// message('Mentors ', Math.round(numOfMentors));
-// console.log(message);
 
 function percentage(numOfPeople, totalNumOfPeople) {
     return Math.round((numOfPeople * 100 / totalNumOfPeople));
 }
 
+
 var percentageStudents = percentage(15,23);
 var percentageMentors = percentage(8, 23);
 
 function message() {
-    return(`Percentage Students: ${percentageStudents}
-    Percentage Mentors: ${percentageMentors}`);
+     return(`Percentage Students: ${percentageStudents}
+Percentage Mentors: ${percentageMentors}`);    
 }
 
-console.log(message);
+console.log(message());
