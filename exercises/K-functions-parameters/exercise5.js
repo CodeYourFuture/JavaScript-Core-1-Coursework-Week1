2515986e460f1e0baab724e6373f3dce3748b84e
Lawrence <74542269+LawrenceBaatjies@users.noreply.github.com>
2021-01-29 22:18:49 +0200

    K-function-paramenters complete
    Exercise 5 completed

--- exercises/K-functions-parameters/exercise5.js
+++ exercises/K-functions-parameters/exercise5.js
@@ -1,5 +1,8 @@
 // Declare your function here
-
 const greeting = createLongGreeting("Daniel", 30);
 
-console.log(greeting);
+    function createLongGreeting(name, age) {
+         return `Hello, my name is ${name} and I'm ${age} years old`
+}
+
+    console.log(greeting);
