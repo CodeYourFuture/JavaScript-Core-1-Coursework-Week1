d4909926b03931347815cee9a80b33106cbd4c6f
Ryno <43918267+RynoFourieZA@users.noreply.github.com>
2021-01-29 22:59:06 +0200

    mandatory
    syntax pass

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,6 +1,29 @@
-function total 
+
+
+function message(createMessage) {
+   console.log("Percentage student: ");
+}
+
+function percentage(x, y) {
+return   Math.round((x / y) * 100);
+
+}
+
+var totalStudents = percentage(15, 23); 
+var totalMentors = percentage(8, 23)
+message()
+console.log(message + totalStudents);
+console.log(message + totalMentors);  
+
+
 var mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function upperCase() {
+    return (mentor1, mentor2,mentor3).toUpperCase();
+}
+mentor = upperCase();
+console.log(mentor);
