333ac24dc83ab170653413c76729009c76fca563
remidoukmak <73621087+remidoukmak@users.noreply.github.com>
2021-03-04 16:06:27 +0000

    Add files via upload

---
+++ exercises/exercise3.js
@@ -0,0 +1,12 @@
+// Write your function here
+function createGreeting(name) {
+  let newGreeting = "Hi " + name ;
+  return newGreeting;
+}
+let greeting = createGreeting("Daniel");
+
+
+console.log(greeting);
+
+let greeting1 = createGreeting("Reem");
+console.log(greeting1);
---
+++ exercises/exercise4.js
@@ -0,0 +1,9 @@
+// Declare your function first
+function sum(a,b) {
+  let val = a + b;
+  return val;
+}
+
+// Call the function and assign to a variable `sum`
+let result = sum(13, 124);
+console.log(result);
