44d5187846da816368ac6679c314b09677f15486
chandredewet <chandredewet@gmail.com>
2021-03-14 23:46:56 +0200

    F  Completed

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,13 @@
 const name = " Daniel  ";
+var trimmedName = name.trim();
+var myNameLength = trimmedName.length;
+var message =
+  "My name is " +
+  trimmedName +
+  " and my name is " +
+  myNameLength +
+  " characters long.";
 
 console.log(message);
+
+//console.log(message);
