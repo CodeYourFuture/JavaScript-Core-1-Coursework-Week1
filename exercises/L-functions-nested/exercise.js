b0497303340e96619010d671fa66640e4daceaa5
chandredewet <chandredewet@gmail.com>
2021-03-17 13:24:49 +0200

    Completed Exercises

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,84 +1,27 @@
-//exercise 1
+let numberOfStudents = 15;
+let numberOfMentors = 8;
 
-let studs;
-let mentors;
+//calculate percentage
+function calculatePercentage(numPersons, totalGroup) {
+  let totalPerc = (numPersons / totalGroup) * 100;
 
-function calculatePercentage() {
-var totGroup = studs + mentors;
-var percentageStuds = (numberOfStudents / totalGroup) * 100;
-var percentageMents = (numberOfMentors / totalGroup) * 100;
-
+  return totalPerc;
 }
 
-function FindPercentage(studs, mentors) {
-  
-  "The percentage of Students are " + Math.round(calculatePercentage(studs)) + "%"; 
-  "The percentage of Students are " + Math.round(calculatePercentage(mentors)) + "%"; 
-    
+//create display percentage
+
+function displayPercentage() {
+  let totalGroup = numberOfStudents + numberOfMentors;
+  console.log(
+    "Percentage students: " +
+      Math.round(calculatePercentage(numberOfStudents, totalGroup)) +
+      "%"
+  );
+  console.log(
+    "Percentage mentors: " +
+      Math.round(calculatePercentage(numberOfMentors, totalGroup)) +
+      "%"
+  );
 }
 
-
-
-console.log(
-  "The percentage of Students are " + Math.round(percentageStuds) + "%"
-);
-console.log(
-  "The percentage of Mentors are " + Math.round(percentageMents) + "%"
-);
-
-
-
-
-- The percentage should be rounded to the nearest whole number (use a search engine to find out how to this with JavaScript)
-- You should have one function that calculates the percentage, and one function that creates a message
-
-> Consider: should your percentage function do the rounding, or should it be done when the greeting is created?
-
-
-var numberOfStudents = 15;
-var numberOfMentors = 8;
-
-var totalGroup = numberOfMentors + numberOfStudents;
-
-var percentageStuds = (numberOfStudents / totalGroup) * 100;
-var percentageMents = (numberOfMentors / totalGroup) * 100;
-
-console.log(
-  "The percentage of Students are " + Math.round(percentageStuds) + "%"
-);
-console.log(
-  "The percentage of Mentors are " + Math.round(percentageMents) + "%"
-);
-
-
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
-
- In `exercise.js` write a program that displays the percentage of students and mentors in the group
-- The percentage should be rounded to the nearest whole number (use a search engine to find out how to this with JavaScript)
-- You should have one function that calculates the percentage, and one function that creates a message
-
-> Consider: should your percentage function do the rounding, or should it be done when the greeting is created?
-
-## Expected result
-
-var numberOfStudents = 15;
-var numberOfMentors = 8;
-
-#calculate percentage
-
-#create message
-
-function displayPercentage{
-  console.log("Percentage students: " + Math.round(calculatePercentage()) +"%" )
-
-}
-
-
-```
-Percentage students: 65%
-Percentage mentors: 35%
-```
+displayPercentage();
--- exercises/L-functions-nested/exercise2.js
+++ exercises/L-functions-nested/exercise2.js
@@ -3,3 +3,21 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+//function that spells their name in uppercase
+
+function makeUpper(mentor) {
+  return mentor.toUpperCase();
+}
+
+//function that creates a shouty greeting.
+
+function greetMentor(mentor) {
+  console.log("Hey! Hey! " + makeUpper(mentor));
+}
+
+greetMentor(mentor1);
+greetMentor(mentor2);
+greetMentor(mentor3);
+greetMentor(mentor4);
+greetMentor(mentor5);
