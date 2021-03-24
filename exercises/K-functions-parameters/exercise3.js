2ca84cbf3be3f47b70dfced633c4b7b5ddd5ae8c
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2021-03-24 22:46:35 +0200

    Update exercise3.js
    updated -K-functions-parameters

--- exercises/K-functions-parameters/exercise3.js
+++ exercises/K-functions-parameters/exercise3.js
@@ -3,7 +3,7 @@ function createGreeting(name){
     return `Hello , my name is ${name}`;
 }
 
-var greeting = createGreeting("Daniel");
+let greeting = createGreeting("Daniel");
 
 
 console.log(greeting);
