fca6a2d6e37fb34f3e3dce395648630363c9a12e
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2021-03-24 22:13:31 +0200

    update-J-Functions
    Exercise-1

--- exercises/J-functions/exercise.js
+++ exercises/J-functions/exercise.js
@@ -1,8 +1,12 @@
 function halve(number) {
-  return number - 6;
+  return number / 6;
   
 }
 
-var result = halve(12);
+let result1 = halve(12);
+let result2 = halve(6);
+let result3 = halve(18);
 
-console.log(result);
+  console.log(result1);
+  console.log(result2);
+  console.log(result3);
