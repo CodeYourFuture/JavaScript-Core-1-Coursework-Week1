97d4b5a3dc6a8711b47c8617eead3d7e32022083
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2021-03-24 22:19:23 +0200

    updated-1-Syntax-errors
    fixed introduceMe function: PASSED

--- mandatory/1-syntax-errors.js
+++ mandatory/1-syntax-errors.js
@@ -5,7 +5,7 @@
     }
 
     function introduceMe(name, age){
-        return "Hello, my name is " + name + "and I am" + age + "years old";
+        return `Hello, my name is ${name} and I am ${age} years old`;
     }  
 
     function getTotal(a, b) {
