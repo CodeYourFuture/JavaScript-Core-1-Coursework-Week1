41d1a0b00d01e1b46ada97125d606cf4fe229db8
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2021-03-24 22:15:41 +0200

    updated-J-Fuctions
    Exercise-2 updated.

--- exercises/J-functions/exercise2.js
+++ exercises/J-functions/exercise2.js
@@ -1,10 +1,14 @@
 
-var triple =  function triple(number) {
+let triple =  function triple(number) {
     return number * 3;
     
 }
 
-    var result = triple(12);
+    let result1 = triple(12);
+    let result2 = triple(22);
+    let result3 = triple(32);
 
-console.log(result);
+    console.log(result1);
+    console.log(result2);
+    console.log(result3);
 
