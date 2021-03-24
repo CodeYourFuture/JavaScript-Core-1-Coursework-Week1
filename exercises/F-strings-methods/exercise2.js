b871d66c63aa8d09e9d5a1ca0d95bfb592ee8203
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2021-03-24 22:03:55 +0200

    updated-F-String-Methods
    update Exercise-2

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,7 +1,7 @@
-var name = "  My Name is Daniel!   ";
-var nameLength = name.length;
-
-console.log(name.trim(), nameLength);
+let name = " Daniel!   ";
+    greeting = "Hi, my name is " + name.trim() + " and it is " + name.length + " characters long.";
+   
+console.log(greeting);
 
 
 
