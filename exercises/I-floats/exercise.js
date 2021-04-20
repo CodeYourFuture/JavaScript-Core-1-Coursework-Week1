94cec22db4a8ae5724bcada02823b444e7dee5b6
remidoukmak <73621087+remidoukmak@users.noreply.github.com>
2021-04-20 14:19:25 +0100

    Update exercise.js

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,15 +1,13 @@
+var numberOfStudents = 15;
+var numberOfMentors = 8;
+var sum = numberOfStudents + numberOfMentors;
+
 function percentage(sample, total) {
   let percentage = sample / total;
   percentage = percentage * 100;
   percentage = Math.round(percentage);
   return percentage;
 }
-var numberOfStudents = 15;
-var numberOfMentors = 8;
-var sum = numberOfStudents + numberOfMentors;
-console.log("number of students " + numberOfStudents);
-console.log("number of mentors " + numberOfMentors);
-console.log("sum of students and mentors " + sum);
 
 console.log("percentage students " + percentage(numberOfStudents, sum) + "%");
 console.log("percentage mentors " + percentage(numberOfMentors, sum) + "%");
