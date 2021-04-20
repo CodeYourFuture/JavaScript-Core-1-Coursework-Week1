38921f67cfaeb9aaecc7a3ffcbe41d35bd101379
remidoukmak <73621087+remidoukmak@users.noreply.github.com>
2021-04-20 13:34:55 +0100

    Update exercise.js

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,8 @@
 // Start by creating a variable `message`
 
-console.log(message);
+var greetingStart = "Hello, my name is ";
+var name = "Reem";
+
+var greeting = greetingStart + name;
+
+console.log(greeting);
