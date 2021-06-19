7589865fd0584d1794c4e6de5712c37b75ecce27
musayuksel <79163385+musayuksel@users.noreply.github.com>
2021-06-20 00:34:44 +0100

    Complate exercises

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,14 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+function createUpperCase (name){
+  return name.toUpperCase(name);
+}
+function createShoutyGreeting(name) {
+  return `HELLO ${createUpperCase(name)}`;
+}
+console.log(createShoutyGreeting(mentor1));
+console.log(createShoutyGreeting(mentor2));
+console.log(createShoutyGreeting(mentor3));
+console.log(createShoutyGreeting(mentor4));
+console.log(createShoutyGreeting(mentor5));
