6059d59ec05aa024196e257c60fe33ad76b5f243
Texx45 <tcalderbank@hotmail.com>
2021-06-19 21:35:53 +0100

    Exercise B Hello World completed

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,7 @@
 console.log("Hello world");
+console.log("Hello world. I just started learning JavaScript");
+console.log("It's pretty exciting to be actually starting JavaScript");
+console.log(
+  "What happens if I remove the quotation marks? => (SyntaxError: missing ) after argument list)"
+);
+console.log(45);
