1df031c596b12dcf068f713766dccf4b6b64deff
musayuksel <79163385+musayuksel@users.noreply.github.com>
2021-06-20 00:36:50 +0100

    Complate all exercises

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,8 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+let numberOfStudents = 27;
+let numberOfMentors = 20;
+console.log(
+  `Number of students: ${numberOfStudents}
+Number of mentors: ${numberOfMentors}
+Total number of students and mentors: ${numberOfMentors+numberOfStudents}`
+);
--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,7 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+let totalPeople = numberOfStudents+numberOfMentors;
+console.log(
+`Percentage students: ${Math.round(100*numberOfStudents/totalPeople)}%
+Percentage mentors: ${Math.round(100*numberOfMentors/totalPeople)}%`
+);
