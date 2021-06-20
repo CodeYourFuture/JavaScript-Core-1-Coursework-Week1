db9c8e2b9e2f430e6bb9e9d2220f75a6d581eb87
Dharma Guadeloupe <67554685+DharmaKarmaStar@users.noreply.github.com>
2021-06-20 12:16:46 +0100

    Console logged several things at once

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,4 @@
 console.log("Hello world I just started learning JavaScript");
+console.log("I'm on week 1 of JavaScript Module 1");
+console.log("I'm doing homework on a Sunday instead of streaming");
+console.log("Soft Kitty, Warm kitty, Little ball of fur");
