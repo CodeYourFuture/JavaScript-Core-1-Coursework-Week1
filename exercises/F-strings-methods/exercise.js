f1895bb5cf0d378316cd5638b20bd9bbc888e5f9
Gulnihal <gh.naldoken@gmail.com>
2021-06-20 12:34:43 +0100

    F exercise 1 is done

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,15 @@
 // Start by creating a variable `message`
 
+var firstName = "Gulnihal";
+
+var firstNameLength = firstName.length;
+
+
+console.log(firstNameLength);
+
+var message = "My name is Gulnihal and my name is 8 characters long"
+
 console.log(message);
+
+var noSpaceMessage = message.trim;
+console.log(noSpaceMessage);
