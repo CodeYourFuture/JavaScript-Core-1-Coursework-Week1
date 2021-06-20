8c3d683d7278eba66c964c48b6117073fa077545
Omer Faruk  Yamak <74216179+omerffaruk@users.noreply.github.com>
2021-06-20 20:40:54 +0100

    A-setup-ide and B-hello-world exercises done

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,12 @@
 console.log("Hello world");
+console.log("Hello everyone. I am now trying using console.log with different kind of inputs.");
+console.log(23);
+console.log(("is this(using a nested paranthesis) gonna cause an error?"));
+console.log("My name is", "Omer");
+console.log((("I am now using three paranthesis inside console.log.")));
+
+const name = "omer";
+const hobby  = "coding";
+
+console.log("Hi everyone. My name is", name, ", and my hobby is", hobby);
+console.log("Hi everyone. My name is" + name + "and my hobby is" + hobby);
