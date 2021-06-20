3f900f5d1fdb6cdfcc3ed181d6de3c39d57c6819
Gulnihal <gh.naldoken@gmail.com>
2021-06-20 12:43:58 +0100

    G exercise is done

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,5 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents =30;
+var numberOfMentors = 16;
+var totalNumberOfStudentsAndMentors = numberOfStudents + numberOfMentors;
+console.log(totalNumberOfStudentsAndMentors);
