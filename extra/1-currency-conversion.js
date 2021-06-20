436f6c3ba510f001f4371e25cad6bb599503b27f
Daniel Wagner-Hall <dawagner@gmail.com>
2021-06-20 18:52:48 +0100

    Fix typos

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -1,5 +1,5 @@
 /*
-  CURRENCY FORMATTING
+  CURRENCY CONVERSION
   ===================
   The business is breaking out into a new market and need to convert prices to USD
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
@@ -8,7 +8,7 @@
 function convertToUSD() {}
 
 /*
-  CURRENCY FORMATTING
+  CURRENCY CONVERSION
   ===================
   The business is now breaking into the Brazilian market
   Write a new function for converting to the Brazilian real (exchange rate is 5.7 BRL to £)
@@ -36,6 +36,6 @@ test("convertToBRL function works for £30", () => {
   expect(convertToBRL(30)).toEqual(169.29);
 });
 
-test("convertToBRL function works for £0.99", () => {
+test("convertToBRL function works for £1.50", () => {
   expect(convertToBRL(1.5)).toEqual(8.46);
 });
