5a8f8034cf60f9040de655b024df25cae6899bba
maham511 <maha.malik511@gmail.com>
2021-06-20 20:27:00 +0100

    Passed test on  3-function-output.js and comments added. Task 3 complete

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,16 +1,17 @@
-// Add comments to explain what this function does. You're meant to use Google!
+//My Comment - This function gives a random number by using math.random() and multiplying the value by 10  
 function getRandomNumber() {
   return Math.random() * 10;
 }
 
-// Add comments to explain what this function does. You're meant to use Google!
+
+//My comment - The function combines two words using the concat method.
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
-  // Look at the test case below to understand what this function is expected to return.
+  return firstWord + " " + secondWord + " " + thirdWord;  
 }
 
 /* 
