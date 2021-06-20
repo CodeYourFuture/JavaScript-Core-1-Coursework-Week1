db6e3836752ec312ab58a59bb58e62d694a5b275
Texx45 <tcalderbank@hotmail.com>
2021-06-20 07:12:14 +0100

    Completed exercise K-functions-parameters

--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,5 +1,8 @@
-// Declare your function first
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
 
 // Call the function and assign to a variable `sum`
+const sum = addTwoNumbers(13, 124);
 
 console.log(sum);
