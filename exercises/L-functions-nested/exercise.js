7d26fc70109145dbe16a2263148a901cf7d9f5c1
Gulnihal <gh.naldoken@gmail.com>
2021-06-20 21:28:08 +0100

    L-function exercise done

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,21 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function nameUpperCase(mentor) {
+    return mentor.toUpperCase();
+}
+function greeting(name){
+    let message = "HELLO " + nameUpperCase(name);
+    return message;
+}
+
+console.log(greeting(mentor1));
+
+console.log(greeting(mentor2));
+
+console.log(greeting(mentor3));
+
+console.log(greeting(mentor4));
+
+console.log(greeting(mentor5));
