ce4700a808b83c24d70dc6a8aacef53fac832d7b
Dharma Guadeloupe <67554685+DharmaKarmaStar@users.noreply.github.com>
2021-06-20 16:41:51 +0100

    functions params exercise4

--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,5 +1,7 @@
 // Declare your function first
-
+function sumCalculate(num1, num2) {
+  return num1 + num2;
+}
 // Call the function and assign to a variable `sum`
-
+const sum = sumCalculate(13, 124);
 console.log(sum);
