aeba26b7c509ce2b522ffc53dbefec342673a76b
Dharma Guadeloupe <67554685+DharmaKarmaStar@users.noreply.github.com>
2021-06-20 14:18:54 +0100

    F strings methods exercise 1

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
-
+const name = "Dharma Emmelene";
+const nameLength = name.length;
+const message = `My name has ${nameLength} characters in it.`;
 console.log(message);
