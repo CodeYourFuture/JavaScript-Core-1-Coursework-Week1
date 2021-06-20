219cc5299af04a31005321b5b80eecc30d58e7c8
Texx45 <tcalderbank@hotmail.com>
2021-06-20 05:52:47 +0100

    Completed E-strings-concatenation

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,7 @@
 // Start by creating a variable `message`
 
-console.log(message);
+let greetingStart = "Hello, my name is";
+let name = "Terry";
+
+let greeting = greetingStart + " " + name;
+console.log(greeting);
