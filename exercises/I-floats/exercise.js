52ac76111d1020ec9a731cef516b81b688436d48
Omer Faruk  Yamak <74216179+omerffaruk@users.noreply.github.com>
2021-06-20 21:39:48 +0100

    I-floats exercise done

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,18 @@
-var numberOfStudents = 15;
-var numberOfMentors = 8;
+const numberOfStudents = 15;
+const numberOfMentors = 8;
+const studentsPercentage = Math.round(100 *(numberOfStudents / (numberOfStudents + numberOfMentors)));
+const mentorsPercentage = Math.round(100 *
+  (numberOfMentors / (numberOfStudents + numberOfMentors)));
+
+const message = `Percentage students: ${studentsPercentage}%
+Percentage mentors: ${mentorsPercentage}%`;
+
+const message2 = `Percentage students: ${Math.round(
+  100 * (numberOfStudents / (numberOfStudents + numberOfMentors))
+)}%
+Percentage mentors: ${Math.round(
+  100 * (numberOfMentors / (numberOfStudents + numberOfMentors))
+)}%`;
+
+console.log(message);
+console.log(message2);
