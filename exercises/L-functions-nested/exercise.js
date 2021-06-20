4702f6424b6493cc0e811d429f45c79c0d86ff31
Dharma Guadeloupe <67554685+DharmaKarmaStar@users.noreply.github.com>
2021-06-20 19:57:11 +0100

    functions nested exercise

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,14 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function shoutyGreeting(name) {
+  const greeting = "hello " + name;
+  return greeting.toUpperCase();
+}
+console.log(shoutyGreeting("Daniel"));
+console.log(shoutyGreeting("Irina"));
+console.log(shoutyGreeting("Mimi"));
+console.log(shoutyGreeting("Rob"));
+console.log(shoutyGreeting("Yohannes"));
+
