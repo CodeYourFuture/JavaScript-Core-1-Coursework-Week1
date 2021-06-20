d0e7429a5c7f5080341274472efc65de7218a7dd
Dharma Guadeloupe <67554685+DharmaKarmaStar@users.noreply.github.com>
2021-06-20 13:26:23 +0100

    D-strings exercise with own message

--- exercises/D-strings/exercise.js
+++ exercises/D-strings/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
-
+const message = "This is a string as recognised by it's quotation marks";
 console.log(message);
+const messageType = typeof message;
+console.log(messageType);
