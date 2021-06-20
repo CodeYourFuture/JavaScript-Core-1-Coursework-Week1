7f6a000e5665325e2ce0b9b41408a4158f208d1e
Dharma Guadeloupe <67554685+DharmaKarmaStar@users.noreply.github.com>
2021-06-20 12:29:04 +0100

    Sees its as a number not a string

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -2,4 +2,4 @@ console.log("Hello world I just started learning JavaScript");
 console.log("I'm on week 1 of JavaScript Module 1");
 console.log("I'm doing homework on a Sunday instead of streaming");
 console.log("Soft Kitty, Warm kitty, Little ball of fur");
-console.log(Happy kitty, sleepy kitty, purr purr purr);
+console.log(5);
