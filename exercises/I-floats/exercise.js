3bffc43e2252a46d61d5e9ea39c896861105beb6
Dharma Guadeloupe <67554685+DharmaKarmaStar@users.noreply.github.com>
2021-06-20 14:44:22 +0100

    i_floats using math.round on exercise

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,9 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+const totalNumber = numberOfStudents + numberOfMentors;
+const studentsPercentage = (numberOfStudents / totalNumber) * 100;
+const studentsTotal = Math.round(studentsPercentage);
+const mentorsPercentage = (numberOfMentors / totalNumber) * 100;
+const mentorsTotal = Math.round(mentorsPercentage);
+console.log(`Percentage students: ${studentsTotal}%`);
+console.log(`Percentage mentors: ${mentorsTotal}%`);
