dc8f77a631b8db1359e360559cbf8d2d4cdad8e6
Texx45 <tcalderbank@hotmail.com>
2021-06-20 07:55:53 +0100

    Completed exercise L-functions-nested

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,16 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function shoutyGreeting(name) {
+  function capitaliseName(name) {
+    return name.toUpperCase();
+  }
+  return `HELLO ${capitaliseName(name)}`;
+}
+
+console.log(shoutyGreeting(mentor1));
+console.log(shoutyGreeting(mentor2));
+console.log(shoutyGreeting(mentor3));
+console.log(shoutyGreeting(mentor4));
+console.log(shoutyGreeting(mentor5));
