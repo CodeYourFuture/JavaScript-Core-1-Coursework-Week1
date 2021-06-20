8673b79dad25057eabf7c350630bb833a066b022
Gulnihal <gh.naldoken@gmail.com>
2021-06-20 12:39:28 +0100

    couldn't do exercise2

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,4 @@
-const name = " Daniel  ";
-
-console.log(message);
+const name = " Gulnihal  ";
+var message = "My name is Gulnihal and my name is 8 characters long"
+var noSpace = message.trim;
+console.log(noSpace);
