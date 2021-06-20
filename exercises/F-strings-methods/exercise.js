0f361efbd9c4f6eb5475f291e6f42d2a5b99ea2f
Omer Faruk  Yamak <74216179+omerffaruk@users.noreply.github.com>
2021-06-20 21:09:42 +0100

    F-strings-methods exercise done

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,8 @@
 // Start by creating a variable `message`
 
+const name = "Omer";
+const nameLength = name.length;
+const message = `   My name is ${name} and my name is ${nameLength} characters long.  `;
+
 console.log(message);
+console.log(message.trim());
--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,7 @@
 const name = " Daniel  ";
+const nameTrim = name.trim();
+const nameLength = name.length;
+const message = `My name is ${nameTrim} and my name is 6 characters long`;
 
 console.log(message);
+
