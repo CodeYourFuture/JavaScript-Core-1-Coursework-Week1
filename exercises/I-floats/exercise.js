c23bdb117ca18a6f2ccf2b9e4a5af90bb22fa66d
Texx45 <tcalderbank@hotmail.com>
2021-06-20 06:46:15 +0100

    Completed exercise I-floats

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,20 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var totalNumber = numberOfStudents + numberOfMentors;
+console.log(totalNumber);
+
+// Student percentage
+const percentageOfStudents = (numberOfStudents / totalNumber) * 100;
+console.log(percentageOfStudents);
+const roundedStudentPercentage = Math.round(percentageOfStudents);
+console.log(roundedStudentPercentage);
+
+// Mentor percentage
+const percentageOfMentors = (numberOfMentors / totalNumber) * 100;
+console.log(percentageOfMentors);
+const roundedMentorPercentage = Math.round(percentageOfMentors);
+console.log(roundedMentorPercentage);
+
+// Result
+console.log(`Percentage students: ${roundedStudentPercentage}%`);
+console.log(`Percentage mentors: ${roundedMentorPercentage}%`);
