027ee5983a0398c813cb3483152698feb183bbbb
Omer Faruk  Yamak <74216179+omerffaruk@users.noreply.github.com>
2021-06-20 21:26:50 +0100

    G-numbers exercise done

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,8 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+
+const numberOfStudents = 15;
+const numberOfMentors = 8;
+const message = `Number of students: ${numberOfStudents}
+Number of mentors: ${numberOfMentors}
+Total number of students and mentors: ${numberOfStudents + numberOfMentors}`;
+console.log(message);
