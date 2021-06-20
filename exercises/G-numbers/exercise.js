d039d8a892ead83c15527bb4aa8b15aed1575989
Dharma Guadeloupe <67554685+DharmaKarmaStar@users.noreply.github.com>
2021-06-20 14:36:01 +0100

    G-numbers exercise

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,5 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+const numberOfStudents = 15;
+const numberOfMentors = 8;
+const totalNumber = numberOfStudents + numberOfMentors;
+console.log(`Total number of students and mentors ${totalNumber}`);
