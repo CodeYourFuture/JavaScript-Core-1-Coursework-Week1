abc9ca1642150050e1c82b99eb8777e00874f9b1
Texx45 <tcalderbank@hotmail.com>
2021-06-20 17:11:11 +0100

    Passed function-output.js

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,16 +1,17 @@
 // Add comments to explain what this function does. You're meant to use Google!
 function getRandomNumber() {
-  return Math.random() * 10;
+  // This function generates a random number
+  return Math.random() * 10; // Returns a number between 0 and 1 (not including 1) You then multiply it by 10 to get a number.
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
 function combine2Words(word1, word2) {
-  return word1.concat(word2);
+  // This function concatenates 2 strings together.
+  return word1.concat(word2); // This returns a value of the 2 arguments concatenated.
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
-  // Write the body of this function to concatenate three words together.
-  // Look at the test case below to understand what this function is expected to return.
+  return firstWord.concat(" ", secondWord, " ", thirdWord);
 }
 
 /* 
