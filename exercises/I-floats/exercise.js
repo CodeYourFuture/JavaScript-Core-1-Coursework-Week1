c80ac4e361586c922a29b45d9bf55e02eafaf72c
Gulnihal <gh.naldoken@gmail.com>
2021-06-20 12:52:42 +0100

    I floats exercise is done

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,17 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+
+// var preciseAge = 30.612437;
+// var roughAge = Math.round(preciseAge);
+// console.log(roughAge);
+
+var proportionStudents = (numberOfStudents / (numberOfStudents + numberOfMentors)) *100;
+console.log(proportionStudents);
+var roughProportionStudents = Math.round(proportionStudents);
+console.log(roughProportionStudents);
+
+var proportionMentors = (numberOfMentors / (numberOfStudents + numberOfMentors)) *100;
+console.log(proportionMentors);
+var roughProportionMentors = Math.round(proportionMentors);
+console.log(roughProportionMentors);
