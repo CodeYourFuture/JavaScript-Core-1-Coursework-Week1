8da5b326392b4931fce46519fce8302ee6201999
Mya <myaskylar14@gmail.com>
2021-06-21 20:52:27 +0100

    Update 3-magic-8-ball.js

--- extra/3-magic-8-ball.js
+++ extra/3-magic-8-ball.js
@@ -43,11 +43,41 @@
     Very doubtful.
 */
 
+const choicesOfAnswer = [
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes - definitely.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful.",
+];
+
 // This should log "The ball has shaken!"
 // and return the answer.
 function shakeBall() {
   //Write your code in here
+  const answer =
+    choicesOfAnswer[Math.floor(Math.random() * choicesOfAnswer.length)];
+
+  console.log("The ball has shaken!");
+  return answer;
 }
+
+console.log(shakeBall());
 
 /* 
   This function should say whether the answer it is given is
@@ -59,8 +89,24 @@ function shakeBall() {
   This function should expect to be called with any value which was returned by the shakeBall function.
 */
 function checkAnswer(answer) {
-  //Write your code in here
+  let message = "";
+  for (let i = 0; i < choicesOfAnswer.length; i++) {
+    if (answer === choicesOfAnswer[i]) {
+      message = i;
+    }
+  }
+  if (message < 5) {
+    return "very positive";
+  } else if (message >= 5 && message < 10) {
+    return "positive";
+  } else if (message >= 10 && message < 15) {
+    return "negative";
+  } else {
+    return "very negative";
+  }
 }
+
+console.log(checkAnswer(shakeBall()));
 
 /* 
 ==================================
