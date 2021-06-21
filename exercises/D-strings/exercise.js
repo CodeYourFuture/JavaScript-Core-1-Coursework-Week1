fe1a98066ea767032a65536b7dca6e5b841c40d3
humailhasankhan <h.hasan.khan88@hotmail.com>
2021-06-21 11:40:32 +0100

    Finished Strings typeOf exercise by creating myMessage variable and another variable with its typeof and logging both variables to the console

--- exercises/D-strings/exercise.js
+++ exercises/D-strings/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
+const myMessage = "Hi, I am Humail";
+const myMessageType = typeof myMessage;
 
-console.log(message);
+console.log(myMessage);
+console.log(myMessageType);
