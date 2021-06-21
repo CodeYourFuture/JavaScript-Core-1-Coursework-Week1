0e328a41fe4634a0845693c888506ce285ea258c
maham511 <maha.malik511@gmail.com>
2021-06-21 20:29:17 +0100

    Finished exercise task till I-Floats

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,8 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+
+const numberOfStudents = 12;
+const numberOfMentors = 9;
+
+const total = numberOfStudents + numberOfMentors;
+
+console.log(total);
--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,13 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+var totalNumber = numberOfMentors + numberOfStudents;
+
+var studentPercentage = numberOfStudents / totalNumber * 100;
+var mentorsPercentage = numberOfMentors / totalNumber * 100;
+
+var roundedStudentPercentage = Math.round(studentPercentage);  
+var roundedMentorsPercentage = Math.round(mentorsPercentage);
+
+ console.log("Percentage student: " + roundedStudentPercentage + "%");
+ console.log("Percentage mentors: " + roundedMentorsPercentage + "%");
