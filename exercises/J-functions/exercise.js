84508b15c084ad32459fa909ea10c30f9597e161
Mya <myaskylar14@gmail.com>
2021-06-21 10:36:50 +0100

    answered exe-J

--- exercises/J-functions/exercise.js
+++ exercises/J-functions/exercise.js
@@ -1,7 +1,13 @@
 function halve(number) {
   // complete the function here
+  return number / 2
 }
 
-var result = halve(12);
-
-console.log(result);
+var result1 = halve(12);
+var result2 = halve(50);
+var result3 = halve(350);
+var result4 = halve(2540);
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
--- exercises/J-functions/exercise2.js
+++ exercises/J-functions/exercise2.js
@@ -1,7 +1,13 @@
 function triple(number) {
   // complete function here
+  return number * 3;
 }
 
 var result = triple(12);
-
+var result1 = triple(9);
+var result2 = triple(300);
+var result3 = triple(78920);
 console.log(result);
+console.log(result1);
+console.log(result2);
+console.log(result3);
