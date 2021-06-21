845b7b4e0816bb921f82331910414ff91e9ff105
humailhasankhan <h.hasan.khan88@hotmail.com>
2021-06-21 14:38:05 +0100

    completed numbers exercise by creating two variable for total number of students and mentors. Finally, created a third variable with template litterals to give a string including the totals

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,8 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+const numberOfStudents = 20;
+const numberOfMentors = 10;
+const totalNumbers = `There are ${numberOfStudents} students and ${numberOfMentors} mentors. In total, there are ${
+  numberOfStudents + numberOfMentors
+} students and mentors.`;
+
+console.log(totalNumbers);
