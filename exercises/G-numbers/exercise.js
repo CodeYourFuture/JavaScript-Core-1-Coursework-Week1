ddbe76cd7d677318a3ef6f130883db52d6ba8399
konikalily <konika.l.biswas@gmail.com>
2021-06-21 23:26:31 +0100

    Done some number data type exercises

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,5 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfMentors = 15;
+var numberOfStudents = 35;
+var totalNumber = numberOfMentors + numberOfStudents;
+console.log("Total number of students and mentors is : " + totalNumber);
