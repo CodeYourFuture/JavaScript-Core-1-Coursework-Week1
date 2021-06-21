bec6ba9804ba6f1154787dff267aadcac9d383b3
Alexx <alexandra_ciungan@yahoo.com>
2021-06-21 20:12:16 +0100

    exercises

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,12 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var total = numberOfStudents + numberOfMentors;
+
+var percentStudents = (numberOfStudents/total) * 100;
+var percentRStudents = Math.round(percentStudents);
+
+var percentMentors = (numberOfMentors/total) * 100;
+var percentRMentors = Math.round(percentMentors);
+
+console.log(percentRStudents);
+console.log(percentRMentors);
--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,18 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+  
+function createGreeting() {
+    var messageOne = "Hello " + mentor1; 
+    console.log(messageOne.toUpperCase());
+    var messageTwo = "Hello " + mentor2; 
+    console.log(messageTwo.toUpperCase());   
+    var messageThree = "Hello " + mentor3; 
+    console.log(messageThree.toUpperCase());   
+    var messageFour = "Hello " + mentor4; 
+    console.log(messageFour.toUpperCase());   
+    var messageFive = "Hello " + mentor5; 
+    console.log(messageFive.toUpperCase());   
+  };
+
