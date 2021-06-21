8fc596dfec4252566db9af6ba81e68dea2b54270
Mya <myaskylar14@gmail.com>
2021-06-21 10:51:56 +0100

    anw exe-K

--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,5 +1,8 @@
 // Declare your function first
-
+function addsNumber(num1, num2) {
+    return num1 + num2;
+}
 // Call the function and assign to a variable `sum`
+var sum = addsNumber(13,124);
 
 console.log(sum);
