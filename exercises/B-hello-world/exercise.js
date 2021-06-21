bfd501c3096499bae4a592cf41ba9a84f19d4c02
konikalily <konika.l.biswas@gmail.com>
2021-06-21 21:59:18 +0100

    Checked some exercise in terminal

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,6 @@
 console.log("Hello world");
+console.log("I started to learn javascript!");
+console.log("Good Evening");
+console.log("I am thrilled!");
+//console.log(I started to learn javascript!);/* I got error message for not using quote marks.*/
+console.log(6);
