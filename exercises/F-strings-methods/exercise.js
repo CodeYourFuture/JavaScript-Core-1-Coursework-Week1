b614a3a3ca62f33e4ec38823da823bd57d1dcd2b
humailhasankhan <h.hasan.khan88@hotmail.com>
2021-06-21 14:29:06 +0100

    completed both exercises by creating variables to concatenate the first one and .trim for the second one to use the given variable and count the characters.

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,11 @@
 // Start by creating a variable `message`
+const firstName = "Humail";
+const lengthOfMyName = firstName.length;
+const message =
+  "My name is " +
+  firstName +
+  " and my name is " +
+  lengthOfMyName +
+  " characters long.";
 
-console.log(message);
+console.log(message.trim());
--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,10 @@
 const name = " Daniel  ";
+const lengthOfMyName = name.trim().length;
+const message =
+  "My name is " +
+  name +
+  " and my name is " +
+  lengthOfMyName +
+  " characters long.";
 
 console.log(message);
