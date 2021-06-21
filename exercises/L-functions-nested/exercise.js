c904d863eed783129708cf1b2076415074a448b6
humailhasankhan <h.hasan.khan88@hotmail.com>
2021-06-21 15:40:47 +0100

    completed nested function exercise by creating a function which works inside another function.

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,19 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function createUppercaseNames(string) {
+  return string.toUpperCase();
+}
+
+function createShoutyGreeting(name) {
+  let mentorName = createUppercaseNames(name);
+  let message = "HELLO " + mentorName;
+  return message;
+}
+
+console.log(createShoutyGreeting(mentor1));
+console.log(createShoutyGreeting(mentor2));
+console.log(createShoutyGreeting(mentor3));
+console.log(createShoutyGreeting(mentor4));
+console.log(createShoutyGreeting(mentor5));
