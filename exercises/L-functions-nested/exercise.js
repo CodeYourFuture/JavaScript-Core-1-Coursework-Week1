9fa958db9d5df55469cfeb274fb8a5177629d6fb
Omer Faruk  Yamak <74216179+omerffaruk@users.noreply.github.com>
2021-06-21 09:21:04 +0100

    L-functions-nested exercise done

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,5 +1,43 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+const mentor1 = "Daniel";
+const mentor2 = "Irina";
+const mentor3 = "Mimi";
+const mentor4 = "Rob";
+const mentor5 = "Yohannes";
+
+
+function createShoutyGreeting (name) {
+    name = name.toUpperCase();
+    console.log(`HELLO ${name}`);
+}
+
+createShoutyGreeting(mentor1);
+createShoutyGreeting(mentor2);
+createShoutyGreeting(mentor3);
+createShoutyGreeting(mentor4);
+createShoutyGreeting(mentor5);
+
+
+
+
+// Another way of doing the exercise
+/*
+function nameInUpperCase(name) {
+    return name.toUpperCase();
+}
+
+function createShoutyGreeting() {
+    const name1 = nameInUpperCase(mentor1);
+    const name2 = nameInUpperCase(mentor2);
+    const name3 = nameInUpperCase(mentor3);
+    const name4 = nameInUpperCase(mentor4);
+    const name5 = nameInUpperCase(mentor5);
+    console.log(`HELLO ${name1}`);
+    console.log(`HELLO ${name2}`);
+    console.log(`HELLO ${name3}`);
+    console.log(`HELLO ${name4}`);
+    console.log(`HELLO ${name5}`);
+}
+
+createShoutyGreeting();
+*/
+
