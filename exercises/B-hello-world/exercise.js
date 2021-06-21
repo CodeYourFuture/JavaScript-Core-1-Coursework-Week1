5e25b1e2e61e134d6ccef72f2fd00b61ed4183af
konikalily <konika.l.biswas@gmail.com>
2021-06-21 22:01:04 +0100

    added a comment to exercise.js

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -3,4 +3,4 @@ console.log("I started to learn javascript!");
 console.log("Good Evening");
 console.log("I am thrilled!");
 //console.log(I started to learn javascript!);/* I got error message for not using quote marks.*/
-console.log(6);
+console.log(6); //number was printed.
