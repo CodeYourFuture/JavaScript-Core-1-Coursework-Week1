604cc73ec4a1596dfb35a88e5c2063a16c100022
Mya <myaskylar14@gmail.com>
2021-06-21 10:25:34 +0100

    answered exe-I

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,13 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+let percentageOfStudents = (15 / 23) * 100;
+let percentageOfMentors = (8 / 23) * 100;
+
+let studentsRounded = Math.round(percentageOfStudents);
+let mentorsRounded = Math.round(percentageOfMentors);
+console.log(`Percentage of students: ${studentsRounded} % `);
+console.log(`Percentage of mentors: ${mentorsRounded} %`);
+// Number of students: 15
+// Number of mentors: 8
+// Total number of students and mentors: 23
