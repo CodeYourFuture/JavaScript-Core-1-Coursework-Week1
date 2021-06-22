96fc41413dc82b6ffe28af13349d9ea1bffedef5
humailhasankhan <h.hasan.khan88@hotmail.com>
2021-06-22 10:04:40 +0100

    completed extra exercise 2 by returning all three functions and declaring badCode variable and two more variables to go along goodCode variable. Added parseFloat toFixed at 1 decimal so it meets the requirements to cater decimal numbers with 1 decimal

--- extra/2-piping.js
+++ extra/2-piping.js
@@ -16,26 +16,27 @@
   the final result to the variable goodCode
 */
 
-function add() {
-
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function multiply() {
-
+function multiply(a, b) {
+  return a * b;
 }
 
-function format() {
-
+function format(amount) {
+  return `£${parseFloat(amount.toFixed(1))}`;
 }
 
 const startingValue = 2;
 
 // Why can this code be seen as bad practice? Comment your answer.
-let badCode =
+let badCode = format(multiply(add(startingValue, 10), 2));
 
 /* BETTER PRACTICE */
-
-let goodCode =
+let addedNumbers = add(startingValue, 10);
+let multipliedNumbers = multiply(addedNumbers, 2);
+let goodCode = format(multipliedNumbers);
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
