f5575177625c2884f8b58be3d8759ee653191fc7
konikalily <konika.l.biswas@gmail.com>
2021-06-22 13:54:33 +0100

    update syntax-error

--- mandatory/1-syntax-errors.js
+++ mandatory/1-syntax-errors.js
@@ -4,14 +4,12 @@ function addNumbers(a, b, c) {
   return a + b + c;
 }
 
-function introduceMe(name, age){
-   return "Hello, my name is " + name + " and I am " + age + " years old";
+function introduceMe(name, age) {
+  return "Hello, my name is " + name + " and I am " + age + " years old";
 }
- 
 
 function getTotal(a, b) {
-  return  "The total is " + (a + b);
-
+  return "The total is " + (a + b);
 }
 /* 
 ===================================================
