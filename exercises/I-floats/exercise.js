0ef92711e23a9ebc3dd19b097646de55e5231e8a
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2021-06-23 18:30:25 +0100

    exercise I-floats

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,9 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var percentageStudents = Math.round((numberOfStudents * 100) / (numberOfStudents + numberOfMentors));
+var percentageMentors = Math.round(
+  (numberOfMentors * 100) / (numberOfStudents + numberOfMentors)
+);
+
+console.log("Percentage students: " + percentageStudents + "%");
+console.log("Percentage mentors: " + percentageMentors + "%");
