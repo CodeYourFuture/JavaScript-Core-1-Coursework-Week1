8b8d4667b6938147f2d333e0c206ddaa0eea90fb
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2021-06-23 18:19:53 +0100

    exercise G-numbers

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,6 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 15;
+var numberOfMentors = 10;
+var numberTotal = numberOfStudents + numberOfMentors;
+
+console.log(numberTotal);
