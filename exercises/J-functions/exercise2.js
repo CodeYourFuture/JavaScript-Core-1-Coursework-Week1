eda1a0dd8a5f3f206aaa3e5ee7e953fc1d0eac0e
Seyed Sharifi <seyedsharifi12@gmail.com>
2021-06-23 23:48:00 +0100

    Completed the extras upto the 8-ball-magic.

--- exercises/J-functions/exercise2.js
+++ exercises/J-functions/exercise2.js
@@ -1,7 +1,6 @@
 function triple(number) {
   return number * 3;
 }
-
 var result = triple(12);
 
 console.log(result);
--- extra/3-magic-8-ball.js
+++ extra/3-magic-8-ball.js
@@ -13,40 +13,41 @@
   and have different levels of positivity or negativity.
 
   Below are the possible answers:
-
-  ## Very positive
-    It is certain.
-    It is decidedly so.
-    Without a doubt.
-    Yes - definitely.
-    You may rely on it.
-
-  ## Positive
-    As I see it, yes.
-    Most likely.
-    Outlook good.
-    Yes.
-    Signs point to yes.
-
-  ## Negative
-    Reply hazy, try again.
-    Ask again later.
-    Better not tell you now.
-    Cannot predict now.
-    Concentrate and ask again.
-
-  ## Very negative
-    Don't count on it.
-    My reply is no.
-    My sources say no.
-    Outlook not so good.
-    Very doubtful.
 */
+const arrayOfAnswers = [
+  //Very positive
+    'It is certain.',
+    'It is decidedly so.',
+    'Without a doubt.',
+    'Yes - definitely.',
+    'You may rely on it.',
+
+  //Positive
+    'As I see it, yes.',
+    'Most likely.',
+    'Outlook good.',
+    'Yes.',
+    'Signs point to yes.',
+
+  //Negative
+   'Reply hazy, try again.',
+    'Ask again later.',
+    'Better not tell you now.',
+    'Cannot predict now.',
+    'Concentrate and ask again.',
+
+  //Very negative
+    'Don\'t count on it.',
+    'My reply is no.',
+    'My sources say no.',
+    'Outlook not so good.',
+    'Very doubtful.' ]
+
 
 // This should log "The ball has shaken!"
 // and return the answer.
 function shakeBall() {
-  //Write your code in here
+  
 }
 
 /* 
