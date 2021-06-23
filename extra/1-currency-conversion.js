7bc4b4c9522a4b938de85d9d69bddca7eaba8ad5
Omer Faruk  Yamak <74216179+omerffaruk@users.noreply.github.com>
2021-06-23 09:34:18 +0100

    1-currency-conversion exercise done

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -5,7 +5,10 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(pound) {
+  const usd = pound * 1.4;
+  return usd;
+}
 
 /*
   CURRENCY FORMATTING
@@ -15,7 +18,11 @@ function convertToUSD() {}
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL() {}
+function convertToBRL(pound) {
+  const afterFee = pound * 0.99; 
+  const brl = afterFee * 5.7;
+  return parseFloat(brl.toFixed(2));
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
@@ -37,5 +44,5 @@ test("convertToBRL function works for £30", () => {
 });
 
 test("convertToBRL function works for £0.99", () => {
-  expect(convertToBRL(1.5)).toEqual(8.46);
+  expect(convertToBRL(1.50)).toEqual(8.46);
 });
