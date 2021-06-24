dd548f150d0616830698e72073869f017d94a46b
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2021-06-24 21:10:07 +0100

    madnatory 3

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,15 +1,18 @@
 // Add comments to explain what this function does. You're meant to use Google!
+//Return a random number between 0 (inclusive) and 1 (exclusive):
 function getRandomNumber() {
   return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+//The concat() method is used to join two or more strings. This method does not change the existing strings, but returns a new string containing the text of the joined strings.
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
+  return firstWord.concat(" ", secondWord," ", thirdWord);
   // Look at the test case below to understand what this function is expected to return.
 }
 
