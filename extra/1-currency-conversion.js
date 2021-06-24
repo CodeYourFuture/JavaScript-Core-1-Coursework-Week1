c87a9b161759332c19de1c78726bb32439aaab65
maham511 <maha.malik511@gmail.com>
2021-06-24 23:42:06 +0100

    Passed all tests in 1-currency-conversion.js in 'Extra'

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -5,7 +5,9 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(pounds) {
+  return pounds * 1.4;
+}
 
 /*
   CURRENCY FORMATTING
@@ -15,8 +17,13 @@ function convertToUSD() {}
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL() {}
-
+function convertToBRL(pounds) {
+  var brReal = pounds * 5.7;
+  var totalAfterFee = brazReal * 0.99;
+  var totalWithDec = totalAfterFee.toFixed(2);
+  var finalTotalFee = parseFloat(totalWithDec);
+   return finalTotalFee;
+}
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
 
