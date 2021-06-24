10eea64c121795ab1af14743e1a6b451c44f6ba4
Gulnihal <gh.naldoken@gmail.com>
2021-06-24 20:12:06 +0100

    done

--- extra/2-piping.js
+++ extra/2-piping.js
@@ -16,26 +16,27 @@
   the final result to the variable goodCode
 */
 
-function add() {
-
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function multiply() {
-
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-function format() {
-
+function format(number) {
+  return "£" + number;
 }
 
 const startingValue = 2;
 
-// Why can this code be seen as bad practice? Comment your answer.
-let badCode =
+// Why can this code be seen as bad practice? Comment your answer. 
+// bacause: lots of parantheses, and there  are already mathematical symbols to do these operations.
+let badCode = format(multiply((add(startingValue, 10)), 2));
 
 /* BETTER PRACTICE */
 
-let goodCode =
+let goodCode = "£" + ((startingValue+10)*2);
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
