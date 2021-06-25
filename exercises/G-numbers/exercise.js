b1e85710eeb54e613a92524fceacb709177d3e77
niamhmadden23 <niamhmadden15@gmail.com>
2021-06-25 20:05:29 +0100

    Completed G-numbers

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,7 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 50;
+var numberOfMentors = 10;
+
+var sum = numberOfStudents + numberOfMentors;
+
+console.log(sum);
