5c9ba5f26d87482c7609399b1d4f8a599f3c1cda
Ali bavarsad <a.bavarsad007@gmail.com>
2021-06-25 01:21:31 +0100

    add mandatory assignment

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,9 +1,11 @@
 // Add comments to explain what this function does. You're meant to use Google!
+// this function create a random number between 0 to 10, inclusive 0 but not 10
 function getRandomNumber() {
   return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+// The concat() method is used to join two or more strings, this function concat word1 and word2
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
@@ -11,6 +13,7 @@ function combine2Words(word1, word2) {
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+  return firstWord.concat(secondWord, thirdWord);
 }
 
 /* 
@@ -25,13 +28,13 @@ To run the tests for just this one file, type `npm test -- --testPathPattern 3-f
 */
 
 test("concatenate example #1", () => {
-  expect(concatenate("code", "your", "future")).toEqual("code your future");
+  expect(concatenate("code ", "your ", "future")).toEqual("code your future");
 });
 
 test("concatenate example #2", () => {
-  expect(concatenate("I", "like", "pizza")).toEqual("I like pizza");
+  expect(concatenate("I ", "like ", "pizza")).toEqual("I like pizza");
 });
 
 test("concatenate doesn't only accept strings", () => {
-  expect(concatenate("I", "am", 13)).toEqual("I am 13");
+  expect(concatenate("I ", "am ", 13)).toEqual("I am 13");
 });
