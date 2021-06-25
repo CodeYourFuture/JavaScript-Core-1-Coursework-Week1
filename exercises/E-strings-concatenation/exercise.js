788ca2affbf559699650e967bfb63abce1621d6a
Khadar Dagal <57036418+Kdagaal@users.noreply.github.com>
2021-06-25 12:39:47 +0100

    JS 1 Concatenation
    adding to strings together by using plus + operator

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
+var greeting = "Hello my name is ";
+var names = "Khadar";
+var completeGre = greeting + names;
 
-console.log(message);
+console.log(completeGre);
