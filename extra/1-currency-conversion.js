1ae9ac04717dc7fac2f575b9b8a6a756e30f58a9
Mor Khojasteh <mori.kh1987@gmail.com>
2021-06-25 22:57:55 +0100

    1-Currency Conversion

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -5,7 +5,10 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(exchange) {
+  return exchange * 1.4;
+
+}
 
 /*
   CURRENCY CONVERSION
@@ -15,7 +18,11 @@ function convertToUSD() {}
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL() {}
+function convertToBRL(exchange) {
+  const exchangeFee = exchange * 0.99 * 5.7
+  return parseFloat(exchangeFee.toFixed(2));
+
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
