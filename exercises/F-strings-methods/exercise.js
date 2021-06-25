4cb4dfe91291ec26561d9c976ac140047854ca70
Khadar Dagal <57036418+Kdagaal@users.noreply.github.com>
2021-06-25 12:52:42 +0100

    .length practice

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
-
-console.log(message);
+var myName = "KHADAR";
+var checkLenght = myName.length;
+console.log(myName);
+// above this line I am Just practicing
