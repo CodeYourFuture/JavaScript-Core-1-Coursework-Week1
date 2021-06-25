c9b90d740ea4025daaa3f43837e7366f2da501a6
Mor Khojasteh <mori.kh1987@gmail.com>
2021-06-25 18:17:06 +0100

    Edit const

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,6 +1,6 @@
 // Start by creating a variable `message`
 const greetingStart = "Hello, My Nmae Is ";
-const name = "Morteza";
-const message = greetingStart + name ;
+const names = "Morteza";
+const message = greetingStart + names ;
 
 console.log(message);
