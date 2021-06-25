f0c6370c85380a878dad010db1f555f2cf717d88
niamhmadden23 <niamhmadden15@gmail.com>
2021-06-25 21:53:06 +0100

    Completed mandatory exercise 3

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,16 +1,19 @@
-// Add comments to explain what this function does. You're meant to use Google!
+// Get a random number and times by 10
 function getRandomNumber() {
   return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
 function combine2Words(word1, word2) {
-  return word1.concat(word2);
+     const firstWord = word1.concat(" ", word2);
+  return firstWord ;
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+   const result = firstWord.concat(" ", secondWord, " ", thirdWord);
+  return result ;
 }
 
 /* 
