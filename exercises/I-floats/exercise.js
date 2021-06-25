34c61bbcba9336a5aaf804a669e8cde414b03732
Khadar Dagal <57036418+Kdagaal@users.noreply.github.com>
2021-06-25 15:01:40 +0100

    Update exercise.js

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,9 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var total = numberOfStudents + numberOfMentors;
+var students = "Percentage Students are :";
+var mentors = "Percentage Mentors are :";
+var PercentageStudents = students + (numberOfStudents / total) * 100;
+var PercentageMentors = mentors + (numberOfMentors / total) * 100;
+console.log(PercentageStudents);
+console.log(PercentageMentors);
