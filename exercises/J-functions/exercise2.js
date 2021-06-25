cafbba2ab6f0ebba2b18373fff7f4b58fa6733b4
Khadar Dagal <57036418+Kdagaal@users.noreply.github.com>
2021-06-25 15:58:01 +0100

    JS 1 exercises - J-functions
    created function and returned statement with console.log
    
    and then called the function  and passed different arguments. result is as required.

--- exercises/J-functions/exercise2.js
+++ exercises/J-functions/exercise2.js
@@ -1,7 +1,13 @@
 function triple(number) {
-  // complete function here
+    // complete function here
+    return number * 3;
 }
 
 var result = triple(12);
 
 console.log(result);
+
+// 1. the word "number" is parameter
+// 2. we created a variable then
+// 3. the we call the function to pass argument or value
+// 4. we console the to see the output
