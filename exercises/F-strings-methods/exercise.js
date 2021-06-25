13083e16f736f01c606bb3a5a2566823a2336cea
Khadar Dagal <57036418+Kdagaal@users.noreply.github.com>
2021-06-25 13:15:02 +0100

    JS 1 F-strings methods
    this one when I want to print whole sentence the trim() method is not working. the space is still there when you look in them sentance.

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,5 +1,10 @@
 // Start by creating a variable `message`
 var myName = "KHADAR";
 var checkLenght = myName.length;
-console.log(myName);
+var complete = "This is my name " + myName + ", and it is " + checkLenght + " character long";
+console.log(complete);
 // above this line I am Just practicing
+// now trying to practice string_methods wow!!
+
+var nameLowerCase = myName.toLocaleLowerCase();
+console.log(nameLowerCase);
--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,6 @@
-const name = " Daniel  ";
-
+const myName = " Daniel  ";
+const message = myName.trim();
+const checkLenght = myName.length;
+var complete = "This is my name" + myName + ",and it is " + checkLenght + " character long";
 console.log(message);
+console.log(complete);
