af308230c3703e083350393522e8635b9c5ac29a
Khadar Dagal <57036418+Kdagaal@users.noreply.github.com>
2021-06-25 15:39:07 +0100

    JS 1 exercise I-floats
    Finally Student Percentage is 65%. whereas Mentors are 35%.
    
    to get this I employed different technique of JavaScript that I have learnt so far.

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,12 +1,26 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
 var total = numberOfStudents + numberOfMentors;
+
+// creating string the will introduce what the number or % is about
 var students = "Percentage Students are :";
 var mentors = "Percentage Mentors are :";
-var PreciseStudentPercentage = (numberOfStudents / total) * 100;
-var PreciseMentorsPercentage = (numberOfMentors / total) * 100;
+
+// This is precise calculation not round up yet.
+var StudentProportion = (numberOfStudents / total) * 100;
+var MentorsProportion = (numberOfMentors / total) * 100;
+
+// This is the rounded percentage of the Student and Mentors
+var roundedPercStudent = Math.round(StudentProportion);
+var roundedPercMentor = Math.round(MentorsProportion);
+
+// Now we are going to put whole things together
+var StudentPercentage = students + roundedPercStudent + "%";
+var mentorPercentage = mentors + roundedPercMentor + "%";
+
+// console to see the output of the codeconsole.log(StudentProportion);
+console.log(StudentPercentage);
+console.log(mentorPercentage);
 
 
-
-console.log(PreciseStudentPercentage);
-console.log(PreciseMentorsPercentage);
+// Woow the output is as required!
