12833899f7a39eb1a23717ea09aee673ffc61636
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-06-26 19:45:04 +0100

    C-variables complete

--- exercises/C-variables/exercise.js
+++ exercises/C-variables/exercise.js
@@ -1,3 +1,9 @@
-// Start by creating a variable `greeting`
+// Program to print a `greeting` variable in the console 3 times
+// Hopefully it is ok to be a bit creative with my solution and reduce code repetition :)
 
-console.log(greeting);
+let greeting =
+  ". My name is Andrew and Ive been learning how to code with <Code>YourFuture since 10/04/21";
+
+for (let i = 1; i < 4; i++) {
+  console.log(i + greeting);
+}
