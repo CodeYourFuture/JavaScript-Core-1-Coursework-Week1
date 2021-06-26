482aea575d31573ae128f763453fcc35594bc1c0
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-06-26 21:11:50 +0100

    D-strings Complete

--- exercises/D-strings/exercise.js
+++ exercises/D-strings/exercise.js
@@ -1,3 +1,23 @@
-// Start by creating a variable `message`
+// Program that selects one of the three basic types of variables then outputs it along with its type.
+// Again, hopefully its ok that I added some randomness to this exercise, I had a lot of fun doing it :)
 
-console.log(message);
+// Declare the `messageChoice` variable, then set it to a random number between 0 and 2 rounded to the nearest integer.
+let messageChoice = Math.round(Math.random() * 2);
+
+// Declares the `message` variable.
+let message;
+
+// Determines which message to display using the random `messageChoice` value.
+if (messageChoice == 0) {
+  message = "Hello world!";
+} else if (messageChoice == 1) {
+  message = 25.5;
+} else {
+  message = true;
+}
+
+// Declares `messageType` variable and sets it equal to  the variable `message` type (string, number or boolean)
+let messageType = typeof message;
+
+// Outputs the selected `message` and `messageType`
+console.log(message + " is a " + messageType);
