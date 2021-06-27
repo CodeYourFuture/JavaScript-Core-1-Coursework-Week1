6099521de170b6c6cb363d5268ca494d18b5ea78
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-06-27 20:10:35 +0100

    L-functions-nested complete

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,5 +1,14 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+// I hope the way I have solved this exercise is ok although the readme seemed to have
+// some wiggle room but happy to redo if needed. As its the last exercise I wanted
+// to try and be a bit creative so to solve this I have put the pre-defined mentor names into
+// an array then iterated through it using a for loop which contains two nested ES6
+// arrow functions. Had great fun figuring out how to solve it like this:)
+
+let mentors = ["Daniel", "Irina", "Mimi", "Rob", "Yohannes"];
+
+for (let i = 0; i < mentors.length; i++) {
+  let mentor = mentors[i];
+  const getNameInUpperCase = (mentor) => mentor.toUpperCase();
+  const shoutyGreeting = (mentor) => `HELLO ${getNameInUpperCase(mentor)}`;
+  console.log(shoutyGreeting(mentor));
+}
