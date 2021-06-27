5e0a6a37f39d35b5d3d87392bcc0b6904a790a07
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-06-27 15:06:35 +0100

    J-functions complete

--- exercises/J-functions/exercise.js
+++ exercises/J-functions/exercise.js
@@ -1,7 +1,13 @@
 function halve(number) {
-  // complete the function here
+  // Function to half the input parameter `number`
+  return result = number / 2;
 }
 
-var result = halve(12);
+// Testing with specified input which also produces an integer value output
+console.log(halve(12));
 
-console.log(result);
+// Testing with an input that produces a float value output
+console.log(halve(1));
+
+// Testing 0 just to see what happens :)
+console.log(halve(0));
