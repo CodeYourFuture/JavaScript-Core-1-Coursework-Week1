02b3ed65cea57baa03fd4c0a0858bfaffc6debc2
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-06-27 10:28:12 +0100

    F-strings-methods complete

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,11 @@
-// Start by creating a variable `message`
+// Program that outputs a concatenated string stored in variable `message` containing constant `myName`
+// and constant `nameLength` after calculating `myNames` character length using string method .length.
+// I used constants here as `myName` will never change and consequently its length wont either, however
+// the variable `message` concatenation may be updated so I declared it using let
+
+const myName = "Andrew";
+const nameLength = myName.length;
+let message =
+  "My name is " + myName + " and my name is " + nameLength + " characters long";
 
 console.log(message);
--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,10 @@
-const name = " Daniel  ";
+// This program performs the same basic function as exercise.js
+// however using the string method .trim() to remove the white space
+// at both sides of the string " Daniel ".
+
+const myName = " Daniel  ".trim();
+const nameLength = myName.length;
+let message =
+  "My name is " + myName + " and my name is " + nameLength + " characters long";
 
 console.log(message);
