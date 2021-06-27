dc27fb3b037b9551d71cc1e90c9f3afcd1a22a99
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-06-27 11:09:03 +0100

    G-numbers complete

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,13 @@
-// Start by creating a variables `numberOfStudents` and `numberOfMentors`
+// Program that outputs the value of `numberOfStudents`, `numberOfMentors` 
+// and `totalStudentsAndMentors` after using template string literal syntax to concatenate 
+// the string stored in `message`.
+
+let numberOfStudents = 15;
+let numberOfMentors = 8;
+const totalStudentsAndMentors = numberOfStudents + numberOfMentors;
+
+let message = `Number of students: ${numberOfStudents}
+Number of mentors: ${numberOfMentors}
+Total number of students and mentors: ${totalStudentsAndMentors}`;
+
+console.log(message);
