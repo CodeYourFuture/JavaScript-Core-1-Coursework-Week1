cbb8d05bc7f455bd1f894d3fd828f7228375fe0d
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-06-28 21:49:40 +0100

    I

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,8 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var percentageStudent = (numberOfStudents / (numberOfStudents + numberOfMentors)) * 100
+var perecentageMentors = (numberOfMentors / (numberOfStudents + numberOfMentors))*100
+var roughPercentageStudent = Math.round(percentageStudent);
+var roughPerecentageMentors = Math.round(perecentageMentors);
+console.log("Perecentage student:", roughPercentageStudent, "%");
+console.log("Percentage mentors:", roughPerecentageMentors, "%");
