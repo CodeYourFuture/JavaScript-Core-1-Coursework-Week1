844a27eaaf16d42164ebf5588f9f27faf43fc93a
Aj <ahmedjalalsd@gmail.com>
2021-06-28 19:29:27 +0100

    Solution F-I

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,8 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+
+let numberOfStudents = 15;
+let numberOfMentors = 8;
+
+let totalStudentsMentors = numberOfMentors + numberOfStudents;
+
+console.log("Total number of students and mentors: " + totalStudentsMentors);
--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,10 @@
-var numberOfStudents = 15;
-var numberOfMentors = 8;
+let numberOfStudents = 15;
+let numberOfMentors = 8;
+let totalNumber = numberOfStudents + numberOfMentors;
+
+let percentageOfStudents = (numberOfStudents / totalNumber) * 100;
+percentageOfStudents = Math.round(percentageOfStudents);
+let percentageOfMentors = (numberOfMentors / totalNumber) * 100;
+
+console.log("Percentage students: " + percentageOfStudents + "%");
+console.log("Percentage mentors: " + Math.round(percentageOfMentors) + "%");
