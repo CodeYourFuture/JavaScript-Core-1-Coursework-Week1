6af81f546775d3f98d5f24d75e13c3e4242fc304
Mohammedal-abdulhamid <mohammedalabdulhamid@gmail.com>
2021-06-28 23:57:20 +0100

    from E to I

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
+var greetingMassage = "hello ,";
+var myName = "my name is Mohammed !";
+var message = greetingMassage + myName;
 
 console.log(message);
--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,12 @@
 // Start by creating a variable `message`
+const myName = "Mohammed";
+const myNameLength = myName.length;
+var message =
+  " my name is " +
+  myName +
+  " and my name length is " +
+  myNameLength +
+  " characters long";
+console.log(message);
 
 console.log(message);
