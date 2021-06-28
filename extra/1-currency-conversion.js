a35e87148abdd90aa7863513580e01cb54ec4c86
Aj <ahmedjalalsd@gmail.com>
2021-06-28 23:10:09 +0100

    extra execrise

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -5,17 +5,33 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(pound) {
+  let usd = pound * 1.4;
+  return usd;
+}
+
+console.log(convertToUSD(32));
+console.log(convertToUSD(50));
 
 /*
   CURRENCY CONVERSION
   ===================
   The business is now breaking into the Brazilian market
   Write a new function for converting to the Brazilian real (exchange rate is 5.7 BRL to £)
-  They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
+  They have also decided that they should add a 1% fee to all foreign transactions, 
+  which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL() {}
+function convertToBRL(pound) {
+  let fees = pound * 0.01;
+  pound = pound - fees;
+  let brl = pound * 5.7;
+  // let fees = 0.01 * brl
+  // brl = brl - fees;
+  return brl;
+}
+
+console.log(convertToBRL(30));
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
