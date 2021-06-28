edc497279312fe5a2ebef72e2494e5ae4f056f51
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-06-28 21:30:54 +0100

    F - 2

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,6 @@
-const name = " Daniel  ";
+var name = "Abdulrahman ";
+var nameLength = name.length;
+var introName = "My name is ";
+var chara = "and my name is " + nameLength + " characters long";
 
-console.log(message);
+console.log(introName.trim() + name.trim() + chara.trim());
