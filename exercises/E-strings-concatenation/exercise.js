cd205946e4aa5e2c8fe3a0b60516e1790c0033fb
ISLEEM75 <isleem52@gmail.com>
2021-06-30 02:12:26 +0300

    JavaScript Exercises‏

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,7 @@
 // Start by creating a variable `message`
+let greeting = "Hello, my name is ";
+let name = "Ibrahim Isleem";
+
+let message = greeting + name;
 
 console.log(message);
--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,17 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function greeting(name) {
+    return `hello ${name}`;
+}
+
+function greetingUpperCase(name) {
+    return greeting(name).toUpperCase();
+}
+
+console.log(greetingUpperCase(mentor1));
+console.log(greetingUpperCase(mentor2));
+console.log(greetingUpperCase(mentor3));
+console.log(greetingUpperCase(mentor4));
+console.log(greetingUpperCase(mentor5));
