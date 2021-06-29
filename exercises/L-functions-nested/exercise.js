ddb0d7194b71bdf948b383f41c4de45690e64546
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-06-29 06:59:34 +0100

    L

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,5 +1,19 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+
+
+function  shoutyGreeting (name) {
+ var  great= "Hello " + name; 
+ great = great.toUpperCase();
+ return great; 
+}
+
+var mentor1 = shoutyGreeting ("Daniel");
+var mentor2 = shoutyGreeting ("Irina");
+var mentor3 = shoutyGreeting ("Mimi");
+var mentor4 = shoutyGreeting ("Rob");
+var mentor5 = shoutyGreeting ("Yohannes");
+
+console.log(mentor1);
+console.log(mentor2);
+console.log(mentor3);
+console.log(mentor4);
+console.log(mentor5);
