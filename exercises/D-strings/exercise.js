63a722c35d5493aa1c8ae33f6e26d5f93c0f79f7
Mahmoud-Nwiry <mamo992001@gmail.com>
2021-06-29 09:54:28 +0300

    Finised exercises folder

--- exercises/D-strings/exercise.js
+++ exercises/D-strings/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
+let message = "My name's Mahmoud.";
+let messageType = typeof message;
 
 console.log(message);
+console.log(messageType);
--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,4 +1,9 @@
 // Declare your function first
+function add(num1, num2){
+    return num1 + num2;
+}
+
+let sum = add(13, 124);
 
 // Call the function and assign to a variable `sum`
 
