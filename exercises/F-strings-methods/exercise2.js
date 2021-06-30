530a9d845fd9b417b0494bb5a113c7ae8be47e96
Lana201999 <lanasharabati99@gmail.com>
2021-07-01 01:28:33 +0300

    JavaScript Exercises
    JavaScript Exercises

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,11 @@
-const name = " Daniel  ";
-
+const name = " Lana  ";
+var trimName = name.trim();
+var message;
+var nameLength = trimName.length;
+message =
+    "My name is " +
+    trimName +
+    " and my name is " +
+    nameLength +
+    " characters long";
 console.log(message);
