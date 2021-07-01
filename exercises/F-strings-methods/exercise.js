9e328a1325b02b5d69fa5177aeb731959b910c13
Amelia Komorowska <83351833+AmeliaK94@users.noreply.github.com>
2021-07-01 19:12:55 +0100

    Finished exercise F

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,8 @@
 // Start by creating a variable `message`
 
+var name = "Amelia";
+var nameLength = name.length;
+
+var message = "My name is " + name + " and my name is " + nameLength + " characters long.";
+
 console.log(message);
