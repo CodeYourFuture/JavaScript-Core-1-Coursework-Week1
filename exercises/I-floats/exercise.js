b494cfaa5251663662baf7a9d062eb73f915e45d
sondos zaro <197075@ppu.edu.ps>
2021-07-01 11:19:06 +0300

    changes in I-floats file

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,6 +1,8 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
-
-var str = `Percentage students: ${numberOfStudents}%
-Percentage mentors: ${numberOfMentors}%`;
+var sum = numberOfMentors + numberOfStudents;
+var studentsPercentage = Math.round((numberOfStudents / sum) * 100);
+var mentorsPercentage = Math.round((numberOfMentors / sum) * 100);
+var str = `Percentage students: ${studentsPercentage}%
+Percentage mentors: ${mentorsPercentage}%`;
 console.log(str);
