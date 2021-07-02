d8f87e0bae2c83cbe19318f4b19e31c2ecdaca0c
Dilekcn <dcanturk1@gmail.com>
2021-07-03 00:09:55 +0100

    L function nested
    L function nested

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,20 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+
+function mentor11(greet,name){
+    return greet +"," + name
+}
+const men1 = mentor11( "Hello"," Daniel");
+const men2 = mentor11( "Hello"," Irina");
+const men3 = mentor11( "Hello"," Mimi");
+const men4 = mentor11( "Hello"," Rob");
+const men5 = mentor11( "Hello"," Yohannes");
+
+console.log(men1.toUpperCase())
+console.log(men2.toUpperCase())
+console.log(men3.toUpperCase())
+console.log(men4.toUpperCase())
+console.log(men5.toUpperCase())
+
