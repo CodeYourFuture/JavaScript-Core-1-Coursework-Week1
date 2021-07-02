8a6b200707929288af171de58d7bd2c6fb3d2d49
Megumi Yanaka <margaret_mori0102@yahoo.com>
2021-07-03 00:02:39 +0100

    I used ".length" instead of number for "i" in "L-functions-nested"

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,6 +1,6 @@
 
 const mentors = ["Daniel","Irina","Mimi","Rob","Yohanne"];
-for(let i=0; i<5; i++){
+for(let i=0; i< mentors.length; i++){
     console.log("HELLO " + mentors[i].toUpperCase());
 }
 
