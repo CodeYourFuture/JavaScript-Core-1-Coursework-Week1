7306d4fe7ec15885144d86473042f29fd867cd7a
Amelia Komorowska <83351833+AmeliaK94@users.noreply.github.com>
2021-07-02 12:32:21 +0100

    Finished task L

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,19 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function uppercase (mentor) {
+    return mentor.toUpperCase();
+}
+
+function createGreeting (mentor) {
+    var name = uppercase(mentor);
+    var message = "HELLO " + name;
+    return message;
+}
+
+console.log (createGreeting(mentor1));
+console.log (createGreeting(mentor2));
+console.log (createGreeting(mentor3));
+console.log (createGreeting(mentor4));
+console.log (createGreeting(mentor5));
