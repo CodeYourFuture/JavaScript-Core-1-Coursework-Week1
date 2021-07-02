b5b2406c1aea5274946e69c7139573bcf0d2566d
Dilekcn <dcanturk1@gmail.com>
2021-07-02 22:49:10 +0100

    A-setup-ide and B hello Word did
    A-setup-ide and B hello Word did

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,11 @@
 console.log("Hello world");
+
+let name = "Dilek";
+let surname = "Canturk";
+let city ="Glasgow";
+
+let person ="My name is " + name +" " +surname +" "+ "and I live in " +city;
+
+console.log(person);
+console.log('Hello World. I just started learning JavaScript!');
+
