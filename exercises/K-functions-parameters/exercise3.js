93697b63e59b5b70f37c548e2bddefbf6ad00a81
Dilekcn <dcanturk1@gmail.com>
2021-07-02 23:58:29 +0100

    K function parametre
    K function parametre

--- exercises/K-functions-parameters/exercise3.js
+++ exercises/K-functions-parameters/exercise3.js
@@ -1,5 +1,10 @@
 // Write your function here
+function createGreeting(x){
+var y ="Hello, my name is "
+    return y+x;
+}
 
-var greeting = createGreeting("Daniel");
+
+var greeting = createGreeting("Dilek");
 
 console.log(greeting);
--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,5 +1,7 @@
 // Declare your function first
-
+function added(a,b){
+    return a +b;
+}
 // Call the function and assign to a variable `sum`
-
+const sum = added(13,124);
 console.log(sum);
