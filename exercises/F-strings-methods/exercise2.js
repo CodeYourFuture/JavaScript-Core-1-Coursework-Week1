f16bf72fd4d31cd5af185301ea20f64d07f5af57
Megumi Yanaka <margaret_mori0102@yahoo.com>
2021-07-02 23:06:14 +0100

    Finished by "Kofunctions-parameters"

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,8 +1,8 @@
-const name = " Daniel  ";
+const name = "     Daniel    ";
 var messageStart = "My name is ";
 var nameLength = name.length;
 var messageLast = " characters long";
-console.log(messageStart + name.trim() + " and my name is " + nameLength + messageLast);
+console.log(messageStart + name + " and my name is " + nameLength + messageLast);
 
 
 
--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,6 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+
+
