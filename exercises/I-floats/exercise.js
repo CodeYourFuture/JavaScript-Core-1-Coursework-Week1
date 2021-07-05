d65bf9e1b39cc3c73b6196d6bc1d659eca422827
Colette <83985997+TheCheekyGit@users.noreply.github.com>
2021-07-05 21:46:58 +0100

    I-floats

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,15 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var totalSM = numberOfStudents + numberOfMentors;
+
+//student calculation
+var perStudent = (12 * 100 / 23);
+var perStudent = Math.round(perStudent);
+var displayPerStudent = "Percentage student " + perStudent + "% ";
+console.log(displayPerStudent);
+
+//mentor calculation
+var perMentor = (8 * 100 / 23);
+var perMentor = Math.round(perMentor);
+var displayPerMentor = "Percentage mentor " + perMentor + "% ";
+console.log(displayPerMentor);
