e97d6821de4b489d4736b09831f6599801110605
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2021-07-08 23:18:07 +0100

    F-strings-methods
    exercises completed

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,6 @@
-const name = " Daniel  ";
+const name = "Benjamin";
 
+var nameTrimmed = name.trim();
+var nameLength = name.length;
+var message = "My name is "  + nameTrimmed + " and my name is " + nameLength + " characters long!";
 console.log(message);
