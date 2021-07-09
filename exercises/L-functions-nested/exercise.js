c33dfe39562c4f51985fd31534501c600a2a882f
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2021-07-09 19:32:05 +0100

    L-functions-nested
    exercise completed

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,13 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function shortGreeting(name){
+    return "Hello " + name.toUpperCase() +"!";
+}
+
+console.log(shortGreeting(mentor1));
+console.log(shortGreeting(mentor2));
+console.log(shortGreeting(mentor3));
+console.log(shortGreeting(mentor4));
+console.log(shortGreeting(mentor5));
