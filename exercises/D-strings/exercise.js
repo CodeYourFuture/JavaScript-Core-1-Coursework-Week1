1dd7974fc869672b5b6a1f03bf4a863a4f265a67
Omer249a <omer249aa@gmail.com>
2021-07-10 02:55:02 +0100

    finish

--- exercises/D-strings/exercise.js
+++ exercises/D-strings/exercise.js
@@ -1,3 +1,22 @@
 // Start by creating a variable `message`
 
-console.log(message);
+let messageChoice = 0;
+let messageType;
+let message = "";
+
+messageChoice = Math.round(Math.random() * 2);
+
+
+if (messageChoice == 0) {
+  message = "This is a string";
+} else if (messageChoice == 1) {
+  message = 25.5;
+} else {
+  message = true;
+}
+
+messageType = typeof message;
+
+console.log(`
+${message}
+${messageType}`);
