759e9c487be664d0e1a52f1f7d4516eb881fe053
socrateskld <77156381+socrateskld@users.noreply.github.com>
2021-07-18 01:46:57 +0300

    i float done

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,16 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+
+let percentValue = 100 / (numberOfStudents + numberOfMentors);
+
+let percentageStudents = Math.round(percentValue * numberOfStudents);
+let PercentageMentors = Math.round(percentValue * numberOfMentors);
+
+let result = `
+Percentage students: ${percentageStudents}%
+Percentage mentors: ${PercentageMentors}% 
+`;
+
+
+console.log(result);
