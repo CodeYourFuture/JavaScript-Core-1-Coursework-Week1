c4ce821c928bfe76cdfcab84a3040c54995702b9
dotdashdon <80584408+dotdashdon@users.noreply.github.com>
2021-08-07 18:51:08 +0100

    Completed Exercise I
    This was quite hard!

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,11 @@
-var numberOfStudents = 15;
-var numberOfMentors = 8;
+let numberOfStudents = 15;
+let numberOfMentors = 8;
+
+let total =numberOfStudents + numberOfMentors;
+console.log(`Total number of students and mentors: ${numberOfStudents + numberOfMentors}`);
+
+let percentageStudents =(100/total)*numberOfStudents;
+console.log(`Percentage of students: ${Math.round(percentageStudents)}`);
+
+let percentageMentors=(100/total)*numberOfMentors;
+console.log(`Percentage of mentors: ${Math.round(percentageMentors)}`);
