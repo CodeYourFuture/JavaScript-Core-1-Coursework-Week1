fb9159dd7b03907113e1d730f9ccf21174b3e57a
dotdashdon <80584408+dotdashdon@users.noreply.github.com>
2021-08-07 17:45:18 +0100

    Completed exercise F(i).

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,8 @@
 // Start by creating a variable `message`
+let myName ='Donna';
+let nameLength =myName.length
+console.log(myName.length);
 
-console.log(message);
+console.log(`My name is ${myName} and my name is ${myName.length} characters long.`);
+
+console.log(myName.trim);
