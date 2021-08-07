6c72665b77de4d3fb70ddc5f27795586b97e4e56
dotdashdon <80584408+dotdashdon@users.noreply.github.com>
2021-08-07 17:56:03 +0100

    Completed Exercise G

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,6 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+
+let numberOfStudents = 15;
+let numberOfMentors = 8;
+
+console.log(`There are ${numberOfStudents} students and ${numberOfMentors} mentors.`);
