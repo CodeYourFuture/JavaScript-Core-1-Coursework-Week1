376089e92146da858460583e6881cc3bffa76103
dotdashdon <80584408+dotdashdon@users.noreply.github.com>
2021-08-07 17:30:20 +0100

    Completed Exercise C

--- exercises/C-variables/exercise.js
+++ exercises/C-variables/exercise.js
@@ -1,3 +1,13 @@
 // Start by creating a variable `greeting`
-
+let greeting ='Hello world';
 console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+
+// Alternative using loop?
+
+let greeting1 ='Hello world';
+
+for(let i = 0; i <3;i++) {
+    console.log(greeting1);
+}
