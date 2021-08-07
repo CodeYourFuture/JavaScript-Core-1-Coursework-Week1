8c076f8c4d884e724d2b75db5ea204b3ee4b7123
dotdashdon <80584408+dotdashdon@users.noreply.github.com>
2021-08-07 17:36:09 +0100

    Completed Exercise E

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
+let greetingOpener = 'Hello my name is ';
+let myName = ' Donna.';
+let greeting = greetingOpener + myName;
 
-console.log(message);
+console.log(greeting);
