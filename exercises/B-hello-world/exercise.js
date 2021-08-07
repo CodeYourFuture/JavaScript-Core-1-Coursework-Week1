1ec16c5a137cc693ebf0822096a9816fe5f5961a
dotdashdon <80584408+dotdashdon@users.noreply.github.com>
2021-08-07 17:23:52 +0100

    Completed Exercise B

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,9 @@
 console.log("Hello world");
+//Experiment
+console.log("Hello world. I just started learning Javascript!");
+console.log("This");
+console.log("is");
+console.log("a");
+console.log("drill.");
+
+console.log("hello world");//error shown when run without quotes. 
