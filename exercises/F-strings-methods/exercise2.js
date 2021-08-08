6bc4aad2b4f0286d942150470d580f8104a87fee
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-08-08 23:03:52 +0100

    Update exercise2.js
    used string trim function and character length.

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,5 @@
-const name = " Daniel  ";
-
+const myName = " Daniel  ";
+const nameTrim = myName.trim();
+const nameLength = nameTrim.length;
+const message = `My name is ${nameTrim} and my name is ${nameLength} characters long.`;
 console.log(message);
