523c72886111ae967f5583c9f6d4015819990ac7
dotdashdon <80584408+dotdashdon@users.noreply.github.com>
2021-08-08 18:19:15 +0100

    Exercise L
    This took ages and was really confusing. I do not feel this kind of thing was covered in class.

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,5 +1,20 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+let mentor1 = " Daniel";
+let mentor2 = " Irina";
+let mentor3 = " Mimi";
+let mentor4 = " Rob";
+let mentor5 = " Yohannes";
+
+function shoutName (mentor) {
+    return mentor.toUpperCase();
+}
+
+function shoutGreeting (mentor) {
+    let greeting = 'HELLO';
+    return greeting  +  shoutName(mentor);
+}
+
+ console.log(shoutGreeting(mentor1));
+ console.log(shoutGreeting(mentor2));
+ console.log(shoutGreeting(mentor3));
+ console.log(shoutGreeting(mentor4));
+ console.log(shoutGreeting(mentor5));
