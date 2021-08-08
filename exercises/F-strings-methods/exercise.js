cfcba32f73a8bf2fc04819e9039e3af1e9324210
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-08-08 22:53:25 +0100

    Update exercise.js
    My name's character length is printed.

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
-
+let myName = "Tenzynski";
+let nameLength = myName.length;
+let message = `My name is ${myName} and my name is ${nameLength} characters long.`
 console.log(message);
