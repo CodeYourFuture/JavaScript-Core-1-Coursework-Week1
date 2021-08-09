fbd9593aa446e101024d77d4f4569d7d282b2bc4
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-08-09 15:43:48 +0100

    Update exercise.js
    G-numbers exercise completed

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,5 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+let numberOfStudents = 13;
+let numberOfMentors = 8;
+totalNum = numberOfMentors + numberOfStudents;
+console.log(`The total number of students and mentors is ${totalNum}.`)
