82d3c6248e7f9737e9c4152aeee1971cff5b8193
GemPearl <65920912+GemPearl@users.noreply.github.com>
2021-08-09 14:12:44 +0100

    Started ExerciseL
    function code not completed

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,5 +1,25 @@
-var mentor1 = "Daniel";
+let mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function upperCaseName(mentor1)
+{
+  let callName = mentor1.toLowerCase;
+  return callName;
+}
+console.log(upperCaseName);
+
+
+
+// function getAgeInDays(age) {
+//   return age * 365;
+// }
+
+// function createCreeting(name, age) {
+//   var ageInDays = getAgeInDays(age);
+//   var message =
+//     "My Name is " + name + " and I was born over " + ageInDays + " days ago!";
+//   return message;
+// }
