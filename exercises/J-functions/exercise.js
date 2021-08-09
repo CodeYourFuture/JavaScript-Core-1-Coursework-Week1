21b381440e51055c44c0eb04647a0be74975c197
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-08-09 16:08:43 +0100

    Function exercises updated
    2 exercises in J-function folder completed. Added function that halves a number and another function that triples a number.

--- exercises/J-functions/exercise.js
+++ exercises/J-functions/exercise.js
@@ -1,7 +1,11 @@
 function halve(number) {
   // complete the function here
+  return number/2;
 }
 
 var result = halve(12);
-
+let answer = halve(34);
+let output = halve(98);
 console.log(result);
+console.log(answer);
+console.log(output);
--- exercises/J-functions/exercise2.js
+++ exercises/J-functions/exercise2.js
@@ -1,7 +1,9 @@
 function triple(number) {
   // complete function here
+  return number * 3;
 }
 
 var result = triple(12);
-
+let answer = triple(49);
 console.log(result);
+console.log(answer);
