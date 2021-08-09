692bb05f7273f7f982219629df11039250b73214
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-08-09 21:09:14 +0100

    Nested function update
    Shouty greeting completed

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,16 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function getUpperCase(yourName){
+    return yourName.toUpperCase();
+}
+
+function shoutyGreeting(yourName){
+    return `HELLO ${getUpperCase(yourName)}`;
+}
+console.log(shoutyGreeting(mentor1));
+console.log(shoutyGreeting(mentor2));
+console.log(shoutyGreeting(mentor3));
+console.log(shoutyGreeting(mentor4));
+console.log(shoutyGreeting(mentor5));
