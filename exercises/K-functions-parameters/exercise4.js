5aa78fb301b70039877239cbc27c572a1b527391
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-08-09 14:58:04 +0100

    regular commit L needs fixing

--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,5 +1,8 @@
 // Declare your function first
-
+function sumOf(a, b){
+    return a+b
+}
 // Call the function and assign to a variable `sum`
+const sum = sumOf(13, 124);
 
 console.log(sum);
--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,18 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function upperCaseString(string){
+    return cap
+}
+
+function createShoutyGreeting(name){
+    let getNameUpper = upperCaseString(name)
+    return `HELLO, ${getNameUpper}`
+}
+
+console.log(createShoutyGreeting(mentor1));
+console.log(createShoutyGreeting(mentor2));
+console.log(createShoutyGreeting(mentor3));
+console.log(createShoutyGreeting(mentor4));
+console.log(createShoutyGreeting(mentor5));
