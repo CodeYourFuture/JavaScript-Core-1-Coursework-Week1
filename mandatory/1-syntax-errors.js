1602a0863bf45d7d2383d4a3aed50960890fddf1
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-08-09 20:20:42 +0100

    I believe I fixed the syntax errors
    however when I run the tests in the terminal it says there is 1 syntax issue and I can't figure out what it is.

--- mandatory/1-syntax-errors.js
+++ mandatory/1-syntax-errors.js
@@ -8,9 +8,9 @@ function introduceMe(name, age)
   return "Hello, my name is " + name + "and I am " + age + "years old";
 
 function getTotal(a, b) {
-  total = a ++ b;
+  let total = a + b;
 
-  return "The total is total";
+  return "The total is "+total;
 }
 
 /* 
