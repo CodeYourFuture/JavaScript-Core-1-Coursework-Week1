b932e4c434f2afee14fc3f26664955dda1af6c1b
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-08-09 15:56:04 +0100

    Update exercise.js
    Floats exercise completed

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,6 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+percentageStudent = Math.round(numberOfStudents *100/(numberOfStudents + numberOfMentors));
+percentageMentor =  Math.round(numberOfMentors*100/(numberOfMentors + numberOfStudents));
+console.log(`Percentage of students: ${percentageStudent}%`);
+console.log(`Percentage of mentors: ${percentageMentor}%`);
