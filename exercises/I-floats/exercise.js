6610613677bb00a4a4ae7314898a088ea5d943ad
Roman-Hal <85693082+Roman-Hal@users.noreply.github.com>
2021-08-13 23:18:03 +0100

    exercises_I_floats_exercise_done

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,11 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+let total = numberOfStudents + numberOfMentors;
+function percentageOf(num) {
+  var precisePercentage = (num / total) * 100;
+  let roughPercentage = Math.round(precisePercentage);
+  return roughPercentage;
+}
+
+console.log("Percentage students: " + percentageOf(numberOfStudents) + "%");
+console.log("Percentage mentors: " + percentageOf(numberOfMentors) + "%");
