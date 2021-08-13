21ac5568a22f3399cf70ee208a772c69fd4968d1
Haseeb3927 <haseeb3927@gmail.com>
2021-08-13 10:34:31 +0100

    Updated Nested function
    Updated the nested functions exercise.
    by reading the instructions again

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -4,25 +4,30 @@ var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
 
-function callGreeting(name) {
-  return "Hello " + name;
+function upperCaseName(name) {
+  let upperCase = name.toUpperCase();
+  return upperCase;
+}
+function shoutyGreeting(name) {
+  let capitalName = upperCaseName(name);
+  return "Hello " + capitalName;
 }
 //Hello Daniel
-let greeting1 = callGreeting(mentor1);
+let greeting1 = shoutyGreeting(mentor1);
 console.log(greeting1);
 
 //Hello Irina
-let greeting2 = callGreeting(mentor2);
+let greeting2 = shoutyGreeting(mentor2);
 console.log(greeting2);
 
 //Hello Mimi
-let greeting3 = callGreeting(mentor3);
+let greeting3 = shoutyGreeting(mentor3);
 console.log(greeting3);
 
 //Hello Rob
-let greeting4 = callGreeting(mentor4);
+let greeting4 = shoutyGreeting(mentor4);
 console.log(greeting4);
 
 //Hello Yohannes
-let greeting5 = callGreeting(mentor5);
+let greeting5 = shoutyGreeting(mentor5);
 console.log(greeting5);
