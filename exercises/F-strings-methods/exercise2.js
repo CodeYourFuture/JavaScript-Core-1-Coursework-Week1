af4ded33ada42e092ee6ac6ac9714aae6e39a6f2
Roman-Hal <85693082+Roman-Hal@users.noreply.github.com>
2021-08-13 22:46:00 +0100

    exercise_F_strings_methods_exercise2_done

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,18 @@
 const name = " Daniel  ";
-
+var noSpaceName = name.trim();
+var nameLength = noSpaceName.length;
+var messagePart;
+if (noSpaceName.length === 1) {
+  var messagePart = "character";
+} else {
+  var messagePart = "characters";
+}
+var message =
+  "My name is " +
+  noSpaceName +
+  " and my name is " +
+  nameLength +
+  " " +
+  messagePart +
+  " long.";
 console.log(message);
