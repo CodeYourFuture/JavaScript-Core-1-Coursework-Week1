27abb131fdad2b4e4ccb08901eaa2a37f1dba3dc
MireilleNyoubissi <nmireillemat@yahoo.fr>
2021-08-13 23:12:42 +0100

    corrected mandatory

--- mandatory/1-syntax-errors.js
+++ mandatory/1-syntax-errors.js
@@ -4,9 +4,11 @@ function addNumbers(a, b, c) {
   return a + b + c;
 }
 
-function introduceMe(name, age)
-  return "Hello, my name is " + name + "and I am " + age + "years old";
+function introduceMe(name, age){
+  return "Hello, my name is " + name + " and I am " + age + " years old";
 
+}
+  
 function getTotal(a, b) {
   var total = a + b;
 
