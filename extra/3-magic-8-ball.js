78623dc2418118ecce282a751a73c66dcfa85272
Halla-S <halla.sulaiman.33@gmail.com>
2021-08-13 23:23:14 +0100

    Change in extra 3

--- extra/3-magic-8-ball.js
+++ extra/3-magic-8-ball.js
@@ -45,29 +45,30 @@
 
 // This should log "The ball has shaken!"
 // and return the answer.
-let expectedAnswers = [
-  "It is certain.",
-  "It is decidedly so.",
-  "Without a doubt.",
-  "Yes - definitely.",
-  "You may rely on it.",
-  "As I see it, yes.",
-  "Most likely.",
-  "Outlook good.",
-  "Yes.",
-  "Signs point to yes.",
-  "Reply hazy, try again.",
-  "Ask again later.",
-  "Better not tell you now.",
-  "Cannot predict now.",
-  " Concentrate and ask again.",
-  "Don't count on it.",
-  "My reply is no.",
-  " My sources say no.",
-  "Outlook not so good.",
-  "Very doubtful.",
-];
+
 function shakeBall() {
+  expectedAnswers = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    " Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    " My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful.",
+  ];
   console.log("The ball has shaken!");
   let random1 = Math.floor(Math.random() * 20);
   let answer = expectedAnswers[random1];
