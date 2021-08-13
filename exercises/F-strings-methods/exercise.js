b5cc1149e8c0366c960a30fc1200e293453e949c
Roman-Hal <85693082+Roman-Hal@users.noreply.github.com>
2021-08-13 22:37:13 +0100

    exercise_F_strings_method_exercise1_done

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,18 @@
 // Start by creating a variable `message`
-
+var name = "Daniel";
+var nameLength = name.length;
+var messagePart;
+if (name.length === 1) {
+  var messagePart = "character";
+} else {
+  var messagePart = "characters";
+}
+var message =
+  "My name is " +
+  name +
+  " and my name is " +
+  nameLength +
+  " " +
+  messagePart +
+  " long.";
 console.log(message);
