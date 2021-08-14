ef774857cc20917cef0991a6ec77dc0db83bf34c
Roman-Hal <85693082+Roman-Hal@users.noreply.github.com>
2021-08-14 01:10:33 +0100

    extra_1_and_2

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -5,7 +5,9 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(num) {
+  return num * 1.4;
+}
 
 /*
   CURRENCY CONVERSION
@@ -15,12 +17,15 @@ function convertToUSD() {}
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL() {}
+function convertToBRL(num) {
+  let result = num * 0.99 * 5.7;
+  return Number(result.toFixed(2));
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
 
-To run these tests type `npm run extraTo run the tests for just this one file, type `npm run extra-tests -- --testPathPattern 1-syntax-errors` into your terminal
+To run these tests type `npm run extraTo run the tests for just this one file, type `npm run extra-tests -- --testPathPattern 1-syntax-errors` correct path: `npm run extra-tests -- --testPathPattern 1-currency-conversion` into your terminal
 (Reminder: You must have run `npm install` one time before this will work!)
 */
 
