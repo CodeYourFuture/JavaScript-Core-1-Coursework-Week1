acddef1f3a46cff447edb4105cf5c71c2521f382
Sineads-Osb <77743497+Sinead-CYF@users.noreply.github.com>
2021-08-15 15:40:32 +0100

    Updated exercise F

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,12 +1,11 @@
 // Start by creating a variable `message`
-var message = 'My name is Sinead and my name is ';
-var name = "Sinead"
+var message = "My name is Sinead and my name is ";
+var name = "Sinead";
 var nameLength = name.length;
 console.log(message);
 console.log(nameLength);
 
-
-//string method command example - make name lower case 
+//string method command example - make name lower case
 
 var nameLowerCase = name.toLowerCase();
-console.log(nameLowerCase); 
+console.log(nameLowerCase);
--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,5 +1,6 @@
-const name = " Daniel  ";
+const myName = " Daniel  ";
+const nameTrimmed = myName.trim();
 
-console.log(message);
+console.log(nameTrimmed);
 
-//Unsure if how I can display the name length mid sentence, or do I need to create another string afterwards? Also trim what white space?
+//Unsure how I can display the name length mid sentence, or do I need to create another string afterwards? Also trim what white space?
