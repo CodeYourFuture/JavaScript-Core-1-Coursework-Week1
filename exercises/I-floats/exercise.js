1d0a32d1e5a596436f7df3677edd52961dc0450e
justsealan <justsealan@gmail.com>
2021-08-17 16:10:04 +0100

    Complete exercise I

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,13 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+var percentageOfStudents = (numberOfStudents * 100) / (numberOfStudents + numberOfMentors);
+var percentageOfMentors =
+  (numberOfMentors * 100) / (numberOfStudents + numberOfMentors);
+
+console.log( "Percantage students: " +
+    Math.round(percentageOfStudents) 
+    + "%" );
+console.log( "Percentage mentors: " +
+    Math.round(percentageOfMentors)
+    + "%" );
