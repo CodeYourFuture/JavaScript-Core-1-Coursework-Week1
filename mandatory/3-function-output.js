5afddb91be03e08f468c99f46db6656930d00755
justsealan <justsealan@gmail.com>
2021-08-17 23:31:07 +0100

    Complete 3-function-output

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,9 +1,12 @@
 // Add comments to explain what this function does. You're meant to use Google!
+//This function returns a random number between 0 (inclusive),  and 1 (exclusive):
 function getRandomNumber() {
   return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+//This function concatenates two or more strings, and returns a new joined strings
+
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
@@ -11,6 +14,8 @@ function combine2Words(word1, word2) {
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+return firstWord + " ".concat(secondWord) + " ".concat(thirdWord);
+
 }
 
 /* 
