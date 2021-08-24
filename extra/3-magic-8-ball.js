260030ee024612e2308ffc00a978875aff6b11fe
justsealan <justsealan@gmail.com>
2021-08-24 15:54:18 +0100

    Complete Magic 8 Ball

--- extra/3-magic-8-ball.js
+++ extra/3-magic-8-ball.js
@@ -47,6 +47,27 @@
 // and return the answer.
 function shakeBall() {
   //Write your code in here
+  //  seenAnswers = ["Very positive", "It is certain.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
+  //  let randomNumber = Math.floor(Math.random() * seenAnswers.length);
+  //  return seenAnswers[randomNumber];
+
+  let veryPositiveAnswers = ["It is certain.","It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it."];
+  let positiveAnswers = ["As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes."];
+  let negativeAnswers = ["Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again."];
+  let veryNegativeAnswers = ["Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
+
+  let randomNumber = Math.floor(Math.random() * veryPositiveAnswers.length);
+ 
+    let randomAnswer = [
+      veryPositiveAnswers[randomNumber],
+      positiveAnswers[randomNumber],
+      negativeAnswers[randomNumber],
+      veryNegativeAnswers[randomNumber],
+    ];
+
+  let randomNumber2 = Math.floor(Math.random() * randomAnswer.length);
+  console.log("The ball has shaken!");
+  return randomAnswer[randomNumber2];
 }
 
 /* 
@@ -60,6 +81,19 @@ function shakeBall() {
 */
 function checkAnswer(answer) {
   //Write your code in here
+  if (answer === "It is certain." || answer === "It is decidedly so." || answer === "Without a doubt." || answer === "Yes - definitely." || answer === "You may rely on it.") {
+    return "very positive";
+  }
+  else if (answer === "As I see it, yes." || answer === "Most likely." || answer === "Outlook good." || answer === "Yes." || answer === "Signs point to yes.") {
+    return "positive";
+  }
+  else if (answer === "Reply hazy, try again." || answer === "Ask again later." || answer === "Better not tell you now." || answer === "Cannot predict now." || answer === "Concentrate and ask again.") {
+    return "negative";
+  }
+  else if (answer === "Don't count on it." || answer === "My reply is no." || answer === "My sources say no." || answer === "Outlook not so good." || answer === "Very doubtful.") {
+    return "very negative";
+  }
+
   
 }
 
