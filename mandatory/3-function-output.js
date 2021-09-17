862e4386560e4ef0c782f1acc20042ba1752502c
itsrahmaa <r.gulaid@hotmail.com>
2021-09-18 00:26:24 +0100

    Mandatory folder
    exercises completed for mandatory folder

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,5 +1,5 @@
 // Add comments to explain what this function does. You're meant to use Google!
-function getRandomNumber() {
+function getNumber() {
   return Math.random() * 10;
 }
 
@@ -11,8 +11,8 @@ function combine2Words(word1, word2) {
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+return firstWord.concat('', secondWord, '', thirdWord);
 }
-
 /* 
 ===================================================
 ======= TESTS - DO NOT MODIFY BELOW THIS LINE =====
