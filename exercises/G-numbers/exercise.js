94f93731db67cc9d15010cad7360cc3330c96de1
Marina Almeida <67846587+MarinaAlmeida20@users.noreply.github.com>
2021-12-12 19:48:47 +0000

    g-numbers

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,6 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+let numberOfStudents = 15;
+let numberOfMentors = 8;
+let total = numberOfMentors + numberOfStudents;
+let message = `Total number of mentors and students: ${total}`;
+console.log(message);
