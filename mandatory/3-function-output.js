92816eadb893064f7203d32625b9c95769c538db
KatarzynaRuksza <91133235+KatarzynaRuksza@users.noreply.github.com>
2021-12-12 23:31:13 +0000

    Update 3-function-output.js

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -2,15 +2,16 @@
 function getRandomNumber() {
   return Math.random() * 10;
 }
+// Function getRandomNumber between 0 and 1 and multiplies it by 10. //
 
 // Add comments to explain what this function does. You're meant to use Google!
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
+//This function is used to connect strings together into one new string. //
 
 function concatenate(firstWord, secondWord, thirdWord) {
-  // Write the body of this function to concatenate three words together.
-  // Look at the test case below to understand what this function is expected to return.
+  return firstWord.concat(thirdWord);
 }
 
 /* 
