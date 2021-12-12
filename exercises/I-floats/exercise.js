1ec0691394f240432e7ca6fb7e02a88fca83fdad
KatarzynaRuksza <91133235+KatarzynaRuksza@users.noreply.github.com>
2021-12-12 21:37:02 +0000

    Update exercise.js

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,23 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var totalNumber = 23;
+
+const quotient = numberOfStudents / totalNumber;
+const product = quotient * 100;
+
+console.log(product);
+
+const quotientTwo = numberOfMentors / totalNumber;
+const productTwo = quotientTwo * 100;
+
+console.log(productTwo);
+
+var precisePercentage = product;
+var roughPercentage = Math.round(precisePercentage);
+
+console.log(roughPercentage);
+
+var precisePercentageTwo = productTwo;
+var roughPercentageTwo = Math.round(precisePercentageTwo);
+
+console.log(roughPercentageTwo);
