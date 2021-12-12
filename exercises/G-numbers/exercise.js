7a5646a9736924762dccb49d60d5e52f5fe1d418
KatarzynaRuksza <91133235+KatarzynaRuksza@users.noreply.github.com>
2021-12-12 21:06:08 +0000

    Update exercise.js

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,14 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 15;
+var numberOfMentors = 8;
+var totalNumberOfStudents = 23;
+
+var message =
+  "Number of students:" +
+  numberOfStudents +
+  "Number of mentors:" +
+  numberOfMentors +
+  "Total number of mentors and students:" +
+  totalNumberOfStudents;
+
+console.log(message);
