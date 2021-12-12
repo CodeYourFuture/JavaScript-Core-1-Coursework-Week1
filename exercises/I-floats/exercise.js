45425a8be3de12a6383cbbadcb57013d4cb1c477
Marina Almeida <67846587+MarinaAlmeida20@users.noreply.github.com>
2021-12-12 20:14:17 +0000

    I-floats

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,13 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+let total = numberOfMentors + numberOfStudents;
+
+let percentageOfMentors = Math.round((100 / total) * numberOfMentors);
+let percentageOfStudents = Math.round((100 / total) * numberOfStudents);
+
+let messageStudents = `Percentage students: ${percentageOfStudents}%`;
+let messageMentors = `Percentage mentors: ${percentageOfMentors}%`;
+
+console.log(messageStudents);
+console.log(messageMentors);
