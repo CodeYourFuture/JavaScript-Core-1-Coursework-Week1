37bc4a748edaf3a231f5c5cec6927f664b85bbf4
Marina Almeida <67846587+MarinaAlmeida20@users.noreply.github.com>
2021-12-12 19:11:31 +0000

    hello-world/exercise

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,9 @@
 console.log("Hello world");
+console.log("Hello World. I just started learning JavaScript!");
+const MY_NAME = "Marina";
+console.log(`Hello My name is ${MY_NAME}.`);
+console.log(10 + 2);
+console.log(3 * 9);
+console.log((40 / 6).toFixed(2));
+console.log(54 - 12);
+console.log(4 % 2);
