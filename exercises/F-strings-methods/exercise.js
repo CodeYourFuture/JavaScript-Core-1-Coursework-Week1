672728b4d7abf5ac160b0e61cd67ef87055aebf8
Marina Almeida <67846587+MarinaAlmeida20@users.noreply.github.com>
2021-12-12 19:43:42 +0000

    f-strings-methods

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,4 @@
 // Start by creating a variable `message`
-
+let name = "Marina";
+let message = `My name is ${name} and my name is ${name.length} characters long.`;
 console.log(message);
--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,5 @@
 const name = " Daniel  ";
-
+let message = `My name is ${name.trim()} and my name is ${
+  name.length
+} characters long.`;
 console.log(message);
