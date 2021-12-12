ea4f9bbead18e034b0d1684535bc10aff8eb9551
Marina Almeida <67846587+MarinaAlmeida20@users.noreply.github.com>
2021-12-12 19:35:03 +0000

    E-strings-concatenation

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,9 @@
 // Start by creating a variable `message`
-
+let greetingStart = "Hello, My name is ";
+const HIS_NAME = "Daniel";
+let message = greetingStart + HIS_NAME;
 console.log(message);
+
+const MY_NAME = "Marina";
+let message2 = greetingStart + MY_NAME;
+console.log(message2);
