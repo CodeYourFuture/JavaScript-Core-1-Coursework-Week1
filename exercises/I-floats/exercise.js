20670abc5be2a57782a75adbdabad036302c6ef5
Selcuk Karakus <selcukkarakus@Selcuks-MBP.lan>
2021-12-13 15:05:25 +0000

    Create percentage formula using Math Object.

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,11 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+// - Using the variables provided in the exercise calculate the percentage of mentors and students in the group //
+
+let total = numberOfStudents + numberOfMentors;
+let percentageOfStudents = Math.round((numberOfStudents / total) * 100);
+let percentageOfMentors = Math.round((numberOfMentors / total) * 100);
+
+console.log(`Percentage students: ${percentageOfStudents}%`);
+console.log(`Percentage mentors: ${percentageOfMentors}%`);
