64f952be5ec6ac2b385831edfa426638abbdee4e
Selcuk Karakus <selcukkarakus@Selcuks-MBP.lan>
2021-12-13 17:03:43 +0000

    Create a function that calls another function.

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,5 +1,24 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+// - In `exercise.js` you have been provided with the names of some mentors. Write a program that logs a shouty greeting to each one.
+// - Your program should include a function that spells their name in uppercase, and a function that creates a shouty greeting.
+// - Log each greeting to the console.
+
+let mentor1 = "Daniel";
+let mentor2 = "Irina";
+let mentor3 = "Mimi";
+let mentor4 = "Rob";
+let mentor5 = "Yohannes";
+
+const allCapitals = (namePerson) => {
+  return namePerson.toUpperCase();
+}
+
+const greetMentors = (mentor) => {
+  let upperCaseName = allCapitals(mentor)
+  return `A big shout out to ${upperCaseName}. You rock!`;
+}
+
+console.log(greetMentors(mentor1));
+console.log(greetMentors(mentor2));
+console.log(greetMentors(mentor3));
+console.log(greetMentors(mentor4));
+console.log(greetMentors(mentor5));
