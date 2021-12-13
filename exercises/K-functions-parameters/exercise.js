bc4e59de9133e17646926c1184c06a8dad20ac8d
Selcuk Karakus <selcukkarakus@Selcuks-MBP.lan>
2021-12-13 16:26:54 +0000

    Create a function that multiplies two numbers.

--- exercises/K-functions-parameters/exercise.js
+++ exercises/K-functions-parameters/exercise.js
@@ -1,9 +1,12 @@
 // Complete the function so that it takes input parameters
-function multiply() {
+// - Write a function that multiplies two numbers together
+
+const multiply = (number1, number2) => {
   // Calculate the result of the function and return it
+  return number1 * number2;
 }
 
 // Assign the result of calling the function the variable `result`
-var result = multiply(3, 4);
+let result = multiply(3, 4);
 
 console.log(result);
