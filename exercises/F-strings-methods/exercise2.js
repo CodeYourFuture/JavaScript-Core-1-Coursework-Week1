2b89080e53d27376b472ddec03b9a2a451c81d2f
Selcuk Karakus <selcukkarakus@Selcuks-MBP.lan>
2021-12-13 14:22:22 +0000

    Create a program that removes white space.

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,14 @@
 const name = " Daniel  ";
 
-console.log(message);
+const trimWhiteSpace = (name) => {
+  let nameTrimmed = name.trim();
+  let nameLength = nameTrimmed.length;
+
+  let message = `My name is ${nameTrimmed} and my name is ${nameLength} characters long`;
+
+  return message;
+};
+
+let result = trimWhiteSpace(name);
+
+console.log(result);
