c22078bf9bcc6c48ffdf8816c3eef479f47b1821
Selcuk Karakus <selcukkarakus@Selcuks-MBP.lan>
2021-12-13 13:36:52 +0000

    Add and print variable multiple times.

--- exercises/C-variables/exercise.js
+++ exercises/C-variables/exercise.js
@@ -1,3 +1,9 @@
-// Start by creating a variable `greeting`
+// Start by creating a variable `greeting` //
+// - Add a variable `greeting` to exercise.js (make sure it comes _before_ the console.log) //
+let greeting = "Welcome to the jungle!";
 
+// - Print your `greeting` to the console 3 times //
 console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+
