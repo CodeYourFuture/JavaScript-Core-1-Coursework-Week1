8478fe366a283f27902bd8e4d2c50019f9ff57c9
Selcuk Karakus <selcukkarakus@Selcuks-MBP.lan>
2021-12-13 13:48:35 +0000

    Create a program that returns data type.

--- exercises/D-strings/exercise.js
+++ exercises/D-strings/exercise.js
@@ -1,3 +1,12 @@
-// Start by creating a variable `message`
-
+// Start by creating a variable `message` //
+let message = "This is a string";
 console.log(message);
+
+// - Write a program that logs a message and its type //
+const returnDataType = (data) => {
+  return `This is a data type of ${typeof data}`;
+}
+
+let result = returnDataType(message);
+console.log(result);
+
