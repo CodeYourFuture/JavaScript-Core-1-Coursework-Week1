5552068156fbcce10ea0bbad3cf39b9403bef0a1
Selcuk Karakus <selcukkarakus@Selcuks-MBP.lan>
2021-12-13 16:49:21 +0000

    Create a function that greets users.

--- exercises/K-functions-parameters/exercise5.js
+++ exercises/K-functions-parameters/exercise5.js
@@ -1,4 +1,9 @@
 // Declare your function here
+// - Write a function that takes a name (a string) and an age (a number) and returns a greeting (a string)
+const createLongGreeting = (personName, age) => {
+  return `Hello, my name is ${personName} and I'm ${age} years old`
+
+}
 
 const greeting = createLongGreeting("Daniel", 30);
 
