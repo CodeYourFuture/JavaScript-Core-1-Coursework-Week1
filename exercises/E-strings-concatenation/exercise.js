6bce069ea5f44e2cb77c1083bbad7a62a86b709a
Selcuk Karakus <selcukkarakus@Selcuks-MBP.lan>
2021-12-13 13:57:35 +0000

    Create a program that returns a greeting message.

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,12 @@
-// Start by creating a variable `message`
-
+// Start by creating a variable `message` //
+let message = "Hello, my name is ";
 console.log(message);
+
+// - Write a program that logs a message with a greeting and your name //
+const greetPerson = (person) => {
+  return message + person;
+}
+
+let personOne = "Daniel";
+let result = greetPerson(personOne);
+console.log(result);
