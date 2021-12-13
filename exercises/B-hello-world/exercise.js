f2657fc1e2e9286a7f64536cf5bbfe70b66a9304
Selcuk Karakus <selcukkarakus@Selcuks-MBP.lan>
2021-12-13 13:15:25 +0000

    Experiment with console.log

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,26 @@
-console.log("Hello world");
+// - Try to `console.log()` something different. For example, 'Hello World. I just started learning JavaScript!'. //
+// Terminal prints out every character wrapped in between the quotes //
+console.log("Hello World. I just started learning JavaScript!");
+
+// - Try to console.log() several things at once. //
+// Terminal prints out everything in the same line //
+console.log(
+  'It\'s Selchuk Karakus here.',
+  "I'm experimenting with console.log()",
+  12345,
+  { key: "Value" },
+  [1, "two", { three: 3 }, true, false, null],
+  undefined,
+  null
+);
+
+// - What happens when you get rid of the quote marks? //
+// - Terminal throws a syntax error //
+console.log(Hello world!);
+
+// - What happens when you console.log() just a number without quotes? //
+// Terminal prints out the number //
+console.log(12345);
+// Single quote use //
+
+
