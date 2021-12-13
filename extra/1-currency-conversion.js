d3026713a169acf82ac53763008e82a9ef299560
Selcuk Karakus <selcukkarakus@Selcuks-MBP.lan>
2021-12-13 22:42:37 +0000

    Refactor, convert func into arrow func.

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -5,7 +5,7 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD(amount) {
+const convertToUSD = (amount) => {
   return amount * 1.4;
 }
 
@@ -17,7 +17,7 @@ function convertToUSD(amount) {
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL(amount) {
+const convertToBRL = (amount) => {
   let deductionFee = amount / 100;
   let poundToConvert = amount - deductionFee;
   let payOut = +(poundToConvert * 5.7).toFixed(2);
