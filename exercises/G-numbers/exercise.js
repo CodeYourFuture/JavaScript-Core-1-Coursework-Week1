92398d9e7161cf30aee85db79d489dbadddc6ffc
Selcuk Karakus <selcukkarakus@Selcuks-MBP.lan>
2021-12-13 14:33:10 +0000

    Create a program that returns the total class size.

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,22 @@
-// Start by creating a variables `numberOfStudents` and `numberOfMentors`
+// Start by creating a variables `numberOfStudents` and `numberOfMentors` //
+let numberOfStudents;
+let numberOfMentors;
+
+// - Create two variables `numberOfStudents` and `numberOfMentors` //
+numberOfStudents = 15;
+console.log(`Number of students: ${numberOfStudents}`);
+
+numberOfMentors = 8;
+console.log(`Number of mentors: ${numberOfMentors}`);
+
+// - Log a message that displays the total number of students and mentors //
+const sumOfClass = (students, mentors) => {
+  let total = students + mentors;
+
+  return `Total number of students and mentors: ${total}`;
+}
+
+let result = sumOfClass(numberOfStudents, numberOfMentors);
+console.log(result);
+
+
