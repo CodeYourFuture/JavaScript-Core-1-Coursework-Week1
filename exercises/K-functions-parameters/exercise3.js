9c7677957bbba12b6544c83e93413044c7959fc1
Selcuk Karakus <selcukkarakus@Selcuks-MBP.lan>
2021-12-13 16:33:21 +0000

    Create a function that greets users.

--- exercises/K-functions-parameters/exercise3.js
+++ exercises/K-functions-parameters/exercise3.js
@@ -1,5 +1,10 @@
 // Write your function here
+// - Write a function that takes a name (a string) and returns a greeting
+const createGreeting = (nameOfPerson) => {
+  return `Hello, my name is ${nameOfPerson}`;
+}
 
-var greeting = createGreeting("Daniel");
+
+let greeting = createGreeting("Daniel");
 
 console.log(greeting);
