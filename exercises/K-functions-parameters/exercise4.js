2f0273bef5c79873e36321908c84ac4dbfac20d2
Selcuk Karakus <selcukkarakus@Selcuks-MBP.lan>
2021-12-13 16:43:50 +0000

    Create a function that adds two inputs.

--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,5 +1,12 @@
+// - Write a function that adds two numbers together
+// - Call the function, passing `13` and `124` as parameters, and assigning the returned value to a variable `sum`
 // Declare your function first
 
+const getTotal = (number1, number2) => {
+  return number1 + number2;
+}
+
 // Call the function and assign to a variable `sum`
+let sum = getTotal(13, 124);
 
 console.log(sum);
