ea8fbce17b4ac58ce655d9f583a0020e1bb385ad
craig-dsilva <90406619+craig-dsilva@users.noreply.github.com>
2021-12-14 11:41:28 +0000

    2 of 3 extras complete

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -5,7 +5,9 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(amount) {
+  return 1.4 * amount;
+}
 
 /*
   CURRENCY CONVERSION
@@ -15,7 +17,13 @@ function convertToUSD() {}
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL() {}
+function convertToBRL(amount) {
+  let fee = (amount * 1) / 100;
+  let withFee = amount - fee;
+  let withFeeConverted = withFee * 5.7;
+  let stringTotal = withFeeConverted.toFixed(2);
+  return parseFloat(stringTotal);
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
--- extra/2-piping.js
+++ extra/2-piping.js
@@ -16,27 +16,28 @@
   the final result to the variable goodCode
 */
 
-function add() {
-
+function add(a, b) {
+  return a + b;
 }
 
-function multiply() {
-
+function multiply(a, b) {
+  return a * b;
 }
 
-function format() {
-
+function format(number) {
+  return `£${number.toString()}`;
 }
 
 const startingValue = 2;
 
 // Why can this code be seen as bad practice? Comment your answer.
-let badCode =
+let badCode = format(multiply(add(startingValue, 10), 2));
 
 /* BETTER PRACTICE */
-
-let goodCode =
-
+let sum = add(startingValue, 10);
+let product = multiply(sum, 2);
+let formatted = `£${product}`;
+let goodCode = formatted;
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
 
