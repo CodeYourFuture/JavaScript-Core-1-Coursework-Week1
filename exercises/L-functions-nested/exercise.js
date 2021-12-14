e34dff100fa75695383e577895214999f2fcfe62
hachi-ops <67706415+hachi-ops@users.noreply.github.com>
2021-12-14 03:39:29 +0000

    exercise: J-L

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,5 +1,32 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+    var mentor1 = "Daniel";
+    var mentor2 = "Irina";
+    var mentor3 = "Mimi";
+    var mentor4 = "Rob";
+    var mentor5 = "Yohannes";
+
+function helloGreeting(name) {
+    return name;     
+}       
+    function upperCaseGreeting(helloGreeting) {
+        return ("Hello " + helloGreeting).toUpperCase();   
+}
+var greeting = helloGreeting;
+var shoutyGreeting = upperCaseGreeting;
+
+
+// console.log(greeting(mentor1));
+// console.log(greeting(mentor2));
+// console.log(greeting(mentor3));
+// console.log(greeting(mentor4));
+// console.log(greeting(mentor5));
+
+console.log(shoutyGreeting(mentor1));
+console.log(shoutyGreeting(mentor2));
+console.log(shoutyGreeting(mentor3));
+console.log(shoutyGreeting(mentor4));
+console.log(shoutyGreeting(mentor5));
+
+
+// - In `exercise.js` you have been provided with the names of some mentors. Write a program that logs a shouty greeting to each one.
+// - Your program should include a function that spells their name in uppercase, and a function that creates a shouty greeting.
+// - Log each greeting to the console
