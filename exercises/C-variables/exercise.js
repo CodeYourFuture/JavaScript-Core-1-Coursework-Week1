2390ccce102a43948bd1134c180dd242189353e4
mairacagri <70014030+mairacagri@users.noreply.github.com>
2021-12-15 15:45:30 +0000

    1-syntax-error week 1

--- exercises/C-variables/exercise.js
+++ exercises/C-variables/exercise.js
@@ -1,3 +1,7 @@
 // Start by creating a variable `greeting`
-
+let greeting= "hello";
 console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+
+
--- exercises/D-strings/exercise.js
+++ exercises/D-strings/exercise.js
@@ -1,3 +1,3 @@
 // Start by creating a variable `message`
-
-console.log(message);
+let message = "";
+console.log(typeof message);
