9f1448a84ab9ec0ea4932f212d80fc71b28b65fd
Margarita Glushkova <m.glushkova1@gmail.com>
2021-12-16 18:56:09 +0000

    local variable
    made variable answers local (moved inside the function)

--- extra/3-magic-8-ball.js
+++ extra/3-magic-8-ball.js
@@ -44,30 +44,30 @@
 */
 
 // This should log "The ball has shaken!"
-// and return the answer.
-let answers = [
-  "It is certain",
-  "It is decidedly so",
-  "Without a doubt",
-  "Yes - definitely",
-  "You may rely on it",
-  "As I see it, yes",
-  "Most likely",
-  "Outlook good",
-  "Yes",
-  "Signs point to yes",
-  "Reply hazy, try again",
-  "Ask again later",
-  "Better not tell you now",
-  "Cannot predict now",
-  "Concentrate and ask again",
-  "Don't count on it",
-  "My reply is no",
-  "My sources say no",
-  "Outlook not so good",
-  "Very doubtful",
-];
+// and return the answer
 function shakeBall() {
+  let answers = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes - definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+  ];
   let index = Math.floor(Math.random() * answers.length);
   let answer = answers[index];
   console.log(answer);
