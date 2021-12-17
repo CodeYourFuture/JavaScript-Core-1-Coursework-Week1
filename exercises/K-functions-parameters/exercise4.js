fe7b389ba4748538a343d0758e0b92d56a650635
Teniolao <tennybetty100@gmail.com>
2021-12-17 07:04:55 +0000

    k-function exer 4

--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,5 +1,8 @@
 // Declare your function first
-
+function sum(num1, num2){
+ return num1 + num2;
+}
 // Call the function and assign to a variable `sum`
+let result = sum(13, 124);
+console.log(result);
 
-console.log(sum);
