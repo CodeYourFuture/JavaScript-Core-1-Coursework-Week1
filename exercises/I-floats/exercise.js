55b5323faf3b4f786a1e2b81b83f912a9777db87
Teniolao <tennybetty100@gmail.com>
2021-12-17 05:58:50 +0000

    l-floats

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,17 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+let totalNumber = numberOfStudents + numberOfMentors;
+
+let quotientOfStudents = numberOfStudents / totalNumber;
+let quotientOfMentors = numberOfMentors / totalNumber;
+
+let productOfStudents = quotientOfStudents * 100;
+let productOfMentors = quotientOfMentors * 100;
+
+let percentageOfStudents = productOfStudents;
+let percentageOfMentors = productOfMentors;
+
+console.log(`Percentage students: ${Math.round(percentageOfStudents)}%`)
+console.log(`Percentage mentors: ${Math.round(percentageOfMentors)}%`)
+
+
