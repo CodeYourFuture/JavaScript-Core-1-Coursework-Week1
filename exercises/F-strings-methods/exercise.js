10fccb159de35fe4df4716a090dea1df0b57a257
Teniolao <tennybetty100@gmail.com>
2021-12-17 05:12:14 +0000

    f-strings

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,7 @@
 // Start by creating a variable `message`
-
+let intro = "My name is ";
+let myName = "Teniola";
+let myNameLength = myName.length;
+let introLowerCase = intro.toLowerCase();
+let message = intro + myName + " and " + introLowerCase + myNameLength + " characters long";
 console.log(message);
