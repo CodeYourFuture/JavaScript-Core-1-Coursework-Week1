4b44777940e55ec76c0ac9bd2e66d723fba30985
Teniolao <tennybetty100@gmail.com>
2021-12-17 05:20:57 +0000

    f-strings exercise 2

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,6 @@
-const name = " Daniel  ";
-
+const myName = " Teniola  ";
+let intro = "My name is ";
+let myNameLength = myName.length;
+let introLowerCase = intro.toLowerCase();
+let message = intro + myName.trim() + " and " + introLowerCase + myNameLength + " characters long";
 console.log(message);
