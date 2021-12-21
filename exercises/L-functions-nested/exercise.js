7e55788dd250574fec85d75b3861c0b3707ea880
Teniolao <tennybetty100@gmail.com>
2021-12-21 21:56:43 +0000

    L-function

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,11 +1,26 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+let mentor1 = "Daniel";
+let mentor2 = "Irina";
+let mentor3 = "Mimi";
+let mentor4 = "Rob";
+let mentor5 = "Yohannes";
 
-function greeting (){
-    return Hello;
+function greeting (mentorName){
+    let capitalizedName = uppercase(mentorName);
+    return "HELLO" + capitalizedName;
 }
 
-console.log (greeting + mentor1)
+function uppercase(str){
+    return str.toUpperCase();
+}
+
+const greetMentor1= greeting(mentor1);
+const greetMentor2 = greeting(mentor2);
+const greetMentor3 = greeting(mentor3);
+const greetMentor4 = greeting(mentor4);
+const greetMentor5 = greeting(mentor5);
+
+ console.log(greetMentor1)
+ console.log(greetMentor2)
+ console.log(greetMentor3)
+ console.log(greetMentor4)
+ console.log(greetMentor5)
