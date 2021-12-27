c2075797657bb7bcefb4143fef0715d6a10c1552
Simon Lawrence <90318503+SimonLawrenceGB@users.noreply.github.com>
2021-12-27 21:07:21 +0000

    Mandatory Complete
    Function output does not pass, I don't know why.

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -4,24 +4,41 @@ var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
 
-function shout(hello) {
-  return hello;
+// function shout(hello) {
+//   return hello;
+// }
+
+// let result = shout("HELLO ");
+
+// function mentors(m1, m2, m3, m4, m5) {
+//   console.log(result + m1);
+//   console.log(result + m2);
+//   console.log(result + m3);
+//   console.log(result + m4);
+//   console.log(result + m5);
+// }
+
+// mentors(
+//   mentor1.toUpperCase(),
+//   mentor2.toUpperCase(),
+//   mentor3.toUpperCase(),
+//   mentor4.toUpperCase(),
+//   mentor5.toUpperCase()
+// );
+
+// =====Alternative=====
+
+function capitalNames(names) {
+  return names.toUpperCase();
 }
 
-let result = shout("HELLO ");
-
-function mentors(m1, m2, m3, m4, m5) {
-  console.log(result + m1);
-  console.log(result + m2);
-  console.log(result + m3);
-  console.log(result + m4);
-  console.log(result + m5);
+function shoutMentors(mentor) {
+  let greetMentor = capitalNames(mentor);
+  return `HELLO ${greetMentor}`;
 }
 
-mentors(
-  mentor1.toUpperCase(),
-  mentor2.toUpperCase(),
-  mentor3.toUpperCase(),
-  mentor4.toUpperCase(),
-  mentor5.toUpperCase()
-);
+console.log(shoutMentors(mentor1));
+console.log(shoutMentors(mentor2));
+console.log(shoutMentors(mentor3));
+console.log(shoutMentors(mentor4));
+console.log(shoutMentors(mentor5));
--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,16 +1,17 @@
 // Add comments to explain what this function does. You're meant to use Google!
 function getRandomNumber() {
   return Math.random() * 10;
-}
+} // This function generates a random number between 0 and 1 (including the number 0 but not the number 1). It then multiplies that number by 10.
 
 // Add comments to explain what this function does. You're meant to use Google!
 function combine2Words(word1, word2) {
   return word1.concat(word2);
-}
+} // This function combines two different words into one. The end result being that "word1" and "word2" now become "word1word2".
 
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+  return firstWord.concat(secondWord) + secondWord.concat(thirdWord);
 }
 
 /* 
