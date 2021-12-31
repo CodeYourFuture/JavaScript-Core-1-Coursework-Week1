d93895ae6f82950ea0f1bd5e2638b0b58f29384c
Simon Lawrence <90318503+SimonLawrenceGB@users.noreply.github.com>
2021-12-31 13:27:07 +0000

    Some Corrections
    In function-output & piping.

--- extra/2-piping.js
+++ extra/2-piping.js
@@ -16,37 +16,39 @@
   the final result to the variable goodCode
 */
 
-function add() {
-
+function add(n1, n2) {
+  return n1 + n2;
 }
 
-function multiply() {
-
+function multiply(n1, n2) {
+  return n1 * n2;
 }
 
-function format() {
-
+function format(number) {
+  return `£${number}`;
 }
 
 const startingValue = 2;
 
 // Why can this code be seen as bad practice? Comment your answer.
-let badCode =
+let badCode = format(multiply(add(startingValue, 10), 2));
 
 /* BETTER PRACTICE */
+let stepOne = add(startingValue, 10);
+let stepTwo = multiply(stepOne, 2);
+let stepThree = format(stepTwo);
 
-let goodCode =
-
-/* ======= TESTS - DO NOT MODIFY ===== 
+let goodCode = stepThree;
+  /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
 
 To run the tests for just this one file, type `npm test -- --testPathPattern 2-piping` into your terminal
 (Reminder: You must have run `npm install` one time before this will work!)
 */
 
-test("add function - case 1 works", () => {
-  expect(add(1, 3)).toEqual(4);
-});
+  test("add function - case 1 works", () => {
+    expect(add(1, 3)).toEqual(4);
+  });
 
 test("add function - case 2 works", () => {
   expect(add(2.4, 5)).toEqual(7.4);
