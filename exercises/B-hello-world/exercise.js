6791dc608eb32df2e77e5475e7e10039981c261c
andersonuk <andersonf.work@gmail.com>
2022-02-06 12:34:54 +0000

    Update exercise.js

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,3 +1,13 @@
-console.log(Hello world);
+// console.log("Hello world");
 
+/* 
+    - What happens when you get rid of the quote marks?
+        Termninal give an SyntaxError:
+*/
+console.log(Hello world);    
 
+/*  
+    - What happens when you console.log() just a number without quotes?
+        It prints a number
+*/
+console.log(2);   
