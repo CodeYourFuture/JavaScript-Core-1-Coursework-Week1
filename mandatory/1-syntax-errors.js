94af39682a868e726f58bd83a498437ca186f6e1
Denver Schouw <105852882+dschouw-CYF@users.noreply.github.com>
2022-06-20 20:00:19 +0200

    Update 1-syntax-errors.js

--- mandatory/1-syntax-errors.js
+++ mandatory/1-syntax-errors.js
@@ -1,7 +1,7 @@
 // There are syntax errors in this code - can you fix it to pass the tests?
 
-function addNumbers(a b c) {
-  return a + b + c;
+function addNumbers(a, b, c) {
+  return a + b + 10;
 }
 
 function introduceMe(name, age)
