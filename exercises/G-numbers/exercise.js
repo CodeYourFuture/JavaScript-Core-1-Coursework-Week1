116c45bf025b7f4e860d5cac0c882f4573caef22
EleMashige <elekanyanimashige@yahoo.com>
2022-06-27 19:36:26 +0200

    updated

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,6 +1,6 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
 const numberOfStudents = 36;
 const numberOfMentors = 5
-console.log(numberOfStudents);
-console.log(numberOfMentors);
-console.log(numberOfStudents + numberOfMentors);
+console.log("Number of students: " + numberOfStudents);
+console.log("Number of mentors: " + numberOfMentors);
+console.log("Total numnber of students and mentors: " + numberOfStudents + numberOfMentors);
--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,6 @@
-var numberOfStudents = 15;
+var numberOfStudents = 15
+numberOfStudents = 15/23*100;
 var numberOfMentors = 8;
+numberOfMentors = 8/23*100;
+console.log("Percentage students: " + numberOfStudents.toFixed() + "%");
+console.log("Percentage mentors: " + numberOfMentors.toFixed() + "%");
