a414b6c0783ae1058dfc8535430a56166cc6cb44
EleMashige <elekanyanimashige@yahoo.com>
2022-06-29 00:27:50 +0200

    updated

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,5 +1,14 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+// var mentor1 = "Daniel";
+// var mentor2 = "Irina";
+// var mentor3 = "Mimi";
+// var mentor4 = "Rob";
+// var mentor5 = "Yohannes";
+
+function greet(username) {
+    console.log('Hello ' + username)
+}
+greet('Daniel')
+greet('Irina')
+greet('Mimi')
+greet('Rob')
+greet('Yohannes')
