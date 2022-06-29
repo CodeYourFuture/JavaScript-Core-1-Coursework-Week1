14cba5bc89ab3775f3803f17fa3f71ec01450173
Anthony Mogotlane <almogotlane@gmail.com>
2022-06-29 16:47:44 +0200

    Completed the exercise

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,15 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+// To uppercase function
+const uppercase = firstName => firstName.toUpperCase();
+// Message function
+const message = firstName => {
+    return "HELLO " + uppercase(firstName);
+}
+console.log(message(mentor1));
+console.log(message(mentor2));
+console.log(message(mentor3));
+console.log(message(mentor4));
+console.log(message(mentor5));
