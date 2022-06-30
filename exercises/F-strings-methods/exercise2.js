a02400de145a3656ddad3fbe7135b2925b48c384
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2022-07-01 00:39:55 +0200

    exercises F - J

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,7 @@
 const name = " Daniel  ";
-
+var nameTrimmed = name.trim();
+var nameLength = nameTrimmed.length;
+var message1 = "my name is " + name;
+var message2 = "and my name is " + nameLength + " characters long";
+var message = message1 + message2;
 console.log(message);
