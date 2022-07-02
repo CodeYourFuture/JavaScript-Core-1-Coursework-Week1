215a35219e7a97b56e8e8a63dfe333583c108a16
Kwanda Silekwa <kwanda9700@gmail.com>
2022-07-02 11:38:34 +0200

    update

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,2 +1,11 @@
-console.log("Hello world");
-console.log("Hello World. I am Kwanda");
+// console.log("Hello world");
+// console.log("Hello World. I am Kwanda");
+
+function convertToBRL(price) {
+  amount = (price - (price * 1) / 100) * 5.7;
+  newAmount = amount.toFixed(2);
+  return parseFloat(newAmount);
+}
+
+amount = convertToBRL(1.5);
+console.log(parseFloat(amount));
