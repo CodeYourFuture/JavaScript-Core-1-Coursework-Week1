8d56133c217b6fe883a7b49391a853de9529b6ac
Kwanda Silekwa <kwanda9700@gmail.com>
2022-07-02 12:26:02 +0200

    Update 3-magic-8-ball.js

--- extra/3-magic-8-ball.js
+++ extra/3-magic-8-ball.js
@@ -42,11 +42,43 @@
     Outlook not so good.
     Very doubtful.
 */
+const matchers = require("jest-extended"); // adds additional matchers to Jest's default ones making it easy to test everything
+expect.extend(matchers);
+
+const possibleAnswers = [
+  // Very positive
+  "It is certain",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes - definitely.",
+  "You may rely on it.",
+  // Positive
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+  // Negative
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  // Very negative
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful.",
+];
 
 // This should log "The ball has shaken!"
 // and return the answer.
 function shakeBall() {
   //Write your code in here
+  let randomAnswer = Math.round(Math.random() * 20);
+  console.log("The ball has shaken!");
+  return possibleAnswers[randomAnswer];
 }
 
 /* 
@@ -60,6 +92,17 @@ function shakeBall() {
 */
 function checkAnswer(answer) {
   //Write your code in here
+  let indexOfAnswers = possibleAnswers.indexOf(answer);
+
+  if (indexOfAnswers <= 4) {
+    return "very positive";
+  } else if (indexOfAnswers >= 5 && indexOfAnswers < 10) {
+    return "positive";
+  } else if (indexOfAnswers >= 10 && indexOfAnswers < 15) {
+    return "negative";
+  } else if (indexOfAnswers >= 15 && indexOfAnswers < 20) {
+    return "very negative";
+  }
 }
 
 /* 
@@ -101,7 +144,9 @@ test("magic 8 ball returns different values each time", () => {
     );
   }
 
-  let seenPositivities = new Set(Array.from(seenAnswers.values()).map(checkAnswer));
+  let seenPositivities = new Set(
+    Array.from(seenAnswers.values()).map(checkAnswer)
+  );
   if (seenPositivities.size < 2) {
     throw Error(
       "Expected to random answers with different positivities each time shakeBall was called, but always got the same one"
