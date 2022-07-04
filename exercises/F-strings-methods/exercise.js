1492cde1d459544a329d5064201410208614ca57
Diego Harera <104278852+diegoharera@users.noreply.github.com>
2022-07-04 23:57:56 +0200

    i completed exercice f

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
-
+let name = "Diego";
+let amount = name.length;
+let message = "hello,my name is " + name + " and my name is " + amount + " characters long";
 console.log(message);
