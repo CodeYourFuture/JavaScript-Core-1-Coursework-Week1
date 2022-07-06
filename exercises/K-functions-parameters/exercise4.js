d958a86a88d7eb3bba5a64b93e94fb633ef4a47f
Lihle Naphakade <31651036+LN2410@users.noreply.github.com>
2022-07-06 10:41:00 +0200

    complete exercises

--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,5 +1,9 @@
 // Declare your function first
+function addNumbers(num1, num2){
+    return num1 + num2;
+}
 
 // Call the function and assign to a variable `sum`
+var sum = addNumbers(13, 124);
 
 console.log(sum);
