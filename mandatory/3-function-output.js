0b4373823f9329e1f5c8c1f442fd0c578c100582
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-07-31 16:52:26 +0100

    Solving mandatory ex3

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,16 +1,19 @@
 // Add comments to explain what this function does. You're meant to use Google!
 function getRandomNumber() {
+  // the function will return a number between 0 and 9
   return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
 function combine2Words(word1, word2) {
+  // the function will join word1 and word2 = word1word2 without space between them
   return word1.concat(word2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+  return firstWord.concat(" ", secondWord, " ", thirdWord);
 }
 
 /* 
