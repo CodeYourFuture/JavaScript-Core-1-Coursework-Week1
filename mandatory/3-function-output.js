ee4f12c170cac534e73572af0df53aa3e2465c37
Chidimma <chidimmaofodum@gmail.com>
2022-07-31 17:39:27 +0100

    mandatory exercises completed

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,9 +1,11 @@
-// Add comments to explain what this function does. You're meant to use Google!
+
+//This function returns a random number between 0 and 10
 function getRandomNumber() {
   return Math.random() * 10;
 }
 
-// Add comments to explain what this function does. You're meant to use Google!
+
+//This function joins word1 and word2 together
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
@@ -11,6 +13,8 @@ function combine2Words(word1, word2) {
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+
+  return firstWord.concat(" ", secondWord, " ", thirdWord)
 }
 
 /* 
