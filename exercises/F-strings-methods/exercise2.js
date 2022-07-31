6136f70e207bdf9de4eb3ed2f43b6eb2ce04ee64
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-07-31 17:51:21 +0100

    Solving ex-F-2

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,7 @@
 const name = " Daniel  ";
+let nameLength = name.length;
+let message = "Hello, my name is";
+console.log(
+  `${message.trim()} ${name.trim()} and my name is ${nameLength} characters long`
+);
 
-console.log(message);
