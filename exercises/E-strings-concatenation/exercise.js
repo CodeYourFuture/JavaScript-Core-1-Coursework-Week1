2b78de9ed31710378d58cd6e70ede22ac8aeaf9d
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-08-01 13:20:55 +0100

    Update exercise.js

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,5 +1,5 @@
-let greetingStart  = "Hello, my name is ";
-let name = "Daniel";
-let greeting = greetingStart + name;
+var greetingStart  = "Hello, my name is ";
+var name = "Daniel";
+var greeting = greetingStart + name;
 
 console.log(greeting);
