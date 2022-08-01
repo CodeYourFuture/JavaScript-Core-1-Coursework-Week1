9d02c6e5a426aa1a252a9f947775224bb363ae58
Alex Phillip <alexjoelphillip@gmail.com>
2022-08-01 09:58:08 +0100

    completed mandatory 3

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -2,15 +2,19 @@
 function getRandomNumber() {
   return Math.random() * 10;
 }
+/* This function assigns a random number between 0 (inclusive) and 1 (exclusive) and multiplies that random number by 10 */
 
 // Add comments to explain what this function does. You're meant to use Google!
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
 
+/* This function takes 2 strings as its parameters and returns the 2 strings joined together, e.g. "Hello" and " World!" = "Hello World!" */ 
+
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+  return firstWord.concat(" ", secondWord, " ", thirdWord);
 }
 
 /* 
