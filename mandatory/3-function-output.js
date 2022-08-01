351321ff57b141e3525981b591a672a0da368cc9
unknown <mesgna.mehari8@gmail.com>
2022-08-02 00:06:30 +0100

    functions explaiend

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,16 +1,19 @@
 // Add comments to explain what this function does. You're meant to use Google!
 function getRandomNumber() {
-  return Math.random() * 10;
+  return Math.random() * 10; 
 }
+// This function gives random number between 0 -9 
 
 // Add comments to explain what this function does. You're meant to use Google!
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
+// This function will join the two words without space between them.
 
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+return firstWord.concat(" ", secondWord, " ", thirdWord);
 }
 
 /* 
