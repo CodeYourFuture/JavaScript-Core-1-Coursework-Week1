9941ed0a97a915a5eae4dc9e41e70e158ae5027d
Alex Phillip <alexjoelphillip@gmail.com>
2022-08-01 01:08:04 +0100

    completed exercise L

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,19 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function uppercase(name) {
+    return name.toUpperCase();
+}
+
+function shoutyGreeting(name) {
+    let message = 
+    "HELLO " + uppercase(name);
+    return message;
+}
+
+console.log(shoutyGreeting(mentor1));
+console.log(shoutyGreeting(mentor2));
+console.log(shoutyGreeting(mentor3));
+console.log(shoutyGreeting(mentor4));
+console.log(shoutyGreeting(mentor5));
