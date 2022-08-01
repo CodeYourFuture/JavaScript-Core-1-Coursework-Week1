fcdf0e0c514aa29a62159b36e6ae98e09b710337
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-08-01 17:48:22 +0100

    Update 3-function-output.js

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,16 +1,20 @@
 // Add comments to explain what this function does. You're meant to use Google!
-function getRandomNumber() {
-  return Math.random() * 10;
+function getRandomNumber()
+{
+    //This function gets a random number between 0 and 1 and returns the result after multiplying the random number and the 10.
+    return Math.random() * 10;
 }
-
-// Add comments to explain what this function does. You're meant to use Google!
-function combine2Words(word1, word2) {
-  return word1.concat(word2);
+  
+  // Add comments to explain what this function does. You're meant to use Google!
+function combine2Words(word1, word2)
+{
+    //This function joins the two words together and then its returns the value.
+    return word1.concat(word2);
 }
-
-function concatenate(firstWord, secondWord, thirdWord) {
-  // Write the body of this function to concatenate three words together.
-  // Look at the test case below to understand what this function is expected to return.
+  
+function concatenate(firstWord, secondWord, thirdWord)
+{
+    return firstWord.concat(" ", secondWord, " ", thirdWord);
 }
 
 /* 
