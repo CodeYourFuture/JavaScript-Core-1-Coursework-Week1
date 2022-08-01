38ad5afba46b998bc421e61b9c24b34aade15279
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-08-01 13:05:30 +0100

    Update exercise.js

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -4,36 +4,16 @@ var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
 
-function createGreeting1(mentor1)
+function createGreeting(mentor)
 {
-    return ("hello " + mentor1).toUpperCase();
+    return ("hello " + mentor).toUpperCase();
 }
 
-function createGreeting2(mentor2)
-{
-    return ("hello " + mentor2).toUpperCase();
-}
-
-function createGreeting3(mentor3)
-{
-    return ("hello " + mentor3).toUpperCase();
-}
-
-function createGreeting4(mentor4)
-{
-    return ("hello " + mentor4).toUpperCase();
-}
-
-function createGreeting5(mentor5)
-{
-    return ("hello " + mentor5).toUpperCase();
-}
-
-const greeting1 = createGreeting1(mentor1);
-const greeting2 = createGreeting2(mentor2);
-const greeting3 = createGreeting3(mentor3);
-const greeting4 = createGreeting4(mentor4);
-const greeting5 = createGreeting5(mentor5);
+const greeting1 = createGreeting(mentor1);
+const greeting2 = createGreeting(mentor2);
+const greeting3 = createGreeting(mentor3);
+const greeting4 = createGreeting(mentor4);
+const greeting5 = createGreeting(mentor5);
 
 console.log(greeting1);
 console.log(greeting2);
