afb10dd200ba395232fbb935159148ed3b3033b1
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-08-01 12:17:43 +0100

    Update exercise.js

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,12 @@
-var numberOfStudents = 15;
-var numberOfMentors = 8;
+let numberOfStudents = 15;
+let numberOfMentors = 8;
+
+let total = numberOfStudents + numberOfMentors;
+
+percent = 100 / total;
+
+let numberOfStudentsPercent =  Math.round(percent * numberOfStudents);
+let numberOfMentorsPercent =  Math.round(percent * numberOfMentors);
+
+console.log("Number of students: " + numberOfStudentsPercent + "%")
+console.log("Number of mentors: " + numberOfMentorsPercent + "%")
