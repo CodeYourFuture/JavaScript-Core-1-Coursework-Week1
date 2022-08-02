931d35346be2d92690d40a4b9e3a22042c636bfe
jonathan willson <jwillson2022@gmail.com>
2022-08-02 16:21:39 +0100

    done i
    shift(); problem shorten my array i--;

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,23 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+const names = [];
+names.push(mentor1, mentor2, mentor3, mentor4, mentor5); 
+//console.log(names.length);
+
+function shouty(nam) {
+    //nam = "G'day " + mentor1 + "!";
+    nam= `G'day ${names[0]} !`;
+    return nam.toUpperCase();
+   
+   // }
+
+   
+    
+}
+for (let i = 0 ; i < names.length; i++) {
+console.log(shouty());
+names.shift();
+i--;  // shift shorts array length
+};
