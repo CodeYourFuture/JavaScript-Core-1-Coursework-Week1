c86ec2a851e02818662b55184ee29b373aac04dd
Rabie Al-Ashmali <70780557+AL-ASHMALI@users.noreply.github.com>
2022-08-02 23:00:52 +0100

    Update exercise.js

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,5 +1,11 @@
-var name = "Rabie"; 
-var nameLnegth = name.length; 
+let firstMessage = "Hello my name is "; 
+let secondmessage = "and my name is "; 
+let thirdmessage = " long"; 
+let name = "Rabie "; 
+let length = name.length; 
 
-console.log(nameLnegth);
-console.log(name.length);
+
+let message = firstMessage + name + secondmessage + length + thirdmessage;
+
+
+console.log(message);
