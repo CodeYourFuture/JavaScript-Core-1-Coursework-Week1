2a2e775dae36264cd77e710d679d79f218dba7d3
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2022-08-02 15:24:49 +0100

    Exercise b completed

--- exercises/C-variables/exercise.js
+++ exercises/C-variables/exercise.js
@@ -1,3 +1,8 @@
 // Start by creating a variable `greeting`
+var greeting;
+greeting = "Hello World";
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
 
 console.log(greeting);
