6602123e7217823b65decbd55b4934f2b7cad985
kavitappatil <patil.poem@gmail.com>
2022-08-02 16:57:46 +0530

    I-floats

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,8 @@
-var numberOfStudents = 15;
-var numberOfMentors = 8;
+var numberOfStudents = 100;
+var numberOfMentors = 80;
+
+var numberOfMentorsToday = .65 * 100;
+console.log(numberOfMentorsToday);
+
+var numberOfStudents = .35 * 100;
+console.log(numberOfStudents);
