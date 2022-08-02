fcd1c9f03263a50f9d6a5436889e4f3b7bb6b267
Kawa MANMI <kawamam2007@gmail.com>
2022-08-02 21:11:43 +0100

    3 fucntion in Mandatory was missing, just fixed

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,13 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function ShouteyGreeting(name){
+    return (console.log("HELLO "+ name.toUpperCase()));
+}
+
+ShouteyGreeting(mentor1)
+ShouteyGreeting(mentor2)
+ShouteyGreeting(mentor3)
+ShouteyGreeting(mentor4)
+ShouteyGreeting(mentor5)
