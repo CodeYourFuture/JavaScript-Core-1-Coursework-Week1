adfde2aa5f7ea8772ffedfe5b63aae10cba4ea9d
Kawa MANMI <kawamam2007@gmail.com>
2022-08-02 20:03:04 +0100

    Exercise K, all tasks  have  been done

--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,5 +1,7 @@
 // Declare your function first
-
+function sumTwoNumbers(a, b){
+    return a+b;
+}
 // Call the function and assign to a variable `sum`
-
+sum=sumTwoNumbers(13,124)
 console.log(sum);
