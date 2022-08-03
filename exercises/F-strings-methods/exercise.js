02c7229f3674dba6953b9339d5b0add971365220
micheale2021 <andemariammicheale@gmail.com>
2022-08-03 10:01:16 +0100

    use .length and .trim()

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,8 @@
 // Start by creating a variable `message`
 
-console.log(message);
+
+var name = "Daniel";
+var nameLength = name.length;
+
+var message = "and my name is" ;
+console.log(`Hello my name is ${name} ${message} ${nameLength} character long`);
