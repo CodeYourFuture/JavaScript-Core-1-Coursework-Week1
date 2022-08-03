45bc47cf2eaf321acfa0f0dec71864991d1253ce
jaspreet kaur <kaurjas1928@yahoo.com>
2022-08-03 17:10:09 +0100

    i have finishe my exercises

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,5 +1,19 @@
+
+
 var mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+
+function mentorGreeting(name) {
+  let getUpperCase = name.toUpperCase();
+  return `Hello ${getUpperCase}`;
+}
+
+console.log(mentorGreeting(mentor1));
+console.log(mentorGreeting(mentor2));
+console.log(mentorGreeting(mentor3));
+console.log(mentorGreeting(mentor4));
+console.log(mentorGreeting(mentor5)); 
