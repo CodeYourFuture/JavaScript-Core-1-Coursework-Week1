9ac436b0e8ec42d83b09af157a27d1bfefec6979
micheale2021 <andemariammicheale@gmail.com>
2022-08-03 10:13:31 +0100

    use Math.round

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,8 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var total = numberOfStudents + numberOfMentors;
+var percentageOfStudents = numberOfStudents / total * 100;
+var percentageOfMentors = numberOfMentors / total * 100;
+
+console.log(`Percentage of students: ${Math.round(percentageOfStudents)}` + "%");
+console.log(`Percentage of mentors: ${Math.round(percentageOfMentors)}` + "%");
