d32c7ae92d5a544956c4ad3ed3b39552a26cf48b
micheale2021 <andemariammicheale@gmail.com>
2022-08-03 10:30:07 +0100

    Write a function that adds two numbers

--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,5 +1,8 @@
 // Declare your function first
+function addNumbers(a, b) {
+    return a + b;  
+};
 
 // Call the function and assign to a variable `sum`
-
+ const sum = addNumbers(13, 124);
 console.log(sum);
