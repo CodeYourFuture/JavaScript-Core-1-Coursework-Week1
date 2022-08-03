a3ebf85e0417a3c8ace01027b4a054c864dfcfc6
micheale2021 <andemariammicheale@gmail.com>
2022-08-03 10:03:25 +0100

    assign numbers to variable and use mathematical operators to calculate

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,6 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 15;
+var numberOfMentors = 8;
+var numberOfStudentsAndMentors = numberOfStudents + numberOfMentors; 
+var message = `Total number of students and mentors: ${numberOfStudentsAndMentors}`;
+console.log(message);
