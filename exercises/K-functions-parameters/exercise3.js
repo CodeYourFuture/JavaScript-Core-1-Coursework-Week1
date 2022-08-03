92e19fc7e53808ffaa927c817980374ccfa30905
micheale2021 <andemariammicheale@gmail.com>
2022-08-03 10:29:25 +0100

    Write a function that takes a name

--- exercises/K-functions-parameters/exercise3.js
+++ exercises/K-functions-parameters/exercise3.js
@@ -1,5 +1,8 @@
 // Write your function here
+function createGreeting(name) {
+    return name;
+}
 
 var greeting = createGreeting("Daniel");
 
-console.log(greeting);
+console.log(`hello, my name is ${greeting}`);
