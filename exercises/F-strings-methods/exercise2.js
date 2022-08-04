ddd4e6bbaf2eb2f5c928118e8affd4eb8672a0de
JanefrancessC <janefrancesschioma@gmail.com>
2022-08-04 23:12:43 +0100

    Completed exercises and mandatory

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,21 @@
-const name = " Daniel  ";
 
-console.log(message);
+let myName = " Chioma ";
+let myNameLength = myName.length;
+
+console.log(`My name is ${myName} and my name is ${myNameLength} characters long`);
+
+let myNameUpperCase = myName.toUpperCase();
+console.log(myNameUpperCase);
+
+let trimName = myName.trim();
+
+console.log(`My name is ${trimName} and my name is ${myNameLength} characters long`)
+
+let firstName = " Daniel  ";
+let firstNameLength = firstName.length;
+
+console.log(firstName);
+console.log(firstNameLength);
+
+let firstNameTrim = firstName.trim();
+console.log(`My name is ${firstNameTrim} and I am ${firstNameLength} characters long`)
--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,16 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function shoutyName(name) {
+  return name.toUpperCase();
+}
+
+let mentors = [mentor1, mentor2, mentor3, mentor4, mentor5];
+
+function shoutyGreeting() {
+  for (i = 0; i < 5; i++) {
+    console.log(`HELLO ${shoutyName(mentors[i])}`);
+  }
+}
+shoutyGreeting();
