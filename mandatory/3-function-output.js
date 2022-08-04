b45c3d684c47ab7b6300f72802bb336f1b269e76
Emeka1993 <brolin933@gmail.com>
2022-08-04 22:33:28 +0100

    Commit

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -9,6 +9,7 @@ function combine2Words(word1, word2) {
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
+  return (firstWord + " " + secondWord + " " + thirdWord )
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
 }
