43aa58880a05e1e1a211b223561513e1139f7e73
Lunamzelo <lunamzelo@gmail.com>
2022-08-05 00:37:39 +0100

    Done exercise L

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,5 +1,23 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+function add(name) {
+  var mentor1 = "Daniel";
+  var mentor2 = "Irina";
+  var mentor3 = "Mimi";
+  var mentor4 = "Rob";
+  var mentor5 = "Yohannes";
+
+  var greeting = "HELLO " + name.toUpperCase();
+
+  return greeting;
+}
+
+var mentor1 = add("Daniel");
+var mentor2 = add("Irina");
+var mentor3 = add("Mimi");
+var mentor4 = add("Rob");
+var mentor5 = add("Yohannes");
+
+console.log(mentor1);
+console.log(mentor2);
+console.log(mentor3);
+console.log(mentor4);
+console.log(mentor5);
