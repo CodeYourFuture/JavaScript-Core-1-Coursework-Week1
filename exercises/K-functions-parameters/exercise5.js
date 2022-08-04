6a7284374f9d68a339f956855d231e3bc86ec978
jaspreet kaur <kaurjas1928@yahoo.com>
2022-08-04 17:59:46 +0100

    changed the function

--- exercises/K-functions-parameters/exercise5.js
+++ exercises/K-functions-parameters/exercise5.js
@@ -1,7 +1,7 @@
 // Declare your function here
-function introduceMe(name, age){
-  return "Hello, my name is " + name + "and I am " + age + "years old";
+function createLongGreeting(name, age) {
+  return "Hello, my name is " + name  +  " and I am " + age  +  " years old";
 }
-const name = "jaspreet"
-const age = 30
-console.log(`Hello, my name is ${name} and i am ${age} years old`);
+const greeting = createLongGreeting("Daniel", 30);
+
+console.log(greeting);
