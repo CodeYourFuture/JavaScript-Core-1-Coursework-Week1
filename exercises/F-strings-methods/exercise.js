4a5f01202aa94d44edc0ad71097f41a37eadbe39
Lunamzelo <lunamzelo@gmail.com>
2022-08-05 00:42:10 +0100

    used strings methods

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,9 @@
 // Start by creating a variable `message`
-
+// var message;
+// console.log(message);
+// var message="hello world. I just started learning JavaScript!."
+// console.log(message);
+var myName="Luna";
+var nameLength=myName.length;
+var message= "my name is " + myName + " my name is " + nameLength +" characters long."; 
 console.log(message);
