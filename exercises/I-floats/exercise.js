96fd58f920a0608604b3ea027adc2f327977cca3
jaspreet kaur <kaurjas1928@yahoo.com>
2022-08-04 17:44:06 +0100

    another update as instructed by pd buddy

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,7 +1,7 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
 var total = numberOfStudents + numberOfMentors
-var pOfStudents = Math.round((numberOfStudents / total)*100);
-var pOfMentors = Math.round((numberOfMentors / total) * 100);
-console.log(`Percentage of students: ${pOfStudents}%`);
-console.log(`Percentage of students: ${pOfMentors}%`);
+var percentageOfStudents = Math.round((numberOfStudents / total)*100);
+var percentageOfMentors = Math.round((numberOfMentors / total) * 100);
+console.log(`Percentage of students: ${percentageOfStudents}%`);
+console.log(`Percentage of students: ${percentageOfMentors}%`);
