6de74377b48d5b42bd0116862481f1006d83b466
Lunamzelo <lunamzelo@gmail.com>
2022-08-05 00:41:07 +0100

    added to strings together

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
-
-console.log(message);
+var myGreeting="Hello, my name is ";
+const myName="Luna."
+let greeting=myGreeting + myName;
+console.log(greeting);
