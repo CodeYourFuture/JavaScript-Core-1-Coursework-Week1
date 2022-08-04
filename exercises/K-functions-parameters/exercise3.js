095ccbba8d391c8b6811affd68964a431a755d20
Lunamzelo <lunamzelo@gmail.com>
2022-08-05 00:47:06 +0100

    created greeting

--- exercises/K-functions-parameters/exercise3.js
+++ exercises/K-functions-parameters/exercise3.js
@@ -1,5 +1,13 @@
 // Write your function here
+function createGreeting(name){
+    return name;
+}
 
 var greeting = createGreeting("Daniel");
 
 console.log(greeting);
+
+
+
+console.log(greeting);
+console.log(`hello, my name is ${greeting}`);
