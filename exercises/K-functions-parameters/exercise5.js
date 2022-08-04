29f8f71e23d9e01bc355195d2c68a2a5b06412ef
micheale2021 <andemariammicheale@gmail.com>
2022-08-04 07:03:39 +0100

    update exercise5

--- exercises/K-functions-parameters/exercise5.js
+++ exercises/K-functions-parameters/exercise5.js
@@ -5,7 +5,7 @@ function getAge(age) {
 
 function createLongGreeting(name, age) {
     return "Hello, my name is" + " " + name + " " + "and I am" + " " + age + " " + "years old."; 
-};
+ };
 
 
 const greeting = createLongGreeting("Daniel", 30);
