dcd838acd7a3acb6d01902127ce80016bf323ae6
Fatemeh Rahimi <fa.rahimi5475@gmail.com>
2022-08-05 02:17:50 +0100

    Majority of Exercise folder are fixed

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,14 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+
+var totalNumber = numberOfStudents + numberOfMentors;
+
+var studentPrecentage = (numberOfStudents / totalNumber)* 100;
+studentPrecentage = Math.round(studentPrecentage);
+
+var mentorPrecentage = (numberOfMentors / totalNumber)* 100;
+mentorPrecentage = Math.round(mentorPrecentage);
+
+console.log("Percentage students: " + studentPrecentage + "%");
+console.log("Percentage mentors: " + mentorPrecentage + "%");
