33fd497b74692e65bee598a9c4b13cbdacbf6dcd
Pakize Bozkurt <pakiozihak@gmail.com>
2022-08-05 12:02:01 +0100

    Changed mandatory 3

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,5 +1,5 @@
 // Add comments to explain what this function does. You're meant to use Google!
-//Getting a random number between 0 (inclusive) and 1 (exclusive)
+//Getting a random number between 0 and <10.
 function getRandomNumber() {
   return Math.random() * 10;
 }
