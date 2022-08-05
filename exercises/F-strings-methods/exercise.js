e90a718e07ed87064b3a3b3423ef0c98d07357da
ozibozkurt <ozipaki@gmail.com>
2022-08-05 11:56:28 +0100

    fixed F-strings

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,8 +1,8 @@
 // Start by creating a variable `message`
 
 
-var name = "Daniel ";
+var name = "Daniel";
 var message = "Hello, my name is ";
 var nameLength = name.length;
 
-console.log(message + name + "and my name is " + nameLength + " characters long");
+console.log(message + name + " and my name is " + nameLength + " characters long");
--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,7 +1,8 @@
-const name = "          Daniel             ";
-var nameLength = name.length;
-let message = "Hello, my name is ";
-
-console.log(message.trim() + name.trim() + " and my name is " + nameLength + " characters long");
 
 
+let name = "  Daniel          ";
+name1 = name.trim();
+let nameLength = name1.length;
+let message = "Hello, my name is " + name1 + " and my name is " + nameLength + " characters long";
+
+console.log(message);
