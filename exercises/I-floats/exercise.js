822a06bc51f1be37912b17dfdde8910a09347a96
ozibozkurt <ozipaki@gmail.com>
2022-08-05 12:01:28 +0100

    fixed I-floats

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,7 +1,7 @@
 const numberOfTrainees = 15;
 const numberOfMentors = 8;
 const total = numberOfTrainees + numberOfMentors;
-const pOfTrainees = (numberOfTrainees / total) * 100;
-const pOfMentors = Math.round((numberOfMentors / total) * 100);
-console.log(`Percentage of trainees ${Math.round(pOfTrainees)}%`);
-console.log(`Percentage of mentors ${Math.round(pOfMentors)}%`);
+const percentOfTrainees = (numberOfTrainees / total) * 100;
+const percentOfMentors = Math.round((numberOfMentors / total) * 100);
+console.log(`Percentage of trainees ${Math.round(percentOfTrainees)}%`);
+console.log(`Percentage of mentors ${Math.round(percentOfMentors)}%`);
