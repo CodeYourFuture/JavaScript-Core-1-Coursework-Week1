f518eb230345131bb8360f298e200aa25fab0d20
ozibozkurt <ozipaki@gmail.com>
2022-08-05 11:57:54 +0100

    fixed G-numbers

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,9 +1,9 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
 var numberOfStudents = 15;
-var NumberOfMentors= 8;
+var numberOfMentors= 8;
 var total = numberOfStudents + NumberOfMentors;
 
 console.log("Number of students: " + numberOfStudents);
-console.log("Number of mentors: " + NumberOfMentors);
+console.log("Number of mentors: " + numberOfMentors);
 console.log("Total number of students and mentors: " + total);
 
