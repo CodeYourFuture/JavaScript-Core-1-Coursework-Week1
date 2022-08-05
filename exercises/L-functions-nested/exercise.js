4544847f068941e88a8d5a8aa75e4a859869e1a1
ozibozkurt <ozipaki@gmail.com>
2022-08-05 11:16:07 +0100

    Fixed L-Function

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,18 +1,8 @@
 function greetings(mentor) {
-var mentor1 = "Daniel";
-var mentor2 = "Chizim";
-var mentor3 = "Biruk";
-var mentor4 = "Louis";
-var mentor5 = "Ali";
-var mentor6 = "Marta";
-var mentor7 = "Elizabeth";
-var mentor8 = "Huma";
-var mentor9 = "Anna";
-var mentor10 = "Matt";
 var result = "Hello " + mentor
 console.log(result.toUpperCase())
 }
-greetings("Biruk");
-greetings("Daniel");
-greetings("Chizim");
-greetings("Marta");
+greetings("Jack");
+greetings("Nick");
+greetings("Ozy");
+greetings("Marta"); 
