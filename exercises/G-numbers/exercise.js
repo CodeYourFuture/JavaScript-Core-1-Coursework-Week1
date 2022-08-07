c54f08ae3a667083cad8863684851c4704638922
Emeka1993 <brolin933@gmail.com>
2022-08-07 19:13:37 +0100

    commit

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,5 +1,6 @@
 numberOfStudents = 15;
 let numberOfMentors = 8;
 let total = numberOfStudents + numberOfMentors
-
-
+console.log(numberOfStudents)
+console.log(numberOfMentors)
+console.log(`total number of students and mentors: ${total}`)
