c52414996c4316969a5823b47210794e1689a77b
ansuj9123 <ansuj9@gmail.com>
2022-08-12 02:05:29 +0100

    completed homework

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
 
+let message = "Hello my name is Ansu";
+
+
 console.log(message);
--- mandatory/4-tax.js
+++ mandatory/4-tax.js
@@ -5,7 +5,9 @@
   Sales tax is 20% of the price of the product.
 */
 
-function calculateSalesTax() {}
+function calculateSalesTax(price) {
+  return price+(price/5)
+}
 
 /*
   CURRENCY FORMATTING
@@ -17,7 +19,11 @@ function calculateSalesTax() {}
   Remember that the prices must include the sales tax (hint: you already wrote a function for this!)
 */
 
-function addTaxAndFormatCurrency() {}
+function addTaxAndFormatCurrency(price) {
+  const saletax = calculateSalesTax(price);
+ 
+  return "£" + saletax.toFixed(2);
+}
 
 /* 
 ===================================================
