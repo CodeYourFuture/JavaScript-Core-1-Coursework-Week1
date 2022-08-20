04dfc7db28e125ca008e694068f336731540ed78
Sharifa Ismail <105314490+Sharifa1@users.noreply.github.com>
2022-08-20 10:49:04 +0100

    L-functions

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,12 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function shouty(name) {
+    return "HELLO " + name.toUpperCase();
+}
+console.log(shouty(mentor1));
+console.log(shouty(mentor2));
+console.log(shouty(mentor3));
+console.log(shouty(mentor4));
+console.log(shouty(mentor5));
