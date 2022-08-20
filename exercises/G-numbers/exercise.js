945d83f828a12701f51c2a5fb38a849281f4e707
omardouglas409 <omardouglas409@gmail.com>
2022-08-20 17:29:34 +0100

    exercises

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,5 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var NumberOfStudents = 15
+var NumberOfMentors = 8
+var TotalNumnberOfStudentsAndMentors = NumberOfStudents + NumberOfMentors
+console.log("Total numnber of students and mentors: " + TotalNumnberOfStudentsAndMentors)
