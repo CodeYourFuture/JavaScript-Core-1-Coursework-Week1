793596f267f863f5e472cf9a81c115edc6fb6d4c
Haroon Ali <87202358+haroon-ali-dev@users.noreply.github.com>
2022-08-21 21:56:47 +0100

    complete exercises

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,13 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function greeting(name) {
+	console.log("HELLO " + name.toUpperCase());
+}
+
+greeting(mentor1);
+greeting(mentor2);
+greeting(mentor3);
+greeting(mentor4);
+greeting(mentor5);
