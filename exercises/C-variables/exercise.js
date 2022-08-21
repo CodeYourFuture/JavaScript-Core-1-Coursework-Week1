b175606e1f09c4018383ac870398918cb982664b
Haroon Ali <87202358+haroon-ali-dev@users.noreply.github.com>
2022-08-21 21:58:34 +0100

    complete exercises

--- exercises/C-variables/exercise.js
+++ exercises/C-variables/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `greeting`
+const greeting = "Hi, how are you?";
 
+console.log(greeting);
+console.log(greeting);
 console.log(greeting);
--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
+const name = "Haroon";
 
-console.log(message);
+console.log(
+	"My name is " + name + " and my name is " + name.length + " characters long."
+);
