2987a4767a9986ff04633766652508732b81180a
Ekrem <ekremteke.uk@gmail.com>
2022-08-22 17:36:11 +0100

    Exercise L was completed

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,15 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function changeToUppercase(name) {
+  return (upperCasename = name.toUpperCase());
+}
+function shoutyGreeting(upperCasename) {
+  return "HELLO " + upperCasename;
+}
+console.log(shoutyGreeting(changeToUppercase(mentor1)));
+console.log(shoutyGreeting(changeToUppercase(mentor2)));
+console.log(shoutyGreeting(changeToUppercase(mentor3)));
+console.log(shoutyGreeting(changeToUppercase(mentor4)));
+console.log(shoutyGreeting(changeToUppercase(mentor5)));
