57a0e325ae996f0a78fa08d4521f529a7691dd0a
Ekrem <ekremteke.uk@gmail.com>
2022-08-22 14:59:19 +0100

    F exercise 1 was completed

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,9 @@
 // Start by creating a variable `message`
 
+var name= "Ekrem";
+var length = name.length;
+
+var message= "My name is " + name + " and my name is " + length + " characters long";
+
+
 console.log(message);
