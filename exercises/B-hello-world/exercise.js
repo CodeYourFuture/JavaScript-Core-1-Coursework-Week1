d5d6ae1407588005883b7b1e732be19374e55caf
khmdagal <81495872+khmdagal@users.noreply.github.com>
2022-08-22 20:13:27 +0100

    firs exercise

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,5 @@
-console.log("Hello world");
+// console.log("Hello world");
+// // this is exercise
+// console.log("Hello World. I just started learning JavaScript!");
+console.log(24);// this consoles number 24
+console.log(70000);
