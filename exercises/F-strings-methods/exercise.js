afb4b4d14f94e7d1dc8034e6105c4d16beb1f29a
Marryggk <99277162+Meronggk@users.noreply.github.com>
2022-08-22 22:35:55 +0100

    completed F-strings-methods

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,14 @@
 // Start by creating a variable `message`
 
+
+
+var greetings = "Hello, my name is  ";
+var name = "Meron";
+var myName = "my name is ";
+var nameLength = name.length;
+var characters = " characters long";
+
+var message =  greetings + name + myName + nameLength + characters;
+
 console.log(message);
+
