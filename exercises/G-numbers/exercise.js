7d382b37249088ecd3d29f5bf6cf081da4984e77
levilu800b <levilu800b@gmail.com>
2022-08-22 22:42:18 +0100

    completed G-numbers

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,8 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+
+var numberOfStudents = 15;
+var numberOfMentors = 8;
+
+var total = numberOfStudents + numberOfMentors;
+
+console.log(total);
