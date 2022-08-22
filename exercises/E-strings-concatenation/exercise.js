c8c840206d8b20da369ea7f64b91be74ecce04ba
khmdagal <81495872+khmdagal@users.noreply.github.com>
2022-08-22 21:01:41 +0100

    String concatenation exercise

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,9 @@
 // Start by creating a variable `message`
-
+var name = "Adam";
+const introducation = "Hello, my name is "
+let greating = introducation + name;
+message = greating;
+const exerciseDone = "this exercise is done 🙂";
 console.log(message);
+console.log(exerciseDone);
+
