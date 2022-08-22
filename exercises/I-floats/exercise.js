c66cd1c0ec078bda9d00c1bcd20da5386cc75fd9
Marryggk <99277162+Meronggk@users.noreply.github.com>
2022-08-22 23:10:01 +0100

    completed I-floats

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,11 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var total = 15 + 8;
+var percent = Math.round((numberOfStudents / total) * 100);
+var percent1 = Math.round((numberOfMentors / total) * 100);
+console.log(percent);
+console.log(percent1);
+
+
+
+ 
