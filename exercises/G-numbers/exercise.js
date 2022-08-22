60a9f1500a593c3b82d0ff3e84fbb560bf7877b8
khmdagal <81495872+khmdagal@users.noreply.github.com>
2022-08-22 21:04:49 +0100

    Number exercise

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,11 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+
+// Numbe of students
+const numberOfStudents = 15;
+ //Number of Mentors
+const numberOfMentors = 8;
+
+const totalOfStudentsAndMentors = numberOfStudents + numberOfStudents;
+console.log(totalOfStudentsAndMentors);
+// to practicing template litteral/ backticks 
+console.log(`There are ${numberOfStudents} students, and ${numberOfMentors} Mentors, and in total they are ${totalOfStudentsAndMentors} peaple.`)
