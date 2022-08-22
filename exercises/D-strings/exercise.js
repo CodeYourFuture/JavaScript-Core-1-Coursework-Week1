0a1ffbecdf58935895cf1ed00e3e81cc0a07b2f8
khmdagal <81495872+khmdagal@users.noreply.github.com>
2022-08-22 21:00:34 +0100

    strings exericse

--- exercises/D-strings/exercise.js
+++ exercises/D-strings/exercise.js
@@ -1,3 +1,11 @@
 // Start by creating a variable `message`
-
+let message;
+message = "this is exericse";
+console.log(message, "---------");
+console.log(typeof message, "---------");
 console.log(message);
+
+const question = "What data type I am?"
+const answer = '😂, your are'
+console.log(`can you tell me ${question} sure ${answer} "${typeof question}" do you get that!!`)
+
