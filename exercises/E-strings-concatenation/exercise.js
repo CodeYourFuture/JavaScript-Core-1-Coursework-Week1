871bf27e382e8d2ae942514ad9916f394932a38f
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2022-08-24 00:19:33 +0100

    Exercise E was completed

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
-
-console.log(message);
+let greetingStart = "Hello, my name is ";
+let name = " Refik ";
+let greeting = greetingStart + name; 
+console.log(greeting);
