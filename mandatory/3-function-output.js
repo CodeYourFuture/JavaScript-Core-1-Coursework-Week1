235e63d79800277f3e3769e5c2289bcba66e38dd
khmdagal <81495872+khmdagal@users.noreply.github.com>
2022-08-23 17:46:57 +0100

    Fix function output exercise

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,16 +1,32 @@
 // Add comments to explain what this function does. You're meant to use Google!
 function getRandomNumber() {
   return Math.random() * 10;
+  /* step 1
+  Math.random()  each time this funcation is executed it returns a number between 0 to 1.
+
+  step 2
+  * 10   will multiply 10 by any number that step 1 produces 
+  
+  */
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
 function combine2Words(word1, word2) {
   return word1.concat(word2);
+/*
+this is similar to word1 + word2;
+
+variable1.concat(varialbe1) returns the comination of the two varialbles
+while the first first varialble will be the first in the sentence.
+*/ 
+
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+  return firstWord.concat(" ",secondWord," ",thirdWord);
+  // return concatnatedTrimedwords.trim();
 }
 
 /* 
