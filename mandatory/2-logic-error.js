20ae565ef87aaaba024f0c4b3750507eb0be56f0
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2022-08-23 15:06:51 +0100

    complete task 2-logic

--- mandatory/2-logic-error.js
+++ mandatory/2-logic-error.js
@@ -1,16 +1,19 @@
 // The syntax for this function is valid but it has an error, find it and fix it.
 
 function trimWord(word) {
-  return wordtrim();
+  
+  return word.trim();
 }
 
-function getStringLength(word) {
-  return "word".length();
-}
+function getStringLength (word) {
 
+
+ return word.length;
+}
+console.log(getStringLength("hi ahmed"));
 function multiply(a, b, c) {
-  a * b * c;
-  return;
+ 
+  return  a * b * c;
 }
 
 /* 
