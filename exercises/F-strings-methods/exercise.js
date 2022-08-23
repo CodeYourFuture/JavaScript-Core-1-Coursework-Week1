bc6e0fc5d8b134cf44188df1baceebd2a6c9467d
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2022-08-23 10:17:58 +0100

    complete task F

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
+let message =`My name is Ahmed and my cheregter numer is  `;
+let name2="Ahmed";
+let numberCherctor=name2.length;
+console.log(message+numberCherctor);
 
-console.log(message);
