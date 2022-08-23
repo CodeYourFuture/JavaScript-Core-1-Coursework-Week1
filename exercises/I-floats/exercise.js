4741beb636f9bbbc237a94f53c756afa136eb53a
leilafaez <79403281+leilafaez@users.noreply.github.com>
2022-08-23 11:44:31 +0100

    Update I-floats exercise

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,7 +1,8 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
-let studentPerc = Math.floor(numberOfMentors / numberOfStudents*100);
-let mentorPerc = Math.abs(Math.floor(studentPerc-100));
+var total = numberOfMentors + numberOfStudents;
+let studentPerc = Math.round((numberOfMentors / total) * 100);
+let mentorPerc = Math.round((numberOfStudents / total) * 100);
 
-console.log("Percent of student :"+" "+studentPerc+"%");
+console.log("Percent of student :" + " " + studentPerc + "%");
 console.log("Percent of mentor :" + " " + mentorPerc + "%");
