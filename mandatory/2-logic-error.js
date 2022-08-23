fd9f497f8238222d592a8d6b44e1fade7a1aa59f
Danti_86 <ajux_86@yahoo.com>
2022-08-23 21:33:24 +0100

    uncomment the test

--- mandatory/2-logic-error.js
+++ mandatory/2-logic-error.js
@@ -26,28 +26,28 @@ To run the tests for just this one file, type `npm test -- --testPathPattern 2-l
 ===================================================
 */
 
-// test("trimWord trims leading and trailing whitespace", () => {
-//   expect(trimWord("   CodeYourFuture ")).toEqual("CodeYourFuture");
-// });
+test("trimWord trims leading and trailing whitespace", () => {
+  expect(trimWord("   CodeYourFuture ")).toEqual("CodeYourFuture");
+});
 
-// test("trimWord doesn't remove whitespace in the middle of the string", () => {
-//   expect(trimWord(" CodeYourFuture teaches coding     ")).toEqual(
-//     "CodeYourFuture teaches coding"
-//   );
-// });
+test("trimWord doesn't remove whitespace in the middle of the string", () => {
+  expect(trimWord(" CodeYourFuture teaches coding     ")).toEqual(
+    "CodeYourFuture teaches coding"
+  );
+});
 
-// test("getStringLength returns the length of a word", () => {
-//   expect(getStringLength("Turtles")).toEqual(7);
-// });
+test("getStringLength returns the length of a word", () => {
+  expect(getStringLength("Turtles")).toEqual(7);
+});
 
-// test("getStringLength returns the length of a sentence", () => {
-//   expect(getStringLength("A wild sentence appeared!")).toEqual(25);
-// });
+test("getStringLength returns the length of a sentence", () => {
+  expect(getStringLength("A wild sentence appeared!")).toEqual(25);
+});
 
-// test("multiply multiplies numbers", () => {
-//   expect(multiply(2, 3, 6)).toEqual(36);
-// });
+test("multiply multiplies numbers", () => {
+  expect(multiply(2, 3, 6)).toEqual(36);
+});
 
-// test("multiply multiplies different numbers", () => {
-//   expect(multiply(2, 3, 4)).toEqual(24);
-// });
+test("multiply multiplies different numbers", () => {
+  expect(multiply(2, 3, 4)).toEqual(24);
+});
--- mandatory/4-tax.js
+++ mandatory/4-tax.js
@@ -36,26 +36,26 @@ To run the tests for just this one file, type `npm test -- --testPathPattern 4-t
 ===================================================
 */
 
-// test("calculateSalesTax for £15", () => {
-//   expect(calculateSalesTax(15)).toEqual(18);
-// });
+test("calculateSalesTax for £15", () => {
+  expect(calculateSalesTax(15)).toEqual(18);
+});
 
-// test("calculateSalesTax for £17.50", () => {
-//   expect(calculateSalesTax(17.5)).toEqual(21);
-// });
+test("calculateSalesTax for £17.50", () => {
+  expect(calculateSalesTax(17.5)).toEqual(21);
+});
 
-// test("calculateSalesTax for £34", () => {
-//   expect(calculateSalesTax(34)).toEqual(40.8);
-// });
+test("calculateSalesTax for £34", () => {
+  expect(calculateSalesTax(34)).toEqual(40.8);
+});
 
-// test("addTaxAndFormatCurrency for £15", () => {
-//   expect(addTaxAndFormatCurrency(15)).toEqual("£18.00");
-// });
+test("addTaxAndFormatCurrency for £15", () => {
+  expect(addTaxAndFormatCurrency(15)).toEqual("£18.00");
+});
 
-// test("addTaxAndFormatCurrency for £17.50", () => {
-//   expect(addTaxAndFormatCurrency(17.5)).toEqual("£21.00");
-// });
+test("addTaxAndFormatCurrency for £17.50", () => {
+  expect(addTaxAndFormatCurrency(17.5)).toEqual("£21.00");
+});
 
-// test("addTaxAndFormatCurrency for £34", () => {
-//   expect(addTaxAndFormatCurrency(34)).toEqual("£40.80");
-// });
+test("addTaxAndFormatCurrency for £34", () => {
+  expect(addTaxAndFormatCurrency(34)).toEqual("£40.80");
+});
