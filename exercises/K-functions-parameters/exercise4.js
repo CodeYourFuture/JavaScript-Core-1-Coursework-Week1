f10f75dd0ea8c1fd5dd2d1cf9149c3b0ca15c806
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2022-08-23 11:59:11 +0100

    complete task K

--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,5 +1,8 @@
 // Declare your function first
 
 // Call the function and assign to a variable `sum`
-
+function addnumber(a,b){
+    return a+b;
+}
+let sum=addnumber(13,124)
 console.log(sum);
--- exercises/K-functions-parameters/exercise5.js
+++ exercises/K-functions-parameters/exercise5.js
@@ -1,5 +1,8 @@
 // Declare your function here
-
+function createLongGreeting(name,age){
+    // console.log(`Hello, my $[name] is  and I'm $[age]  years old`);
+    return "Hello, my name is " + name+" i'm "+age+"years old";
+}
 const greeting = createLongGreeting("Daniel", 30);
 
 console.log(greeting);
