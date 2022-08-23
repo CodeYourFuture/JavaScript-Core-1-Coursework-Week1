d72c4f9a47fcf0aab62cc38a8fae57afa01f021e
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2022-08-24 00:38:19 +0100

    Exercise F was completed

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
-
+const name = "Refik";
+let length = name.length;
+let message = "My name is " + name + " and my name is " + name.length + " characters long.";
 console.log(message);
