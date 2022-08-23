89e7dce9ac95443a2e780341d805e4a6cd66ac3e
Danti_86 <ajux_86@yahoo.com>
2022-08-23 20:50:43 +0100

    Done exercise I-floats

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,6 @@
-var numberOfStudents = 15;
-var numberOfMentors = 8;
+// var numberOfStudents = 15;
+// var numberOfMentors = 8;
+var preciseAge = 30.612437;
+var result = preciseAge / 10;
+var resultWhole = Math.round(result);
+console.log(resultWhole + "%");
