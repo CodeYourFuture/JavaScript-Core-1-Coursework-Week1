8e272fe50aa874b31b88d5a7b4476e061cf821de
Marryggk <99277162+Meronggk@users.noreply.github.com>
2022-08-23 21:14:03 +0100

    completed mandatory 1-syntax-errors.js

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,5 +1,5 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
 var numberOfStudents = 15;
 var numberOfMentors = 8;
-var total = 15 + 8;
+let total = 15 + 8;
 console.log(total);
