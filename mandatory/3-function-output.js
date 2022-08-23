2e974a387cac0b74d8addc5db51cd3a23f74d770
Danti_86 <ajux_86@yahoo.com>
2022-08-23 20:54:39 +0100

    Done mandatory\3

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,17 +1,21 @@
 // Add comments to explain what this function does. You're meant to use Google!
-function getRandomNumber() {
-  return Math.random() * 10;
+function getRandomNumber() { 
+  return Math.random() * 10; // This function multiply tow number and the first number change it to whole number to get the result whole number.
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
 function combine2Words(word1, word2) {
-  return word1.concat(word2);
+  return word1.concat(word2);  // This function work to marge to word together.
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
+  return  "I" + " " + "am" + " " + 13;
+
+
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
 }
+console.log(concatenate());
 
 /* 
 ===================================================
