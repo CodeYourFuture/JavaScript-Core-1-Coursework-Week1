01b37a7a749eff7d6ee98e08d9952bc8b8dbc4a7
Ekrem <ekremteke.uk@gmail.com>
2022-08-23 04:23:47 +0100

    extra 3, two test passed 2 failed

--- extra/3-magic-8-ball.js
+++ extra/3-magic-8-ball.js
@@ -45,9 +45,35 @@
 
 // This should log "The ball has shaken!"
 // and return the answer.
-function shakeBall() {
-  //Write your code in here
+console.log("The ball has shaken!");
+function shakeBall(a) {
+  return a[Math.floor(Math.random() * a.length)];
+  console.log(shakeBall(a));
 }
+
+var a = [
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes - definitely.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful.",
+];
+var answer = console.log(shakeBall(a));
 
 /* 
   This function should say whether the answer it is given is
@@ -55,10 +81,38 @@ function shakeBall() {
     - positive
     - negative
     - very negative
+
   This function should expect to be called with any value which was returned by the shakeBall function.
 */
+
 function checkAnswer(answer) {
-  //Write your code in here
+  if (
+    answer === "It is certain." ||
+    answer === "It is decidedly so." ||
+    answer === "You may rely on it." ||
+    answer === "Without a doubt." ||
+    answer === "Yes - definitely."
+  ) {
+    return "very positive";
+  } else if (
+    answer === "Yes." ||
+    answer === "As I see it, yes." ||
+    answer === "Signs point to yes." ||
+    answer === "Most likely." ||
+    answer === "Outlook good."
+  ) {
+    return "positive";
+  } else if (
+    answer === "Reply hazy, try again." ||
+    answer === "Ask again later." ||
+    answer === "Better not tell you now." ||
+    answer === "Cannot predict now." ||
+    answer === "Concentrate and ask again."
+  ) {
+    return "Negative";
+  } else {
+    return "very negative";
+  }
 }
 
 /* 
