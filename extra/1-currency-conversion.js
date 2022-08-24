fc7dc07117c74e7e588e9d6c5d622d1da6ec0808
khmdagal <81495872+khmdagal@users.noreply.github.com>
2022-08-24 15:27:14 +0100

    currency conversion test pass

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -5,7 +5,10 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(pound) {
+  const USD = pound * 1.4;
+  return USD;
+}
 
 /*
   CURRENCY CONVERSION
@@ -15,7 +18,14 @@ function convertToUSD() {}
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL() {}
+function convertToBRL(pound) {
+  // const convertablePound = pound - (1 / pound) * 100; also same as
+  const convertablePound = pound - (pound * 0.01);
+  const convertedPoundToBRL = convertablePound * 5.7;
+  const convertedToNumber = Number(convertedPoundToBRL.toFixed(2));
+  return convertedToNumber;
+}
+
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
