b6ed0594ae89550bce57b2ec8544bbde6bfee102
Anosh Malik <anoshmalik.am@gmail.com>
2022-08-24 12:38:08 +0100

    Tests passed - all mandatory tests passed

--- mandatory/4-tax.js
+++ mandatory/4-tax.js
@@ -5,7 +5,9 @@
   Sales tax is 20% of the price of the product.
 */
 
-function calculateSalesTax() {}
+function calculateSalesTax(number) {
+  return number * 1.2;
+}
 
 /*
   CURRENCY FORMATTING
@@ -17,7 +19,11 @@ function calculateSalesTax() {}
   Remember that the prices must include the sales tax (hint: you already wrote a function for this!)
 */
 
-function addTaxAndFormatCurrency() {}
+function addTaxAndFormatCurrency(number) {
+  return "£" + (number * 1.20).toFixed(2)  ;
+}
+
+// ALL TESTS PASSED
 
 /* 
 ===================================================
