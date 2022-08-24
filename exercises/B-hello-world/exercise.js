24622fe4435e62df3013f79a111ee7c2f60e3614
Anosh Malik <anoshmalik.am@gmail.com>
2022-08-24 11:02:53 +0100

    Ran - B-hello-world file

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,5 @@
-console.log("Hello world");
+console.log(Hello world I just started learning Javascript);
+
+
+// Throws https://github.com/AnoshMalik/JavaScript-Core-1-Coursework-Week1.git
+// When "" are removed above ^^
