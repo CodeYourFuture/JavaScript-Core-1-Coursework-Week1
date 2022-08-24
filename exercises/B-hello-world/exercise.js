3fe7d2a2de12372797c1192d9e7b018db520faa4
Anosh Malik <anoshmalik.am@gmail.com>
2022-08-24 11:06:49 +0100

    Ran - exercise C

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,5 +1,7 @@
-console.log("Hello world I just started learning Javascript");
+console.log(5);
 
 
 // Throws https://github.com/AnoshMalik/JavaScript-Core-1-Coursework-Week1.git
 // When "" are removed above ^^
+
+// The number gets printed w/o quotes as well
