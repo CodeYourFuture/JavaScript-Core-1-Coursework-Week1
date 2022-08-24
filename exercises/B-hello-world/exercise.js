7549d51f4485cafb23b9ef7bb15209a445c319a3
Anosh Malik <anoshmalik.am@gmail.com>
2022-08-24 11:03:42 +0100

    Put - Quotes inside console.log() again

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,4 +1,4 @@
-console.log(Hello world I just started learning Javascript);
+console.log("Hello world I just started learning Javascript");
 
 
 // Throws https://github.com/AnoshMalik/JavaScript-Core-1-Coursework-Week1.git
