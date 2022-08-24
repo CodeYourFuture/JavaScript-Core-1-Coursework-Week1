8655abeccf3801cadd4f90a4c49e0220df55e8e6
ShimenAfshar <106038638+ShimenAfshar@users.noreply.github.com>
2022-08-24 21:36:41 +0100

    exercises e,f

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
 
+var greeting = "hello, My name is ";
+var myName = "Shimen";
+var message = greeting + myName;
 console.log(message);
--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
 
+const name = "Shimen";
+const nameLength = name.length;
+const message = "My name is " + name + " and the length of my name " + nameLength;
 console.log(message);
