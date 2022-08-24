b899691cddda83958f59500c62f1e6bf880fb3f7
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2022-08-24 01:10:28 +0100

    Exercise G was completed

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,13 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+const numberOfStudents = 35;
+let messageStudents = "Number of students: " + numberOfStudents;
+console.log(messageStudents);
+
+const numberOfMentors = 10;
+let messageMentors = "Number of mentors: " + numberOfMentors;
+console.log(messageMentors);
+
+let sumStudentsAndMentors =
+  "Total number of students and mentors: " +
+  (numberOfStudents + numberOfMentors);
+console.log(sumStudentsAndMentors);
