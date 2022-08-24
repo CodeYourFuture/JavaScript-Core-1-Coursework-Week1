b4e94df4debd6376de879baa978d85c2f18a9866
Anosh Malik <anoshmalik.am@gmail.com>
2022-08-24 11:33:45 +0100

    Ran - exercise I

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,8 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+var percentStudents = numberOfStudents / (numberOfStudents + numberOfMentors);
+var percentMentors = numberOfMentors / (numberOfMentors + numberOfStudents);
+
+console.log("Percentage students: " + Math.round(percentStudents*100));
+console.log("Percentage mentors: " + Math.round(percentMentors*100));
