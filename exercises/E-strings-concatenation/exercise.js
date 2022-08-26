cedec93f09d7846c7b00e807c7709463598e71b3
Sandra-Duarte <106591234+Sandra-Duarte@users.noreply.github.com>
2022-08-26 06:18:52 +0100

    Exercise E

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,7 @@
 // Start by creating a variable `message`
+const startGreeting = "Hi my name is Sandra.";
+const name = "Sandra Duarte";
+
 
 console.log(message);
+console.log(startGreeting + " " + name);
