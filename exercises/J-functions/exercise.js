6433b1b9ab7957ad7cb1db047b04084fb47fcf00
Adiba <adiba.du.bd@gmail.com>
2022-08-26 01:30:23 +0100

    exercise on function

--- exercises/J-functions/exercise.js
+++ exercises/J-functions/exercise.js
@@ -1,7 +1,12 @@
 function halve(number) {
   // complete the function here
+  return number/2;
 }
 
 var result = halve(12);
+let output = halve(6);
+
+
 
 console.log(result);
+console.log(output);
