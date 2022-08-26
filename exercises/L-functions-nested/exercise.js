ef1a1b9d963006a9ca625834e6d489da4262bf40
Jihad Omer <74451984+JihadOmer@users.noreply.github.com>
2022-08-26 19:07:05 +0100

    L Done !!

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,20 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+
+function upperName(name) {
+    return name.toUpperCase()
+}
+
+function greeting(name) {
+
+    var upperspell = upperName(name)
+    var massage = 'Hello ' + name;
+    return massage;
+}
+console.log(greeting(mentor1));
+console.log(greeting(mentor2));
+console.log(greeting(mentor3));
+console.log(greeting(mentor4));
+console.log(greeting(mentor5));
