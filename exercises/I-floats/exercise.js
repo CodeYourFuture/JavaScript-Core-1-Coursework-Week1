a2cf34c9e1a4816b49370a797cfd7b1615b484fe
Adiba <adiba.du.bd@gmail.com>
2022-08-26 01:20:05 +0100

    exercise float number, math.round() method

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,11 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+
+let totalNumofStudentsndMentors = numberOfMentors + numberOfStudents;
+
+let studentsPercentage = Math.round(numberOfStudents / totalNumofStudentsndMentors* 100);
+let mentorsPercentage = Math.round(numberOfMentors / totalNumofStudentsndMentors * 100);
+
+console.log(`Percentage students: ${studentsPercentage}`);
+console.log(`Percentage mentors: ${mentorsPercentage}`);
