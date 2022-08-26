20862b8fc971e8a5c1dc5278a0c3e4c9f3c034ac
Jihad Omer <74451984+JihadOmer@users.noreply.github.com>
2022-08-26 16:03:56 +0100

    D is done

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,10 @@
 // Start by creating a variable `message`
 
-console.log(message);
+
+
+var greetingStart = "Hello, my name is ";
+var name = "JIHAD";
+
+var greeting = greetingStart + name;
+
+console.log(greeting); 
