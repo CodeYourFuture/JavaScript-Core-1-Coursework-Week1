ac0fa0edf232533926208f69573fe09574268d90
Rebwar <rebwar1.azizi@gmail.com>
2022-08-26 01:59:25 +0100

    delete an Array which unused

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,7 +3,7 @@ const mentor2 = "Irina";
 const mentor3 = "Mimi";
 const mentor4 = "Rob";
 const mentor5 = "Yohannes";
-const names = [mentor1, mentor2, mentor3, mentor4, mentor5];
+
 function upperCase(name) {
   return name.toUpperCase();
 }
