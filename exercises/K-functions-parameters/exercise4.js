ac158457e2b31b784eeed3428bd29c82baf0ed00
Adiba <adiba.du.bd@gmail.com>
2022-08-26 10:27:08 +0100

    exercise on functions-parameters

--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,5 +1,10 @@
 // Declare your function first
+function addition ( a, b){
+    return a + b;
+}
+
 
 // Call the function and assign to a variable `sum`
+let sum = addition( 13, 124)
 
 console.log(sum);
