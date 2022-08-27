3116ae334c7a80210bff42ba39341894e427db3b
Miguel-Cabral <miguelanjo2@hotmail.com>
2022-08-27 16:24:56 +0100

    finish the mandatory exercises

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,12 +1,12 @@
 // Add comments to explain what this function does. You're meant to use Google!
 function getRandomNumber() {
   return Math.random() * 10;
-}
+}//The function above takes a random number using Math.random () and multiplies it by 10
 
 // Add comments to explain what this function does. You're meant to use Google!
 function combine2Words(word1, word2) {
   return word1.concat(word2);
-}
+}////function above combines two arguments
 
 function concatenate(firstWord, secondWord, thirdWord) {
   return "code", "your", "future";
