fd35263be4085dffeba24649b6939836028c8ff5
sasarkarimi <106243476+sasarkarimi@users.noreply.github.com>
2022-08-27 12:44:04 +0100

    js

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,4 @@
-const name = " Daniel  ";
-
-console.log(message);
+const name1 = " Daniel  ";
+const message= "hello i am a ";
+var nameLength = name1.length;
+console.log("my name is" +" "+ name1.trim() + " and my name is "+ nameLength + "character.");
