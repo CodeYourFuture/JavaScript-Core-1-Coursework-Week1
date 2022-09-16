f7c25901dadd7e7b2ea3e5205120048dc6f95246
Cuneyt Turker <93094531+KptCnyt@users.noreply.github.com>
2022-09-16 02:20:05 +0100

    exercises\I-floats\exercise.js -- percentage mark added

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,6 +1,7 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
 var total = numberOfMentors + numberOfStudents;
-var percentageOfStudents = (numberOfStudents/total)*100;
+var percentageOfStudents = Math.round((numberOfStudents/total)*100);
 var percentageOfMentors = Math.round((numberOfMentors/total)*100);
-console.log(percentageOfMentors)
+console.log(`Percentage of mentors : ${percentageOfMentors}%`)
+console.log(`Percentage if students : ${percentageOfStudents}%`)
