092d678631dd23a15e930b4be9c7e6e59b2827b4
esamaalesaei <91167099+esamaalesaei@users.noreply.github.com>
2022-09-17 01:12:13 +0100

    more changes

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,5 +1,24 @@
+
 var mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function uppercaseName(name){
+    var updatedName = name.toUpperCase();
+    return updatedName;
+}
+
+function shouti(mentor){
+    var mentorName = uppercaseName(mentor);
+    var message = "Hello " + mentorName;
+    console.log(message);
+}
+shouti(mentor1);
+shouti(mentor2);
+shouti(mentor3);
+shouti(mentor4);
+shouti(mentor5);
+
+
