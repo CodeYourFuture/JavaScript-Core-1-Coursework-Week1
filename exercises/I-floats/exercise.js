12456d912cc8175a91561b30957e875166446e5e
Baz <61154071+bazmurphy@users.noreply.github.com>
2022-11-20 09:04:29 +0000

    forgot variable declaration keywords

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -2,13 +2,13 @@ const numberOfStudents = 15;
 const numberOfMentors = 8;
 const totalNumber = numberOfStudents + numberOfMentors;
 
-studentsFloat = (numberOfStudents / totalNumber) * 100;
-studentsPercentage = Math.round(studentsFloat);
-studentsMessage = `Percentage students: ${studentsPercentage}%`;
+const studentsFloat = (numberOfStudents / totalNumber) * 100;
+const studentsPercentage = Math.round(studentsFloat);
+const studentsMessage = `Percentage students: ${studentsPercentage}%`;
 
-mentorsFloat = (numberOfMentors / totalNumber) * 100;
-mentorsPercentage = Math.round(mentorsFloat);
-mentorsMessage = `Percentage mentors: ${mentorsPercentage}$`;
+const mentorsFloat = (numberOfMentors / totalNumber) * 100;
+const mentorsPercentage = Math.round(mentorsFloat);
+const mentorsMessage = `Percentage mentors: ${mentorsPercentage}$`;
 
 console.log(studentsMessage);
 console.log(mentorsMessage);
