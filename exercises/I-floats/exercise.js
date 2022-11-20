5aa8ea8a22bfff2ef81599dc34650543f300521f
Shayan Mahnam <95313895+Shayanma94@users.noreply.github.com>
2022-11-20 16:30:04 +0000

    exercises completed

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,10 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var preciseAge = 30.612437;
+var roughAge = Math.round(preciseAge); // 30
+let percentageOfStudents = Math.round((numberOfStudents * 100) / (numberOfMentors + numberOfStudents));
+let percentageOfMentors = Math.round((numberOfMentors * 100) / (numberOfMentors + numberOfStudents)
+);
+
+  console.log("Percentage mentors: " + percentageOfMentors + "%");
+  console.log("Percentage students: " + percentageOfStudents + "%");
