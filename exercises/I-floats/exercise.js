da9233a9654863b5f32e82334622b3e647c82dbe
AndrianaOS <andrianasaffo@gmail.com>
2022-11-21 12:22:37 +0000

    Exercise I-floats completed

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,16 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+const totalNumber = numberOfMentors + numberOfStudents;
+
+let percentageStudents = (numberOfStudents / totalNumber) * 100;
+const roughPercentageStudents = Math.round(percentageStudents);
+
+console.log(percentageStudents);
+console.log(roughPercentageStudents);
+
+let percentageMentors = (numberOfMentors / totalNumber) * 100;
+const roughPercentageMentors = Math.round(percentageMentors);
+
+console.log(percentageMentors);
+console.log(roughPercentageMentors);
