5f6040a4de166114594ec0c5566344137bfd3682
tony-dev-wm <ta.devopsuk@gmail.com>
2022-11-21 20:20:46 +0000

    exercises

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,9 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+var sum=numberOfMentors+numberOfStudents;
+var men=Math.round((numberOfMentors/sum)*100);
+var stu=Math.round((numberOfStudents/sum)*100);
+console.log(men);
+console.log(stu);
+
