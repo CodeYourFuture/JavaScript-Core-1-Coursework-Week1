f31e83e70b44df20c06c731cbfe93b0137efccf1
Zobeir <rigi.zobir@yahoo.com>
2022-11-21 18:16:32 +0000

    round5

--- exercises/J-functions/exercise.js
+++ exercises/J-functions/exercise.js
@@ -1,7 +1,12 @@
 function halve(number) {
-  // complete the function here
+    return number/2;
 }
 
 var result = halve(12);
-
 console.log(result);
+
+
+for(let i=0; i<4; i++) {
+let x=Math.random()*100;
+console.log(`the random number is ${x} and the half of this number is `+ Math.round(halve(x)) );
+}
