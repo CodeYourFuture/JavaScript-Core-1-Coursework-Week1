4e32ffe16fcc674aaa7d845012a8738807dc1efc
AndrianaOS <andrianasaffo@gmail.com>
2022-11-21 08:29:00 +0000

    Exercise F completed

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,15 @@
 // Start by creating a variable `message`
+let name = "Andriana";
+const nameLength = name.length;
+
+console.log(name);
+console.log(nameLength);
+
+let nameLowerCase = name.toLowerCase();
+
+console.log(nameLowerCase);
+
+const message = `  My name is ${name} and my name is ${nameLength} characters long.  `;
 
 console.log(message);
+console.log(message.trim());
