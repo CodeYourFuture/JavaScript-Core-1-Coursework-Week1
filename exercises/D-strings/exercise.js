5ed4bbfb15d981c74ad634aac1aa362a95ead054
AndrianaOS <andrianasaffo@gmail.com>
2022-11-21 08:03:37 +0000

    Exercise in folder D completed

--- exercises/D-strings/exercise.js
+++ exercises/D-strings/exercise.js
@@ -1,3 +1,8 @@
 // Start by creating a variable `message`
-
+const message = "Hello World. This is a string";
 console.log(message);
+console.log(typeof message);
+
+const pounds = 5;
+console.log(pounds);
+console.log(typeof pounds);
