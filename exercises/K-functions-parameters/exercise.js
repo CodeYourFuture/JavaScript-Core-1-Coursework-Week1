8f213c47d4fb25b378aaeed64697bcbc17d28a92
AndrianaOS <andrianasaffo@gmail.com>
2022-11-21 12:55:14 +0000

    Completed exercise 1 of K-functions-parameters

--- exercises/K-functions-parameters/exercise.js
+++ exercises/K-functions-parameters/exercise.js
@@ -1,9 +1,17 @@
 // Complete the function so that it takes input parameters
-function multiply() {
-  // Calculate the result of the function and return it
+function multiply(a, b) {
+  return a * b;
 }
 
 // Assign the result of calling the function the variable `result`
 var result = multiply(3, 4);
+
+console.log(result);
+
+result = multiply(2, 2);
+
+console.log(result);
+
+result = multiply(10, 10);
 
 console.log(result);
