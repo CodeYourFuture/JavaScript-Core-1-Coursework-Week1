710fe05f9180e937e1d2788e4fc69038612934d9
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2022-11-21 11:06:37 +0000

    Resolved exercises

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,16 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function stringToUpperCase(text) {
+  return text.toUpperCase();
+}
+
+function createShoutyGreeting(name) {
+  return `HELLO ${stringToUpperCase(name)}`;
+}
+
+console.log(createShoutyGreeting("Mariana"));
+console.log(createShoutyGreeting("jack"));
+console.log(createShoutyGreeting("David"));
+console.log(createShoutyGreeting("Anna"));
