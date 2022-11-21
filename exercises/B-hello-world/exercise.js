51a4debffddc38e4e67e4a7b6978ad78b9db4bc5
AndrianaOS <andrianasaffo@gmail.com>
2022-11-21 07:49:15 +0000

    Folder A and B under exercises completed

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,7 @@
 console.log("Hello world");
+console.log("Hello World. I just started learning JavaScript!");
+
+//npm test -- --testPathPattern mandatory/1-syntax-errors.js -> run single test
+
+console.log(3);
+console.log(Hello);
