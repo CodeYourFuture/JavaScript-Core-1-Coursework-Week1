6d954d78594d9e516fabd39a8ba0b01515e6d59d
elahe <106202329+elahemortazavi@users.noreply.github.com>
2022-11-21 20:06:48 +0000

    syntax-error

--- mandatory/1-syntax-errors.js
+++ mandatory/1-syntax-errors.js
@@ -2,19 +2,26 @@
 
 function addNumbers(a, b, c){
   return a+b+c;
-}
 
+}
 console.log(addNumbers(1, 2, 3));
 
 
-// function introduceMe(name, age)
-//   return `Hello, my name is "  ${name} "and I am " ${age}  years old`;
+function introduceMe(name, age){
 
-// function getTotal(a, b) {
-//   total = a + b;
+  return (`Hello, my name is ${name} and I am ${age} years old`);
+}
+console.log(introduceMe("elahe", 30));
 
-//   return "The total is total";
-// }
+
+
+function getTotal(a, b) {
+  total = a + b;
+
+  return `The total is ${total}`;
+
+}
+console.log(getTotal(2, 4));
 
 /* 
 ===================================================
