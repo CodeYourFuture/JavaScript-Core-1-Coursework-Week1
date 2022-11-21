7db96bb5db3f78a3a4045e72c987c3f45a60df5b
AndrianaOS <andrianasaffo@gmail.com>
2022-11-21 13:08:22 +0000

    Exercise 3 K-functions-parameters completed

--- exercises/K-functions-parameters/exercise3.js
+++ exercises/K-functions-parameters/exercise3.js
@@ -1,5 +1,19 @@
-// Write your function here
+function createGreeting(name) {
+  return (message = `Hello, my name is ${name}`);
+}
 
 var greeting = createGreeting("Daniel");
+
+console.log(greeting);
+
+greeting = createGreeting("Day");
+
+console.log(greeting);
+
+greeting = createGreeting("Lewis");
+
+console.log(greeting);
+
+greeting = createGreeting("Junior");
 
 console.log(greeting);
