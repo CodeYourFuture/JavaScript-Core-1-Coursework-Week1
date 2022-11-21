4f2c12b3c9dab7ceffabd1b9c9725cd59846ff82
Zobeir <rigi.zobir@yahoo.com>
2022-11-21 23:48:27 +0000

    round6

--- exercises/K-functions-parameters/exercise3.js
+++ exercises/K-functions-parameters/exercise3.js
@@ -1,5 +1,6 @@
 // Write your function here
-
+function createGreeting(A){
+    return "How are you? "+A;
+}
 var greeting = createGreeting("Daniel");
-
 console.log(greeting);
--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,5 +1,24 @@
+
+function upper(name){
+    return name.toUpperCase();
+}
+
+function GreetingMentors(name){
+    return `Hello ${upper(name)}`;
+}
+
+function print(name){
+    console.log(GreetingMentors(name));
+}
+
 var mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+print(mentor1);
+print(mentor2);
+print(mentor3);
+print(mentor4);
+print(mentor5);;
