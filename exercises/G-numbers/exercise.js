32e80392d91f28a7fcb1db355f98f60f5c3f5977
Zobeir <rigi.zobir@yahoo.com>
2022-11-21 17:43:48 +0000

    round4

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,5 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+let numberOfStudents=39;
+let numberOfMentors=5;
+let Totalnumber = numberOfStudents + numberOfStudents;
+console.log(`the total number of students and mentors are =  ${Totalnumber}`);
