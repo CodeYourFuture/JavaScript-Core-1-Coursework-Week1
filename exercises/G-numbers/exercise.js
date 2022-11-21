477052e091ac37933875c74aacf233a0bbc3c55a
AndrianaOS <andrianasaffo@gmail.com>
2022-11-21 08:41:38 +0000

    Exercise G completed

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,7 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+let numberOfStudents = 15;
+let numberOfMentors = 8;
+
+const total = numberOfMentors + numberOfStudents;
+
+console.log(total);
