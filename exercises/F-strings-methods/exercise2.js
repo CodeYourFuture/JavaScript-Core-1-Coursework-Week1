f155496d417ca110f7851f276add63446fcae7c9
AndrianaOS <andrianasaffo@gmail.com>
2022-11-21 08:34:30 +0000

    Exercise 2 in folder F completed

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,7 @@
 const name = " Daniel  ";
+const nameLength = name.length;
+
+const message = `  My name is ${name} and my name is ${nameLength} characters long.  `;
 
 console.log(message);
+console.log(message.trim());
