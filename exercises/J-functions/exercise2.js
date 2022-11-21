ac8e16fcf5e2dc03319627d0386f0b8da18581f1
AndrianaOS <andrianasaffo@gmail.com>
2022-11-21 12:50:19 +0000

    Exercise 2 for J-functions completed

--- exercises/J-functions/exercise2.js
+++ exercises/J-functions/exercise2.js
@@ -1,7 +1,19 @@
 function triple(number) {
-  // complete function here
+  return number * 3;
 }
 
 var result = triple(12);
+
+console.log(result);
+
+result = triple(10);
+
+console.log(result);
+
+result = triple(8);
+
+console.log(result);
+
+result = triple(6);
 
 console.log(result);
