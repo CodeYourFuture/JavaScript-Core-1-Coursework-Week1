6e2c92247256b982e363521bc38b16865fc5dd56
ys35code <ys35code@gmail.com>
2022-11-22 00:01:25 +0000

    exercise L - completed

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,14 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function createCreeting(names) {
+  let greet = "HELLO" + names.toUpperCase();
+  return greet;
+}
+
+console.log(createCreeting("Daniel"));
+console.log(createCreeting("Irina"));
+console.log(createCreeting("Mimi"));
+console.log(createCreeting("Rob"));
+console.log(createCreeting("Yohannes"));
