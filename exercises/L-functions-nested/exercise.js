66b46dd312bb82ee5d37592bbcef5e0ba60c2ac0
AndrianaOS <andrianasaffo@gmail.com>
2022-11-22 11:57:14 +0000

    L-functions-nested completed
    Exercise in L-functions-nested has been completed

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -10,3 +10,18 @@ var mentor5 = "Yohannes";
 
 // console.log(nameLowerCase); // "daniel"
 // ```;
+
+function nameUpperCase(name) {
+  return name.toUpperCase();
+}
+
+function shoutyGreeting(name) {
+  let upperCaseName = nameUpperCase(name);
+  return `HELLO ${upperCaseName}`;
+}
+
+console.log(shoutyGreeting(mentor1));
+console.log(shoutyGreeting(mentor2));
+console.log(shoutyGreeting(mentor3));
+console.log(shoutyGreeting(mentor4));
+console.log(shoutyGreeting(mentor5));
