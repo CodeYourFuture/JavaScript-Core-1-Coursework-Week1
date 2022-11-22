ee9f1d606a0ed6af31654ada617bbf97145adbb6
Mo Mahboobian <momahboobian@gmail.com>
2022-11-22 15:00:25 +0000

    exercis L updated

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -4,12 +4,17 @@ var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
 
-function shoutyGreeting(a) {
+function mentorUpperCase(a) {
   return a.toUpperCase();
 }
 
-console.log(shoutyGreeting("Hello ").concat(mentor1.toUpperCase()));
-console.log(shoutyGreeting("Hello ").concat(mentor2.toUpperCase()));
-console.log(shoutyGreeting("Hello ").concat(mentor3.toUpperCase()));
-console.log(shoutyGreeting("Hello ").concat(mentor4.toUpperCase()));
-console.log(shoutyGreeting("Hello ").concat(mentor5.toUpperCase()));
+function shoutyGreeting(a) {
+  let nameUpperCase = mentorUpperCase(a);
+  return "HELLO".concat(" ", nameUpperCase);
+}
+
+console.log(shoutyGreeting(mentor1));
+console.log(shoutyGreeting(mentor2));
+console.log(shoutyGreeting(mentor3));
+console.log(shoutyGreeting(mentor4));
+console.log(shoutyGreeting(mentor5));
