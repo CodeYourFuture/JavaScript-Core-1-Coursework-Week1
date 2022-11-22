09a00945f3bcab9719317820fac07a3bb19b8fef
Mo Mahboobian <momahboobian@gmail.com>
2022-11-22 01:24:23 +0000

    exercises update all

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,5 @@
 console.log("Hello world");
+console.log("Hello world. I just started learning JavaScript!");
+// console.log(Are you joking with me!);
+// without quotation the node issue an error "syntaxError: Missing" after argument list
+console.log(5);
