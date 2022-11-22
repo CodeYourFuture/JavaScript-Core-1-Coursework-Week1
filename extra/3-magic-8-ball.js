b97289c52ff757dc2de81e18e02bb0e94fc4be1c
Mohamed Abdalla <mohamed.e.abdalla1@gmail.com>
2022-11-22 12:39:57 +0000

    Update 3-magic-8-ball.js

--- extra/3-magic-8-ball.js
+++ extra/3-magic-8-ball.js
@@ -1,3 +1,4 @@
+// const { toBeOneOf } = require("jest-extended");
 /**
 
   Let's peer into the future using a Magic 8 Ball!
@@ -45,35 +46,35 @@
 
 // This should log "The ball has shaken!"
 // and return the answer.
-  let answers=['It is certain.', 'It is decidedly so.',
-    'Without a doubt.',
-    'Yes - definitely.',
-    'You may rely on it.',
-    'As I see it, yes.',
-    'Most likely.',
-    'Outlook good.',
-    'Yes.',
-    'Signs point to yes.',
-    'Reply hazy, try again.',
-    'Ask again later.',
-    'Better not tell you now.',
-    'Cannot predict now.',
-    'Concentrate and ask again.',
-    'Don\'t count on it.',
-    'My reply is no.',
-    'My sources say no.',
-    'Outlook not so good.',
-    'Very doubtful.'
-  ]
-        let answer = "";
+let answers = [
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes - definitely.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful.",
+];
+let answer = "";
 
-  function shakeBall() {
-    //Write your code in here
-    console.log("The ball has shaken!");
-    return (answer = answers[Math.floor(Math.random() * answers.length)]);
-
-  };
-
+function shakeBall() {
+  //Write your code in here
+  console.log("The ball has shaken!");
+  return (answer = answers[Math.floor(Math.random() * answers.length)]);
+}
 
 /* 
   This function should say whether the answer it is given is
@@ -84,7 +85,7 @@
 
   This function should expect to be called with any value which was returned by the shakeBall function.
 */
-  let positiveOrNegative ="";
+let positiveOrNegative = "";
 
 function checkAnswer(answer) {
   //Write your code in here
@@ -121,11 +122,10 @@ function checkAnswer(answer) {
   return positiveOrNegative;
 }
 
-
-  // shakeBall();
-  // console.log(answer);
-  // checkAnswer("Very doubtful.");
-  // console.log(positiveOrNegative);
+// shakeBall();
+// console.log(answer);
+// checkAnswer("Very doubtful.");
+// console.log(positiveOrNegative);
 /* 
 ==================================
 ======= TESTS - DO NOT MODIFY =====
@@ -165,7 +165,9 @@ test("magic 8 ball returns different values each time", () => {
     );
   }
 
-  let seenPositivities = new Set(Array.from(seenAnswers.values()).map(checkAnswer));
+  let seenPositivities = new Set(
+    Array.from(seenAnswers.values()).map(checkAnswer)
+  );
   if (seenPositivities.size < 2) {
     throw Error(
       "Expected to random answers with different positivities each time shakeBall was called, but always got the same one"
