078753b8ba90933b2550e9bec38fb3c576599757
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2022-11-22 21:01:07 +0000

    exercises done!

--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -1,5 +1,8 @@
 // Declare your function first
+function add(a,b){
+    return a + b;
+}
 
 // Call the function and assign to a variable `sum`
-
+sum = add(13, 124);
 console.log(sum);
--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,20 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function createShoutyGreeting(mentor1, mentor2, mentor3,mentor4,mentor5){
+    greeting =
+      "Hello " +
+      mentor1.toUpperCase() +
+      "\nHello " +
+      mentor2.toUpperCase() +
+      "\nHello " +
+      mentor3.toUpperCase() +
+      "\nHello " +
+      mentor4.toUpperCase() +
+      "\nHello " +
+      mentor5.toUpperCase();
+    return greeting;
+
+} 
+console.log(createShoutyGreeting("Daniel", "Irina", "Mimi", "Rob", "Yohannes"));
