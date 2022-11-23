7c1f65fde78d3a3dac4adb87564e7a29053eed4a
elahe <106202329+elahemortazavi@users.noreply.github.com>
2022-11-23 23:51:26 +0000

    let instead var

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -11,8 +11,8 @@ console.log(greeting("yohannes").toUpperCase());
 
 
 
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+let mentor1 = "Daniel";
+let mentor2 = "Irina";
+let mentor3 = "Mimi";
+let mentor4 = "Rob";
+let mentor5 = "Yohannes";
