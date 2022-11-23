f25dfde1ca167d07c3bfc32a6cc4d1c932ed0598
SagharHosseinmardi <saghar.h.mardi@gmail.com>
2022-11-23 21:03:47 +0000

    exercises

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,9 @@
-// Start by creating a variable `message`
+let greetingStart = "Hello, my name is ";
+let firstName = "Saghar";
+let greeting = greetingStart + firstName;
+let nameLength = firstName.length;
 
-console.log(message);
+console.log(greeting + " and my name is " + nameLength + " characters long");
+
+// let nameLowerCase = firstName.toLowerCase();
+// console.log(nameLowerCase);
--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,23 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function upperCaseName(teacher) {
+  return teacher.toUpperCase();
+}
+
+// function shoutyGreeting(name) {
+//   console.log("HELLO " + name);
+// }
+
+// shoutyGreeting(mentor1);
+
+function shoutyGreeting(name) {
+  return "HELLO " + upperCaseName(name);
+}
+
+console.log(shoutyGreeting(mentor1));
+console.log(shoutyGreeting(mentor2));
+console.log(shoutyGreeting(mentor3));
+console.log(shoutyGreeting(mentor4));
+console.log(shoutyGreeting(mentor5));
