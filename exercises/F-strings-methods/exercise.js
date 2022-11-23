4951368a10594ccd27f0d395f81705d896d3faab
Sana Asaf <85436780+SanaAsaf@users.noreply.github.com>
2022-11-23 20:27:46 +0000

    Exercises and mandatory done
    Still working on the extra exercises.

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
+var name = "Daniel";
+var nameLength = name.length;
+var combinedstring ="My name is "+ name +" and my name is "+ nameLength +" characters long";
+console.log(combinedstring); // Logs 6
 
-console.log(message);
--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,5 @@
 const name = " Daniel  ";
+var nameLength = name.length;
+var combinedstring ="My name is "+ name.trim +" and my name is "+ nameLength +" characters long";
+console.log(combinedstring);
 
-console.log(message);
