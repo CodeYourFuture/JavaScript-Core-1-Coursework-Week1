25b1baa6ebd1d27e98875936ad7c5d38a21845ed
OleksChep <aachep@gmail.com>
2022-11-23 19:53:37 +0000

    D ex1 ex2

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,25 @@
 // Start by creating a variable `message`
-
+var name1 = "Oleks";
+var nameLength = name1.length;
+var introduce1 = " My name is Oleks and my name is ";
+var introduce2 = "characters long.";
+var message = introduce1 + nameLength + " " + introduce2;
 console.log(message);
+// second way
+var name1 = "Oleks";
+var nameLength = name1.length;
+var introduce1 = "      My name is ";
+var introduce3 = "and my name is ";
+var introduce2 = "characters long.";
+var message = introduce1 + name1 + " "+ introduce3 + 
+nameLength + " " + introduce2;
+console.log(message);
+// trim method
+var name1 = "Oleks";
+var nameLength = name1.length;
+var introduce1 = " My name is ";
+var introduce3 = "and my name is ";
+var introduce2 = "characters long.";
+var message =
+  introduce1 + name1 + " " + introduce3 + nameLength + " " + introduce2;
+console.log(message.trim());
