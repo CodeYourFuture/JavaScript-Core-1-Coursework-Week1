d9011c414b8f79b094014321f15465f249d7764b
OleksChep <aachep@gmail.com>
2022-11-23 21:56:17 +0000

    J functions

--- exercises/J-functions/exercise2.js
+++ exercises/J-functions/exercise2.js
@@ -1,7 +1,9 @@
 function triple(number) {
-  // complete function here
+ return number*3;
 }
 
 var result = triple(12);
 
-console.log(result);
+console.log("result 12 x 3 = ", result);
+
+//   result 12 x 3 =  36
