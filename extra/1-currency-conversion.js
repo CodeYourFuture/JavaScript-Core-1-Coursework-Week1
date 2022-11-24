683f499a7b6d33f5470f2adef47b1d83292e96db
OleksChep <aachep@gmail.com>
2022-11-24 02:17:24 +0000

    extra currency conversion dolar

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -5,7 +5,11 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(price) {
+  let priceDolar = pricePound*1.4;
+  return "$" + priceDolar.toFixed(2);
+}
+console.log(priceDolar);
 
 /*
   CURRENCY CONVERSION
