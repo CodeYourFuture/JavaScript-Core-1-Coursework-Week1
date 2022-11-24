f8e3820c4c421eac1fd351acc9b1a0afa275d3d5
hadipournigjeh <108987953+hadipournigjeh@users.noreply.github.com>
2022-11-24 11:36:01 +0000

    The + was more in this line
    one + was enough and one of them was more then I removed one of them

--- mandatory/1-syntax-errors.js
+++ mandatory/1-syntax-errors.js
@@ -1,6 +1,6 @@
 // There are syntax errors in this code - can you fix it to pass the tests?
 
-function addNumbers(a,b,c) {
+function addNumbers(a, b, c) {
   return a + b + c;
 }
 
@@ -9,7 +9,7 @@ function introduceMe(name, age) {
 }
 
 function getTotal(a, b) {
-  total = a ++ b;
+  total = a + b;
 
   return "The total is total";
 }
