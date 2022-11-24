1e5a3fb4e7bb9714ce81663d105333943a560581
Natalie Zablotska <113237479+nataliiazab@users.noreply.github.com>
2022-11-24 21:17:34 +0000

    complete exercise L

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,19 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function changeToUpper(mentor) {
+  return mentor.toUpperCase();
+}
+
+function createGreeting(mentor) {
+  var mentorToUpper = changeToUpper(mentor);
+  var message = "HELLO " + mentorToUpper;
+  return message;
+}
+
+console.log(createGreeting(mentor1));
+console.log(createGreeting(mentor2));
+console.log(createGreeting(mentor3));
+console.log(createGreeting(mentor4));
+console.log(createGreeting(mentor5));
