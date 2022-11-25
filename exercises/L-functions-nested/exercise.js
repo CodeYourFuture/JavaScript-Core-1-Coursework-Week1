aa47bc580eb5d615d672e8a42c139b93cf433724
Kristina <kristina.dudnyk20@gmail.com>
2022-11-25 03:11:58 +0000

    L

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,16 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function namesToUpperCase(text) {
+  return text.toUpperCase();
+}
+
+function createShoutyGreeting(name) {
+  return `HELLO ${namesToUpperCase(name)}`;
+}
+
+console.log(createShoutyGreeting("Mariana"));
+console.log(createShoutyGreeting("jack"));
+console.log(createShoutyGreeting("David"));
+console.log(createShoutyGreeting("Anna"));
