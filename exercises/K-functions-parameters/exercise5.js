ee30240ae9bde0590005206b824f79127fc984e8
LorenaCapraru <108892538+LorenaCapraru@users.noreply.github.com>
2022-11-25 12:25:08 +0000

    Last commit

--- exercises/K-functions-parameters/exercise5.js
+++ exercises/K-functions-parameters/exercise5.js
@@ -1,5 +1,7 @@
 // Declare your function here
-
+function createLongGreeting(name, age) {
+  return `Hello, my name is ${name} and I'm ${age} years.`;
+}
 const greeting = createLongGreeting("Daniel", 30);
 
 console.log(greeting);
