5df43b7a5ff5fef8b7b569fe6b8e081b6d9285dd
OlushoreOdude <108965036+OlushoreOdude@users.noreply.github.com>
2022-11-25 18:19:27 +0000

    Update exercise.js

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,12 @@
-var numberOfStudents = 15;
-var numberOfMentors = 8;
+let numberOfStudents = 15;
+let numberOfMentors = 8;
+let sum = (numberOfStudents + numberOfMentors);
+
+let percentage = function percent(val, total){ 
+ return (Math.round(val / total) * 100 );
+}
+
+let perOfStud = percentage(numberOfStudents, sum);
+let perOfMent = percentage(numberOfMentors, sum); 
+console.log(`Percentage students: ${perOfStud}`);
+console.log(`Percentage Mentors: ${perOfMent}`);
