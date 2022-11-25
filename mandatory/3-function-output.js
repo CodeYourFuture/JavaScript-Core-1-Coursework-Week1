6ac749e37414758746e1dda86717235d15e19031
Kristina <kristina.dudnyk20@gmail.com>
2022-11-25 02:58:46 +0000

    mandatory

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -2,15 +2,17 @@
 function getRandomNumber() {
   return Math.random() * 10;
 }
+//this function Math.random will  call any random a  0 < Number <  10 
 
 // Add comments to explain what this function does. You're meant to use Google!
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
+//this function use concat() method appends (concatenate) a string to the end of another string, then means she contact then together
 
 function concatenate(firstWord, secondWord, thirdWord) {
-  // Write the body of this function to concatenate three words together.
-  // Look at the test case below to understand what this function is expected to return.
+return firstWord + " " + secondWord + " " + thirdWord;    let message = firstWord + ' ' + secondWord + ' ' + thirdWord;
+    return message;
 }
 
 /* 
