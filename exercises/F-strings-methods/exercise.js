4c6b72292ee0f3ea0c82138b6c85bdd8979f26fa
OlushoreOdude <108965036+OlushoreOdude@users.noreply.github.com>
2022-11-25 17:56:14 +0000

    Update exercise.js

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
-
+let name = "olus"
+let nameLength = name.length;
+let message = `my name is ${name} and my name is ${nameLength} long`;
 console.log(message);
