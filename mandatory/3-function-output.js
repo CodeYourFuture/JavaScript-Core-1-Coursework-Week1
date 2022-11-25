6a7bb1d66aa92e66b1126eefe6e223362ec6b4c0
LorenaCapraru <108892538+LorenaCapraru@users.noreply.github.com>
2022-11-25 13:36:09 +0000

    last commit

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,15 +1,16 @@
-// Add comments to explain what this function does. You're meant to use Google!
+// it generates a random number using Math.random() library and returns the number multiplied with 10
 function getRandomNumber() {
   return Math.random() * 10;
 }
 
-// Add comments to explain what this function does. You're meant to use Google!
+// it concatenates 2 different strings using concat() method
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
+  return firstWord.concat(" ", secondWord.concat(" ", thirdWord));
   // Look at the test case below to understand what this function is expected to return.
 }
 
