1174b5422a86bb2d0de56ba9ff5b91cb8e28637d
Kristina <kristina.dudnyk20@gmail.com>
2022-11-25 03:11:35 +0000

    K

--- exercises/K-functions-parameters/exercise3.js
+++ exercises/K-functions-parameters/exercise3.js
@@ -1,5 +1,7 @@
 // Write your function here
-
+function createGreeting(greeting) {
+    return 'Hello, my name is ' + greeting;
+}
 var greeting = createGreeting("Daniel");
 
 console.log(greeting);
