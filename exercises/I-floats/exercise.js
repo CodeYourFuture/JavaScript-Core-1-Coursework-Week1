01ae4f206960c9618e08f0c9e420d525df8e19f9
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2022-11-25 22:20:53 +0000

    Exercise/I-floats completed!

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,15 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+students = numberOfStudents;
+mentors = numberOfMentors;
+
+var totalInClass = students + mentors;
+let percentageOfStudent = (students / totalInClass) * 100;
+let percentageOfStudentRound = Math.round(percentageOfStudent);
+
+console.log("Percentage students: " + percentageOfStudentRound);
+
+let percentageOfMentorsRound = Math.round((mentors / totalInClass) * 100);
+let messagePercentageOfMentors =
+  "Percentage Mentors: " + percentageOfMentorsRound;
+console.log(messagePercentageOfMentors);
