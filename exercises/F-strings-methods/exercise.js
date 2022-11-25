d9f02366ec552fb099d8d2640589928d96f7cc6a
Kristina <kristina.dudnyk20@gmail.com>
2022-11-25 03:05:33 +0000

    E,  F

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,5 @@
-// Start by creating a variable `message`
+const name = "Daniel";
+let nameLength = name.length;
+message = "My name is " + name + " and my name is " + nameLength + " characters long";
 
 console.log(message);
--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,6 @@
 const name = " Daniel  ";
-
+let myName = name.trim();
+let nameLength = myName.length;
+message =
+  "My name is " + myName + " and my name is " + nameLength + " characters long";
 console.log(message);
