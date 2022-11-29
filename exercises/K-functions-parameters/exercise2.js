3ab14ac15c57578843885ce013d87f5e920b3acd
Farzaneh Haghani <68525882+farzaneh-haghani@users.noreply.github.com>
2022-11-29 19:10:06 +0000

    exercise K updated

--- exercises/K-functions-parameters/exercise2.js
+++ exercises/K-functions-parameters/exercise2.js
@@ -1,6 +1,8 @@
 // Declare your function first
 function divide(x, y) {
-    return x / y;
+    if (y !== 0)
+        return x / y;
+    return "It is not possible!"
 }
 
 var result = divide(3, 4);
