89aaeaf176e4a3cc3f403d629cf58bdb0b27987f
monahvi <nahvi.moghaddam@gmail.com>
2022-12-01 17:35:08 +0000

    some change

--- exercises/K-functions-parameters/exercise4.js
+++ exercises/K-functions-parameters/exercise4.js
@@ -3,7 +3,7 @@
 // Call the function and assign to a variable `sum`
 
 // console.log(sum);
-function sum(num1,num2){
-    return num1+num2
+function sum(num1, num2) {
+    return num1 + num2
 }
-console.log(sum(13,124));
+console.log(sum(13, 124));
