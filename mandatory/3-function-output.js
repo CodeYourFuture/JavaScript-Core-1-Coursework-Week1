07649fa028d3222e13622c1a31eacdf00ca1dc92
Olena Reukova <kronverk01@gmail.com>
2022-12-03 03:08:58 +0000

    mandatory
    syntax
    logic
    function
    tax

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,18 +1,21 @@
 // Add comments to explain what this function does. You're meant to use Google!
+// Get random number and multiply it by 10
 function getRandomNumber() {
   return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+//concatenates the string arguments to the calling string and returns a new string
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
+  return firstWord.concat(' ', secondWord, thirdWord);
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
 }
-
+const result = concatenate(code, your, future);
 /* 
 ===================================================
 ======= TESTS - DO NOT MODIFY BELOW THIS LINE =====
