fcb7aa92768554ca854b216174362ae7b8900384
ZahraaTayyar <zahraatayyar@gmail.com>
2023-01-30 17:26:45 +0000

    started extra 3

--- extra/3-magic-8-ball.js
+++ extra/3-magic-8-ball.js
@@ -43,6 +43,38 @@
     Very doubtful.
 */
 
+const veryPositiveAnswers = [
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes - definitely.",
+  "You may rely on it."
+]
+
+const positiveAnswers = [
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes."
+]
+
+const negative = [
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again."
+]
+
+const veryNegative = [
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful."
+]
+
 // This should log "The ball has shaken!"
 // and return the answer.
 function shakeBall() {
