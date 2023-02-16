b803ace6f5fbb002d77d64be13ff505ad55ef3b6
Mpanasetckiy <maxbuzz8694@gmail.com>
2023-02-16 21:22:15 +0000

    all done except magic ball

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -5,7 +5,9 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(num) {
+  return num * 1.4;
+}
 
 /*
   CURRENCY CONVERSION
@@ -15,7 +17,10 @@ function convertToUSD() {}
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL() {}
+function convertToBRL(brp) {
+  let brl = brp * 5.7;
+  return (Math.round((brl * 0.99) * 100) / 100) ;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -3,12 +3,17 @@ function getRandomNumber() {
   return Math.random() * 10;
 }
 
+// Math.random() can be used to return random integers.
+
 // Add comments to explain what this function does. You're meant to use Google!
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
 
+// The concat() method concatenates the string arguments to the calling string and returns a new string.
+
 function concatenate(firstWord, secondWord, thirdWord) {
+  return  firstWord.concat(" ", secondWord, " ", thirdWord);
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
 }
