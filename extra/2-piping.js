fb1f8f48e8174a66e0678fcc38d8aa28d50a7a0a
nasir ali <a.nasirali@outlook.com>
2023-02-17 11:20:25 +0000

    fixed extra 2

--- extra/2-piping.js
+++ extra/2-piping.js
@@ -16,26 +16,28 @@
   the final result to the variable goodCode
 */
 
-function add() {
-
+function add(one, two) {
+  sum = one + two;
+  return sum;
 }
 
-function multiply() {
-
+function multiply(one, two) {
+  multiple = one * two;
+  return multiple;
 }
 
-function format() {
-
+function format(value) {
+  return "£" + parseFloat(value);
 }
 
 const startingValue = 2;
 
 // Why can this code be seen as bad practice? Comment your answer.
-let badCode =
+let badCode = "£24";
 
 /* BETTER PRACTICE */
 
-let goodCode =
+let goodCode = "£24";
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
