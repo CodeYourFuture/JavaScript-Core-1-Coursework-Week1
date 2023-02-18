6f3edf04a19f9eb123a704e777b07f2df7d5bd1e
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-02-18 12:59:55 +0000

    Update 1-syntax-errors.js

--- mandatory/1-syntax-errors.js
+++ mandatory/1-syntax-errors.js
@@ -7,11 +7,11 @@ function addNumbers(a, b, c) {
 function introduceMe(name, age){
   return `Hello, my name is ${name}and I am ${age} years old`;
 }
-function getTotal(a, b) {
-  total = a + b;
+function getTotal(a, b){
+  total = a + b;}
 
-  return "The total is total";
-}
+  return `The total is ${total}';
+
 
 /* 
 ===================================================
