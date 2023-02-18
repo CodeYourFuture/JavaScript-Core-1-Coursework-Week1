db68532ab101f27da975704025fb77efdbbc5e1c
Delnia Alipour <delniaalipur@gmail.com>
2023-02-18 14:30:47 +0000

    Madatory and two of extra pratices

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -2,15 +2,17 @@
 function getRandomNumber() {
   return Math.random() * 10;
 }
+// this function returns a random number between 0 and 10.
 
 // Add comments to explain what this function does. You're meant to use Google!
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
-
+// this function merger to or more string together and create a new string.
 function concatenate(firstWord, secondWord, thirdWord) {
-  // Write the body of this function to concatenate three words together.
-  // Look at the test case below to understand what this function is expected to return.
+let newString= firstWord.concat(" ",secondWord," ", thirdWord);
+return newString;
+
 }
 
 /* 
