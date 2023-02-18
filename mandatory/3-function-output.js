d0c28ff253319343c4b0bdd8f72ec674b7859c72
saqibjvd <70890519+saqibjvd@users.noreply.github.com>
2023-02-18 16:38:45 +0000

    syntax error

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,7 +1,11 @@
 // Add comments to explain what this function does. You're meant to use Google!
+
+// this multiply 10 to random number generated between 0 and 1.
+
 function getRandomNumber() {
   return Math.random() * 10;
 }
+console.log(getRandomNumber());
 
 // Add comments to explain what this function does. You're meant to use Google!
 function combine2Words(word1, word2) {
@@ -9,6 +13,7 @@ function combine2Words(word1, word2) {
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
+  return firstWord, secondWord, thirdWord;
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
 }
