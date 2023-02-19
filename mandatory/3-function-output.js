7436cf4b74f44380d1ba7289de0f8ce9a227f509
Hussein <hussein.codes@gmail.com>
2023-02-19 02:09:42 +0000

    3rd solved

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,14 +1,15 @@
 // Add comments to explain what this function does. You're meant to use Google!
 function getRandomNumber() {
   return Math.random() * 10;
-}
+} //will probably pick a random num (sorry Greg!) and multipley it by 10.
 
 // Add comments to explain what this function does. You're meant to use Google!
 function combine2Words(word1, word2) {
   return word1.concat(word2);
-}
+}// this will add the content/value of word2 to the one of word1.
 
 function concatenate(firstWord, secondWord, thirdWord) {
+  return firstWord + " " + secondWord + " " + thirdWord;
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
 }
