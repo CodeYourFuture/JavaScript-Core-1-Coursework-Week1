aa894baaf1522aab8634bc1269098e159ad77466
ahmedsaif2002 <ahmedsaif2002@gmail.com>
2023-02-20 03:20:56 +0000

    extra/2-piping
    extra/2-piping  has been fixed

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -5,7 +5,13 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+// function convertToUSD() {}
+function convertToUSD(priceGBP) {
+   exchangeRate = 1.4;
+  const priceUSD = priceGBP * exchangeRate;
+  return priceUSD;
+}
+
 
 /*
   CURRENCY CONVERSION
@@ -15,7 +21,13 @@ function convertToUSD() {}
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL() {}
+
+function convertToBRL(priceGBP) {
+ const exchangeRate = 5.7;
+const transactionFee = 0.01;
+return priceBRL= priceGBP * exchangeRate * (1 - transactionFee);
+
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
