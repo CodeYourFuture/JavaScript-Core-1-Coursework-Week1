28d793f1542d63b074b96f0049f648562dca016e
Appolin Semegni Fotso <108607571+AppolinFotso@users.noreply.github.com>
2023-02-21 13:36:40 +0000

    Passed test

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,9 +1,11 @@
 // Add comments to explain what this function does. You're meant to use Google!
+// This function create a random number that is multiplied by 10.
 function getRandomNumber() {
   return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+// This function take two string arguments and make them one string by concatenating them.
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
@@ -11,6 +13,7 @@ function combine2Words(word1, word2) {
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+  return `${firstWord} ${secondWord} ${thirdWord}`;
 }
 
 /* 
