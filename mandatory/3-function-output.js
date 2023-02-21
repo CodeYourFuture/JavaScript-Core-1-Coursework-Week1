9f46125c152f3c67f2c51a5fb955540639820b9e
CYF <cyf@CYFs-MacBook-Pro.local>
2023-02-21 23:03:20 +0000

    fixed

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -2,15 +2,20 @@
 function getRandomNumber() {
   return Math.random() * 10;
 }
+//Math.random() The Math.random() static method returns a floating-point, pseudo-random number that's greater than or equal to 0 and less than 1, with approximately uniform distribution over that range //
+
 
 // Add comments to explain what this function does. You're meant to use Google!
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
+//The concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.//
 
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+  let sentence = firstWord.concatenate(" ",secondWord , " ",thirdWord);
+  return sentence;
 }
 
 /* 
