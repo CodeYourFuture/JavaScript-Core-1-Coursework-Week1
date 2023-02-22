5a0fe979db8bd675fe04d73fd81659fcf7170cb0
saqibjvd <70890519+saqibjvd@users.noreply.github.com>
2023-02-22 02:24:21 +0000

    mandotry
    completed mandatory  exercises ,

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,19 +1,22 @@
 // Add comments to explain what this function does. You're meant to use Google!
 
-// this multiply 10 to random number generated between 0 and 1.
+// Math.random() returns a random number between 0 (inclusive),  and 1 (exclusive). this  code generate random number between 0 and 1 then multiply by 10.
 
 function getRandomNumber() {
   return Math.random() * 10;
 }
-console.log(getRandomNumber());
 
 // Add comments to explain what this function does. You're meant to use Google!
+
+// The concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
+
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
-  return firstWord, secondWord, thirdWord;
+  return firstWord.concat(" ", secondWord, " ", thirdWord);
+
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
 }
