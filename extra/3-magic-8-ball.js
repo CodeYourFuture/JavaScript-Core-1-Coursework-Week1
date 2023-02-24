3d66a53635e85fb45b5de4ce31c397900a6c92c9
mandy-cheung <109825538+m4ndycheung@users.noreply.github.com>
2023-02-24 17:21:21 +0000

    Added to extra-magic 8 ball

--- extra/3-magic-8-ball.js
+++ extra/3-magic-8-ball.js
@@ -43,10 +43,49 @@
     Very doubtful.
 */
 
+let randomAnswer = 0;
+let answerType = 0;
+
+let veryPositiveAnswers = [
+    `It is certain.`,
+    `It is decidedly so.`,
+    `Without a doubt.`,
+    `Yes - definitely.`,
+    `You may rely on it.`
+]
+
+let positiveAnswers = [
+    `As I see it, yes.`,
+    `Most likely.`,
+    `Outlook good.`,
+    `Yes.`,
+    `Signs point to yes.`
+]
+
+let negativeAnswers = [
+    `Reply hazy, try again.`,
+    `Ask again later.`,
+    `Better not tell you now.`,
+    `Cannot predict now.`,
+    `Concentrate and ask again.`
+]
+
+let veryNegativeAnswers = [
+    `Don't count on it.`,
+    `My reply is no.`,
+    `My sources say no.`,
+    `Outlook not so good.`,
+    `Very doubtful.`
+]
+
+let allAnswers = veryPositiveAnswers.concat(positiveAnswers, negativeAnswers, veryNegativeAnswers);
+
 // This should log "The ball has shaken!"
 // and return the answer.
 function shakeBall() {
-  //Write your code in here
+  console.log(`The ball has shaken!`)
+  randomAnswer = allAnswers[Math.floor(Math.random() * allAnswers.length)]
+  return randomAnswer;
 }
 
 /* 
@@ -59,7 +98,15 @@ function shakeBall() {
   This function should expect to be called with any value which was returned by the shakeBall function.
 */
 function checkAnswer(answer) {
-  //Write your code in here
+  if (veryPositiveAnswers.includes(answer)) {
+    return `Very Positive.`;
+  } else if (positiveAnswers.includes(answer)) {
+    return `Positive.`;
+  } else if (negativeAnswers.includes(answer)) {
+    return `Negative.`;
+  } else if (veryNegativeAnswers.includes(answer)) {
+    return `Very Negative.`;
+  }
 }
 
 /* 
