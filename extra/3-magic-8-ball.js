0a4eec9b3e9f5577c479b8ab18b1244bbd566caf
Saliha Popal <salihapopal@gmail.com>
2023-02-24 11:17:56 +0000

    Mandatory done

--- extra/3-magic-8-ball.js
+++ extra/3-magic-8-ball.js
@@ -13,7 +13,7 @@
   and have different levels of positivity or negativity.
 
   Below are the possible answers:
-
+  
   ## Very positive
     It is certain.
     It is decidedly so.
@@ -43,82 +43,125 @@
     Very doubtful.
 */
 
-
-
-// This should log "The ball has shaken!"
-// and return the answer.
-function shakeBall() {
-  //Write your code in here
-  
-}
-
-/* 
-  This function should say whether the answer it is given is
-    - very positive
-    - positive
-    - negative
-    - very negative
-
-  This function should expect to be called with any value which was returned by the shakeBall function.
-*/
-
-function checkAnswer(answer) {
-  //Write your code in here
-
-}
-
-
-/*
-==================================
-======= TESTS - DO NOT MODIFY =====
-
-There are some Tests in this file that will help you work out if your code is working.
-
-To run the tests for just this one file, type `npm test -- --testPathPattern 3-magic-8-ball` into your terminal
-(Reminder: You must have run `npm install` one time before this will work!)
-==================================
-*/
-
-test("whole magic 8 ball sequence", () => {
-  const consoleLogSpy = jest.spyOn(global.console, "log");
-  const answer = shakeBall();
-
-  expect(typeof answer).toEqual("string");
-
-  expect(consoleLogSpy).toHaveBeenCalledTimes(1);
-  expect(consoleLogSpy).toHaveBeenLastCalledWith("The ball has shaken!");
-
-  expect(checkAnswer(answer)).toBeOneOf([
-    "very positive",
-    "positive",
-    "negative",
-    "very negative",
-  ]);
-});
-
-test("magic 8 ball returns different values each time", () => {
-  const seenAnswers = new Set();
-  for (let i = 0; i < 10; ++i) {
-    seenAnswers.add(shakeBall());
-  }
-  if (seenAnswers.size < 2) {
-    throw Error(
-      "Expected to get different random answers each time shakeBall was called, but always got the same one"
-    );
-  }
-
-  let seenPositivities = new Set(Array.from(seenAnswers.values()).map(checkAnswer));
-  if (seenPositivities.size < 2) {
-    throw (
-      "Expected to random answers with different positivities each time shakeBall was called, but always got the same one"
-    );
-  }
-});
-
-test("checkAnswer works for `It is decidedly so.`", () => {
-  expect(checkAnswer("It is decidedly so.")).toEqual("very positive");
-});
-
-test("checkAnswer works for `My reply is no.`", () => {
-  expect(checkAnswer("My reply is no.")).toEqual("very negative");
-});
+let VeryPositive = [
+  'It is certain.',
+  'It is decidedly so.',
+  'Without a doubt.',
+  'Yes - definitely.',
+  'You may rely on it.'
+ ]
+ let positive = [
+   'As I see it, yes.',
+   'Most likely.',
+   'Outlook good.',
+   'Yes.',
+   'Signs point to yes.'
+ ]
+ let negative = [
+   'Reply hazy, try again.',
+   'Ask again later.',
+   'Better not tell you now.',
+   'Cannot predict now.',
+   'Concentrate and ask again.'
+ ]
+ let veryNegative = [
+   "Don't count on it.",
+   'My reply is no.',
+   'My sources say no.',
+   'Outlook not so good.',
+   'Very doubtful.'
+  ]
+ 
+ // This should log "The ball has shaken!"
+ // and return the answer.
+ 
+ 
+ 
+ function shakeBall() {
+   //Write your code in here
+  console.log("The ball has shaken!");
+  const answerMood=  Math.floor(Math.random() * answers.length);
+  const randomAnswer=  Math.floor(Math.random() * answers[answerMood].length);
+  return answers[answerMood][randomAnswer];
+ 
+ }
+ 
+ /* 
+   This function should say whether the answer it is given is
+     - very positive
+     - positive
+     - negative
+     - very negative
+ 
+   This function should expect to be called with any value which was returned by the shakeBall function.
+ */
+ function checkAnswer(answer) {
+   //Write your code in here
+   if(VeryPositive.includes(answer)){
+     return 'very positive';
+   } else if (positive.includes(answer)){
+     return 'positive';
+   } else if (negative.includes(answer)){
+     return 'negative';
+   } else if(veryNegative.includes(answer)){
+     return 'very negative';
+   } else if (answer.size < 2){
+     return 'throw Error'
+   }
+ }
+ 
+ /* 
+ ==================================
+ ======= TESTS - DO NOT MODIFY =====
+ 
+ There are some Tests in this file that will help you work out if your code is working.
+ 
+ To run the tests for just this one file, type `npm test -- --testPathPattern 3-magic-8-ball` into your terminal
+ (Reminder: You must have run `npm install` one time before this will work!)
+ ==================================
+ */
+ 
+ test("whole magic 8 ball sequence", () => {
+   const consoleLogSpy = jest.spyOn(global.console, "log");
+   const answer = shakeBall();
+ 
+   expect(typeof answer).toEqual("string");
+ 
+   expect(consoleLogSpy).toHaveBeenCalledTimes(1);
+   expect(consoleLogSpy).toHaveBeenLastCalledWith("The ball has shaken!");
+ 
+   expect(checkAnswer(answer)).toBeOneOf([
+     "very positive",
+     "positive",
+     "negative",
+     "very negative",
+   ]);
+ });
+ 
+ test("magic 8 ball returns different values each time", () => {
+   const seenAnswers = new Set();
+   for (let i = 0; i < 10; ++i) {
+     seenAnswers.add(shakeBall());
+   }
+   if (seenAnswers.size < 2) {
+     throw Error(
+       "Expected to get different random answers each time shakeBall was called, but always got the same one"
+     );
+   }
+ 
+   let seenPositivities = new Set(Array.from(seenAnswers.values()).map(checkAnswer));
+   if (seenPositivities.size < 2) {
+     throw Error(
+       "Expected to random answers with different positivities each time shakeBall was called, but always got the same one"
+     );
+   }
+ });
+ 
+ test("checkAnswer works for `It is decidedly so.`", () => {
+   expect(checkAnswer("It is decidedly so.")).toEqual("very positive");
+ });
+ 
+ test("checkAnswer works for `My reply is no.`", () => {
+   expect(checkAnswer("My reply is no.")).toEqual("very negative");
+ });
+ 
