44e89c7b5718a447735c0fc099ef7cc9a8995531
saqibjvd <70890519+saqibjvd@users.noreply.github.com>
2023-02-24 01:23:49 +0000

    Update 3-magic-8-ball.js
    8 ball completed
    piping.js not working

--- extra/3-magic-8-ball.js
+++ extra/3-magic-8-ball.js
@@ -47,36 +47,36 @@
 // and return the answer.
 
 let veryPositiveAnswers = [
-  "It is certain",
-  "It is decidedly so",
-  "Without a doubt",
-  "Yes - definitely",
-  "You may rely on it",
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes - definitely.",
+  "You may rely on it.",
 ];
 
 let positiveAnswers = [
-  "As I see it, yes",
-  "Most likely",
-  "Outlook good",
-  "Yes",
-  "Signs point to yes",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
 ];
 
 let negativeAnswers = [
-  "Reply hazy",
-  "try again",
-  "Ask again later",
-  "Better not tell you now",
-  "Cannot predict now",
-  "Concentrate and ask again",
+  "Reply hazy.",
+  "try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
 ];
 
 let veryNegativeAnswers = [
-  "Don't count on it",
-  "My reply is no",
-  "My sources say no",
-  "Outlook not so good",
-  "Very doubtful",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful.",
 ];
 
 let answers = [
