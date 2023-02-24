4df05a0f1c44fe91abba4784121fd5f0ad715a4d
Pouya <113245009+85pouya@users.noreply.github.com>
2023-02-24 13:36:36 +0000

    solved the issues in exercises 1-4

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,9 +1,11 @@
 // Add comments to explain what this function does. You're meant to use Google!
+// In JavaScript, random() is a built-in method that is used to return a random number in the range 0 to 1 (with 0 being inclusive and 1 exclusive). The distribution of the generated random numbers is approximately uniform. It does not actually return a whole number. Instead, it returns a floating point (decimal) value.
 function getRandomNumber() {
   return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+// The concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
@@ -11,6 +13,8 @@ function combine2Words(word1, word2) {
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+  let sentence= firstWord.concat(" ", secondWord, " ", thirdWord);
+  return sentence;
 }
 
 /* 
--- mandatory/4-tax.js
+++ mandatory/4-tax.js
@@ -5,7 +5,10 @@
   Sales tax is 20% of the price of the product.
 */
 
-function calculateSalesTax() {}
+function calculateSalesTax(price) {
+  let taxprice = price + price*0.2;
+  return taxprice;
+}
 
 /*
   CURRENCY FORMATTING
@@ -17,7 +20,10 @@ function calculateSalesTax() {}
   Remember that the prices must include the sales tax (hint: you already wrote a function for this!)
 */
 
-function addTaxAndFormatCurrency() {}
+function addTaxAndFormatCurrency(price) {
+  let taxprice = price + price*0.2;
+  return taxprice = '£' + taxprice.toFixed(2);
+}
 
 /* 
 ===================================================
