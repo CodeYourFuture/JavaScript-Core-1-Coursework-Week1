9313ca36084b99b728bd6ea3c165dafab6053786
Saliha Popal <salihapopal@gmail.com>
2023-02-26 11:07:47 +0000

    Extra done

--- extra/2-piping.js
+++ extra/2-piping.js
@@ -16,26 +16,30 @@
   the final result to the variable goodCode
 */
 
-function add() {
-
+function add(a, b) {
+  return a + b;
 }
 
-function multiply() {
-
+function multiply(a, b) {
+    return a * b;
 }
 
-function format() {
-
+function format(value) {
+  let currency = "£"
+   return "£" + value;
 }
 
 const startingValue = 2;
 
 // Why can this code be seen as bad practice? Comment your answer.
-let badCode =
+let badCode = format(multiply(add(startingValue, 10), 2));
 
 /* BETTER PRACTICE */
 
-let goodCode =
+let sum = add(startingValue, 10);
+let multiplies = multiply(sum , 2);
+let goodCode = format(multiplies);
+// let goodCode
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
--- mandatory/4-tax.js
+++ mandatory/4-tax.js
@@ -5,7 +5,11 @@
   Sales tax is 20% of the price of the product.
 */
 
-function calculateSalesTax() {}
+function calculateSalesTax(price) {
+  let salesTax = price * 0.2;
+  let productPrice = price + salesTax;
+  return productPrice;
+}
 
 /*
   CURRENCY FORMATTING
@@ -17,7 +21,10 @@ function calculateSalesTax() {}
   Remember that the prices must include the sales tax (hint: you already wrote a function for this!)
 */
 
-function addTaxAndFormatCurrency() {}
+function addTaxAndFormatCurrency(price) {
+  let productPrice = calculateSalesTax(price);
+  return "£" + productPrice.toFixed(2);
+}
 
 /* 
 ===================================================
