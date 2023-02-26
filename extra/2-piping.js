4b2a24ed3e38883cb349c5a942f49bcc7fafbcf3
Your Name <youremail@domain.com>
2023-02-26 15:59:41 +0000

    2-piping fixed

--- extra/2-piping.js
+++ extra/2-piping.js
@@ -26,18 +26,28 @@ function multiply(num1,num2) {
   return result;
 }
 
-function format(num) {
-return `£` +num;
+function format(startingValue) {
+return (`£` +startingValue);
 }
 
 const startingValue = 2;
 
 // Why can this code be seen as bad practice? Comment your answer.
-let badCode =
+/*The string "£24" meaning that it appears without context or explanation
+in the code. This can make the code harder to read and maintain*/
+let badCode = "£24";
+console.log("The bad code is: " + badCode);
 
 /* BETTER PRACTICE */
 
-//let goodCode =
+
+let currencySymbol = "£";
+let  price = 24;
+let goodCode = `${currencySymbol}${price}`;
+//This code is easy to read and understand, with no unnecessary complexity
+
+
+
 
 /* ======= TESTS - DO NOT MODIFY =====
 There are some Tests in this file that will help you work out if your code is working.
