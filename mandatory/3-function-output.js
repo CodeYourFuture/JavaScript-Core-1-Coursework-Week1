7fc7b6d81dd2802c152b4edf27af4b45ae4853a8
RihannaP <114884466+RihannaP@users.noreply.github.com>
2023-04-02 21:24:52 +0100

    update 3-function-output

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,14 +1,18 @@
 // Add comments to explain what this function does. You're meant to use Google!
-function getRandomNumber() {
-  return Math.random() * 10;
+// In JavaScript, random() is a built-in method that is used to return arandom number in the range 0 to 1 (with 0 being inclusive and 1 exclusive). The distribution of the generated random numbers is approximately uniform. It does not actually return a whole number. Instead, it returns a floating point (decimal) value.
+function getRandomNumber(number) {
+  return Math.random(number) * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+// The concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
+
+  return `${firstWord} ${secondWord} ${thirdWord}`
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
 }
