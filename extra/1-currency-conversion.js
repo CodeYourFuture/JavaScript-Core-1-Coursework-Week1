abc5b8a19ab8a61b93366a04af3b33dd5a691c83
AdrianIlovan <adrian88ilovan@yahoo.com>
2023-05-28 15:47:19 +0100

    extra done!

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -5,7 +5,10 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(priceInGBP) {
+  var priceInUSD = priceInGBP * 1.4;
+  return priceInUSD;
+}
 
 /*
   CURRENCY CONVERSION
@@ -15,7 +18,10 @@ function convertToUSD() {}
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL() {}
+function convertToBRL(priceInGBP) {
+  var priceInBRL = (priceInGBP * 5.7) * 0.99;
+  return priceInBRL;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
--- extra/2-piping.js
+++ extra/2-piping.js
@@ -16,26 +16,28 @@
   the final result to the variable goodCode
 */
 
-function add() {
-
+function add(a, b) {
+  return a + b;
 }
 
-function multiply() {
-
+function multiply(a, b) {
+  return a * b;
 }
 
-function format() {
-
+function format(format) {
+  var format = "£" + format;
+  return format;
 }
 
 const startingValue = 2;
 
 // Why can this code be seen as bad practice? Comment your answer.
-let badCode =
+let badCode = (startingValue + 10) * 2; return format+badCode;  // because it's hard to read 
 
 /* BETTER PRACTICE */
 
-let goodCode =
+let goodCode = (startingValue + 10) * 2;
+    return format + goodCode;
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
